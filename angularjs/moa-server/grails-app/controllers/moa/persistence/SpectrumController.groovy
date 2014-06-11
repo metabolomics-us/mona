0b45f8c5543ef9913b03be1e97e8cdf20bff8d6e
Gert Wohlgemuth <berlinguyinca@gmail.com>
2014-06-11 11:53:24 -0700

    removed spectra

--- angularjs/moa-server/grails-app/controllers/moa/persistence/SpectrumController.groovy
+++ angularjs/moa-server/grails-app/controllers/moa/persistence/SpectrumController.groovy
@@ -3,4 +3,19 @@ package moa.persistence
 class SpectrumController {
 
     def index() { }
+
+
+    /**
+     * otherwise grails won't populate the json fields
+     * @return
+     */
+    protected Map getParametersToBind() {
+        if (request.JSON) {
+            params.putAll(
+                    request.JSON)
+        }
+
+        println "modified params: ${params}"
+        params
+    }
 }
