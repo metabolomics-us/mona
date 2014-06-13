df6a4733700cabaab0c797c4c61aa34255bcb91f
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2014-06-13 00:44:42 -0700

    remove files

--- angularjs/moa-server/grails-app/domain/moa/Tag.groovy
+++ angularjs/moa-server/grails-app/domain/moa/Tag.groovy
@@ -4,17 +4,11 @@ class Tag {
     static mapWith = "mongo"
 
     static constraints = {
-<<<<<<< HEAD
-        text(blank: false, unique: true)
-||||||| merged common ancestors
-        text(blank: false)
-=======
         text(blank: false, unique: true)
     }
 
     static mapping = {
         text index: true, indexAttributes: [unique: true]
->>>>>>> origin/master
     }
 
     /**
