5066492e7246cb427c31760f7e535ac3a11a03ca
Gert Wohlgemuth <berlinguyinca@gmail.com>
2014-07-10 14:21:55 -0700

    finished query of doom for now

--- angularjs/moa-server/grails-app/controllers/moa/query/SpectraQueryController.groovy
+++ angularjs/moa-server/grails-app/controllers/moa/query/SpectraQueryController.groovy
@@ -25,7 +25,7 @@ class SpectraQueryController {
 
         def json = request.JSON
 
-
+        /*
         def result = Spectrum.createCriteria().list() {
 
             //we have compound information available
@@ -92,6 +92,92 @@ class SpectraQueryController {
             }
         }
 
+*/
+
+        //completed query string
+        String queryOfDoom = "select s from Spectrum s "
+
+        //defines all our joins
+        String queryOfDoomJoins = ""
+
+        //defines our where clause
+        String queryOfDoomWhere = " where "
+
+        //our defined execution parameters
+        def executionParams = [:]
+
+        //if we have a compound
+        if (json.compound) {
+
+            //if we have a compound name
+            if (json.compound.name) {
+
+                queryOfDoomJoins += " left join s.biologicalCompound.names as bc"
+                queryOfDoomJoins += " left join s.chemicalCompound.names as cc"
+
+                //if we have a like condition specified
+                if (json.compound.name.like) {
+                    queryOfDoomWhere += "(bc.name like :compoundName or cc.name like :compoundName)"
+                    executionParams.compoundName = json.compound.name.like
+                }
+
+                //if we have an equals condition specified
+                else if (json.compound.name.eq) {
+                    queryOfDoomWhere += "(bc.name = :compoundName or cc.name = :compoundName)"
+                    executionParams.compoundName = json.compound.name.eq
+
+                }
+                //well we don't know this, do we?
+                else {
+                    throw new QueryException("invalid query term: ${json.compound.name}")
+                }
+            }
+        }
+
+
+        //if we have a metadata object specified
+        if (json.metaData) {
+
+            //go over each metadata definition
+            json.metaData.eachWithIndex { current, index ->
+                def impl = [:];
+
+                //figure out the correct value for equals
+                if (current.value.eq) {
+                    impl = estimateMetaDataValueImpl(current.value.eq.toString())
+                } else {
+                    throw new QueryException("invalid query term: ${current.value.eq}")
+                }
+
+                //build the join for each metadata object link
+                queryOfDoomJoins += " left join s.metaData as mdv_${index}"
+                queryOfDoomJoins += " left join mdv_${index}.metaData as md_${index}"
+
+                //if there is something in the where clause we need an and
+                if(!queryOfDoomWhere.equals(" where ")){
+                    queryOfDoomWhere+=" and "
+                }
+
+                //add the where clause
+                queryOfDoomWhere += "("
+                queryOfDoomWhere += " md_${index}.name = :metaDataName_${index}"
+                queryOfDoomWhere += " and "
+                queryOfDoomWhere += " mdv_${index}.${impl.name} = :metaDataImplValue_${index}"
+                queryOfDoomWhere += ")"
+
+                executionParams.put("metaDataName_${index}".toString(), current.name);
+                executionParams.put("metaDataImplValue_${index}".toString(), impl.value);
+
+            }
+        }
+
+        //assemble the query of doom
+        queryOfDoom = queryOfDoom + queryOfDoomJoins + queryOfDoomWhere
+
+        log.info("generated doom query: \n\n${queryOfDoom}\n\n")
+        log.info("parameter matrix:\n\n ${executionParams}")
+
+        def result = Spectrum.executeQuery(queryOfDoom, executionParams)
 
         log.info("received results from query: ${result.size()}")
         render(result as JSON)
