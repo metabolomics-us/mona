d46ba2c38a1b0936ef3e183e1331b67a59b5c425
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2014-07-10 16:07:32 -0700

    updated query

--- angularjs/moa-client/app/scripts/controllers/query/spectraQueryController.js
+++ angularjs/moa-client/app/scripts/controllers/query/spectraQueryController.js
@@ -6,43 +6,62 @@
 moaControllers.SpectraQueryController = function ($scope, $modal, MetadataService, $log) {
     // Query values
     $scope.query = {};
-    $scope.query.accurateMassTolerance = 0.5;
+
 
 
     $scope.submitQuery = function() {
         // Build individual criteria
-        var criteria = [];
+        var compound = {};
+        var metaData = [];
 
-        Object.keys($scope.query).forEach(function(element, index, array) {
-            if(element === "accurateMassTolerance")
-                return;
 
-            else if(element === "Mz_exact") {
-                if($scope.query[element]) {
-                    var min = parseFloat($scope.query[element]) - parseFloat($scope.query.Mz_exact);
-                    var max = parseFloat($scope.query[element]) + parseFloat($scope.query.Mz_exact);
-                    criteria.push({between: ["accurateMass", min, max]});
-                }
-            }
-
-            else
-                if($scope.query[element])
-                    criteria.push({eq: [element, $scope.query[element]]});
+        // Get all metadata in a single dictionary
+        var meta = {};
+        Object.keys($scope.metadata).forEach(function(element, index, array) {
+            for(var i = 0; i < $scope.metadata[element].length; i++)
+            meta[$scope.metadata[element][i].name] = $scope.metadata[element][i];
         });
 
 
-        // Build criteria query
-        var criteriaQuery = criteria[0];
-        /*
-        for(var i = 1; i < criteria.length; i++) {
-            var temp = criteriaQuery;
-            criteriaQuery = criteria[i];
-            criteriaQuery.and = temp;
-        }
-        */
-        $scope.criteriaQuery = criteriaQuery;
+        Object.keys($scope.query).forEach(function(element, index, array) {
+            if(element === "nameFilter" && $scope.query[element])
+                compound.name = {like: $scope.query[element]};
 
-        console.log($scope.metadataValues);
+            else if(element === "inchiFilter" && $scope.query[element]) {
+                if(/^([A-Z]{14}-[A-Z]{10}-[A-Z,0-9])+$/.test($scope.query[element]))
+                    compound.inchiKey = {eq: $scope.query[element]};
+                else
+                    compound.inchiKey = {like: '%'+ $scope.query[element] +'%'};
+            }
+
+            // Ignore tolerance values
+            else if(element.indexOf("_tolerance", element.length - 10) !== -1)
+                return;
+
+            else {
+                if($scope.query[element]) {
+                    if (meta[element].type === "double") {
+                        if((element +"_tolerance") in $scope.query && $scope.query[element +"_tolerance"]) {
+                            var min = parseFloat($scope.query[element]) - parseFloat($scope.query[element +"_tolerance"]);
+                            var max = parseFloat($scope.query[element]) + parseFloat($scope.query[element +"_tolerance"]);
+                            metaData.push({name: element, value: {between: [min, max]}});
+                        } else
+                            metaData.push({name: element, value: {eq: parseFloat($scope.query[element])}});
+                    } else
+                        metaData.push({name: element, value: {eq: $scope.query[element]}});
+                }
+            }
+        });
+
+        // Build query
+        var query = {};
+
+        if(compound)
+            query.compound = compound;
+        if(metaData)
+            query.metadata = metaData;
+
+        $scope.compiledQuery = query;
 
         // Do something with it and redirect
     };
@@ -70,7 +89,6 @@ moaControllers.SpectraQueryController = function ($scope, $modal, MetadataServic
                         Object.keys(values).forEach(function(key, index, array) {
                             $scope.metadataValues[element.name].push({value: key});
                         });
-                        console.log($scope.metadataValues[element.name]);
                     },
                     function (error) {
                         $log.error('metadata values failed: ' + error);
