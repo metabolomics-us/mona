d88501056890cb20255197794efa5e399e8df7ee
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2014-07-10 17:23:27 -0700

    infinite scroll for spectra

--- angularjs/moa-client/app/scripts/app.js
+++ angularjs/moa-client/app/scripts/app.js
@@ -21,6 +21,13 @@ app.constant('REST_BACKEND_SERVER', 'http://trashcan.fiehnlab.ucdavis.edu:8080')
 //app.constant('REST_BACKEND_SERVER', 'http://localhost:8080');
 //app.constant('REST_BACKEND_SERVER', 'http://cream.fiehnlab.ucdavis.edu:8080');
 
+
+/**
+ * Number of spectra/compounds to view
+ */
+app.constant('MAX_OBJECTS', 10);
+
+
 /**
  * enable cross domain stuff
  */
