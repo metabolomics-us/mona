6090f316fd6862b73b025b8e7d209796e9ef7165
Gert Wohlgemuth <berlinguyinca@gmail.com>
2014-12-18 13:13:03 -0800

    added little script for simple testdata submission to localhost
    Former-commit-id: fdafef6c28d6b97ec277032aa93a8a18618fc563

---
+++ test-data/submit.local.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for i in `seq $2`; do curl -XPOST -H 'Content-Type:application/json' -H 'Accept:application/json' --data-binary @$1 http://localhost:8080/rest/spectra/batch/save; done
