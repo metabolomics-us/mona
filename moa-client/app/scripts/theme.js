624186e89fcdae1e4879374502d7c1334c811f5c
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-02-18 13:41:16 -0800

    fixed js error in theme (only occurs in dist mode)
    Former-commit-id: c227e635ad88aa4b34021a4a12facc763f933a84

--- moa-client/app/scripts/theme.js
+++ moa-client/app/scripts/theme.js
@@ -11,7 +11,7 @@ $(function() {
 //collapses the sidebar on window resize.
 $(function() {
     $(window).bind("load resize", function() {
-        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
             $('div.sidebar-collapse').addClass('collapse')
         } else {
