c614be3b914299179fb20ac5d65f8ca27b6292cc
Gert Wohlgemuth <berlinguyinca@gmail.com>
2015-02-19 10:40:38 -0800

    added script to completely automated the docker image creation
    Former-commit-id: a93e0be4a1834ee1db2df9bf520ad5e772046212

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#simple build file for our docker image of the latest version
+
+#build client module
+cd moa-client
+grunt dist
+cd ..
+
+#build server module
+
+cd moa-server
+grails war
+cd ..
+
+#assmeble docker file
+
+cd mona-docker
+
+cp ../moa-server/target/mona-server.war root.war
+cp ../moa-client/mona-client.zip client.zip
+
+$(boot2docker shellinit)
+
+docker build .
