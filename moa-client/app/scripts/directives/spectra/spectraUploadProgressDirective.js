ba0ceb4783797ef3bd7233517ab0c231bd7cfcca
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-03-02 15:38:25 -0800

    estimated time remaining for upload
    Former-commit-id: 58be3e1602fb6f25df32450550f761c389ca8b8e

--- moa-client/app/scripts/directives/spectra/spectraUploadProgressDirective.js
+++ moa-client/app/scripts/directives/spectra/spectraUploadProgressDirective.js
@@ -12,10 +12,11 @@ app.directive('spectraUploadProgressBar', function () {
         replace: false,
         template:
             '<div ng-if="running">'+
-            '    <div class="text-center"><i>Processed {{completedSpectraCount}} / {{uploadedSpectraCount}} spectra</i></div>'+
+            '    <div class="text-center"><i>Processed {{completedSpectraCount}} / {{uploadedSpectraCount}} spectra.</i></div>'+
             '    <progressbar ng-class="{active: running, \'progress-striped\': running}" max="100" value="spectraUploadProgress">'+
             '        <span style="color: black; white-space: nowrap; font-style: italic; font-weight: bold;" ng-bind="spectraUploadProgressString"></span>'+
             '    </progressbar>'+
+            '    <div class="text-center">{{etaString}}</div>'+
             '</div>'+
             '<div ng-if="!running"><i>No Upload Started</i></div>',
 
@@ -24,16 +25,47 @@ app.directive('spectraUploadProgressBar', function () {
          * @param $scope
          */
         controller: function ($scope, UploadLibraryService) {
+            $scope.etaString = '';
+
+            var buildEtaString = function() {
+                if (UploadLibraryService.uploadStartTime == -1 || !UploadLibraryService.isUploading()) {
+                    $scope.etaString = '';
+                }
+
+                if ($scope.completedSpectraCount == 0) {
+                    $scope.etaString = 'Starting upload...'
+                }
+
+                // Calculate estimated time remaining
+                var dt = new Date().getTime() - UploadLibraryService.uploadStartTime;
+                var eta = dt * ($scope.uploadedSpectraCount - $scope.completedSpectraCount) / $scope.completedSpectraCount / 1000;
+
+                var seconds = Math.floor(eta % 60);
+                var minutes = Math.floor((eta / 60) % 60);
+                var hours = Math.floor(eta / 3600);
+
+                var etaString = '';
+
+                if (hours > 0) etaString += ' '+ hours +' hours';
+                if (minutes > 0 || hours > 0) etaString += ' '+ minutes +' minutes';
+                if (seconds > 0 || minutes > 0 || hours > 0) etaString += ' '+ seconds +' seconds';
+
+                if (etaString != '') {
+                    $scope.etaString = 'Estimated' + etaString + ' remaining';
+                } else {
+                    $scope.etaString = '';
+                }
+            };
+
             $scope.$on('spectra:uploadprogress', function(event, completedSpectraCount, uploadedSpectraCount) {
                 $scope.completedSpectraCount = completedSpectraCount;
                 $scope.uploadedSpectraCount = uploadedSpectraCount;
 
                 $scope.spectraUploadProgress = parseInt(((completedSpectraCount / uploadedSpectraCount) * 100), 10);
                 $scope.spectraUploadProgressString = $scope.spectraUploadProgress +'%';
+                buildEtaString();
 
                 $scope.running = $scope.spectraUploadProgress != -1;
-
-                console.log('spectra:uploadprogress '+ completedSpectraCount +' '+ uploadedSpectraCount +' '+ $scope.running)
             });
 
             (function() {
@@ -46,6 +78,8 @@ app.directive('spectraUploadProgressBar', function () {
                     $scope.spectraUploadProgressString = 'Processing...';
                     $scope.running = false;
                 }
+
+                buildEtaString();
             })();
         }
     };
