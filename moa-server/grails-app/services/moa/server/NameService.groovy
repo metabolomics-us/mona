ae449f28cb8d9575c0d2d4fa63ceb53201f6aa91
Gert Wohlgemuth <berlinguyinca@gmail.com>
2015-03-19 15:48:07 -0700

    optimized directive a bit to have quicker query speed
    Former-commit-id: 7dd66b4b4aa3bb4f8ab0e7a6036f0469ccc402c3

--- moa-server/grails-app/services/moa/server/NameService.groovy
+++ moa-server/grails-app/services/moa/server/NameService.groovy
@@ -3,6 +3,7 @@ package moa.server
 import grails.transaction.Transactional
 import moa.Compound
 import moa.Name
+import org.apache.xpath.operations.String
 
 @Transactional
 class NameService {
@@ -11,5 +12,7 @@ class NameService {
 
         log.debug("checking if compound ${compound.inchiKey} has name ${name}")
         Name.findOrCreateByNameAndCompound(name, compound).save()
+
+
     }
 }
