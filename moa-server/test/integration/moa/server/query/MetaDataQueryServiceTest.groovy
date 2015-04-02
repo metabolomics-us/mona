f03e365297ca6ef67b57110d0f950f5f8130940b
Diego Pedrosa <linuxmant@gmail.com>
2015-04-02 13:39:14 -0700

    add more tests add schema validation as test method (for learning api)
    Former-commit-id: ed5e7f319539015c7b3ba45ab75d267103af067b

--- moa-server/test/integration/moa/server/query/MetaDataQueryServiceTest.groovy
+++ moa-server/test/integration/moa/server/query/MetaDataQueryServiceTest.groovy
@@ -1,14 +1,18 @@
 package moa.server.query
 
+import com.github.fge.jackson.JsonLoader
+import com.github.fge.jsonschema.core.report.ProcessingReport
+import com.github.fge.jsonschema.main.JsonSchema
+import com.github.fge.jsonschema.main.JsonSchemaFactory
+import groovy.json.JsonBuilder
 import moa.MetaDataValue
 import org.apache.log4j.Logger
 import org.junit.Before
-
 /**
  * Created by diego on 3/31/15.
  */
 class MetaDataQueryServiceTest extends GroovyTestCase {
-	private Logger log = Logger.getLogger(this.class.simpleName)
+	private Logger log = Logger.getLogger(this.class.name)
 
 	def MetaDataQueryService metaDataQueryService
 
@@ -57,7 +61,7 @@ class MetaDataQueryServiceTest extends GroovyTestCase {
 		def res = metaDataQueryService.query(json, params)
 
 		assert res != null
-		assert 10 <= res.size()
+		assert res.size() == 10
 		assert (res['stringValue'] as HashSet<String>).contains("MS2")
 		assert (res['stringValue'] as HashSet<String>).size() == 1
 	}
@@ -69,7 +73,7 @@ class MetaDataQueryServiceTest extends GroovyTestCase {
 		def res = metaDataQueryService.query(json, params)
 
 		assert res != null
-		assert 10 <= res.size()
+		assert res.size() == 10
 		assert (res['stringValue'] as HashSet<String>).contains("MS2")
 		assert (res['stringValue'] as HashSet<String>).size() == 1
 	}
@@ -81,7 +85,7 @@ class MetaDataQueryServiceTest extends GroovyTestCase {
 		def res = metaDataQueryService.query(json, params)
 
 		assert res != null
-		assert 10 <= res.size()
+		assert res.size() == 10
 	}
 
 	void testQueryCategoryShort() {
@@ -91,7 +95,7 @@ class MetaDataQueryServiceTest extends GroovyTestCase {
 		def res = metaDataQueryService.query(json, params)
 
 		assert res != null
-		assert res.size() <= 10
+		assert res.size() == 10
 		assert "143".equals(res[0].value)
 		assert "num peaks".equals(res[0].metaData.name)
 	}
@@ -103,7 +107,7 @@ class MetaDataQueryServiceTest extends GroovyTestCase {
 		def res = metaDataQueryService.query(json, params)
 
 		assert res != null
-		assert res.size() <= 10
+		assert res.size() == 10
 		assert "143".equals(res[0].value)
 		assert "num peaks".equals(res[0].metaData.name)
 	}
@@ -115,21 +119,31 @@ class MetaDataQueryServiceTest extends GroovyTestCase {
 		def res = metaDataQueryService.query(json, params)
 
 		assert res != null
-		assert res.size() <= 10
+		assert res.size() == 0 // units not implemented yet, change to 10 after implementation
 	}
 
-	void testQueryUnitInValueLong() {
-		def json = [value: [ne: "10", unit: [like: "eV"]]]
+	void testQueryUnitShort() {
+		def json = [unit: "eV"]
 		def params = [max: 10]
 
 		def res = metaDataQueryService.query(json, params)
 
 		assert res != null
-		assert res.size() <= 10
+		assert res.size() == 0 // units not implemented yet, change to 10 after implementation
+	}
+
+	void testQueryUnitInValueLong() {
+		def json = [value: [ne: "10", unit: "eV"]]
+		def params = [max: 10]
+
+		def res = metaDataQueryService.query(json, params)
+
+		assert res != null
+		assert res.size() == 0 // units not implemented yet, change to 10 after implementation
 	}
 
 	void testEmptyQuery() {
-		def json = []
+		def json = [:]
 		def params = [max: 10]
 
 		def res = metaDataQueryService.query(json, params)
@@ -137,7 +151,7 @@ class MetaDataQueryServiceTest extends GroovyTestCase {
 		log.debug("res: $res")
 		shouldFail {}
 		assert res != null
-		assert res.size() <= 10
+		assert res.size() == 10
 	}
 
 	void testSimplestQuery() {
@@ -148,6 +162,40 @@ class MetaDataQueryServiceTest extends GroovyTestCase {
 
 		log.debug("res: $res")
 		assert res != null
-		assert res.size() <= 10
+		assert res.size() == 10
+	}
+
+	void testValidCheck() {
+		def jsonObj = [name: ["like": "ms type"], value: ["eq": "MS2"]]
+		def jsonBadObj = [value: ["eq": "MS2"]]
+
+		/**
+		 * checks the json object against a schema
+		 * @param jsonObj map containing the json object
+		 * @return true if the json object is valid according to the schema, false other way.
+		 */
+		def jsonString = new JsonBuilder(jsonObj).toString()
+		def badJsonString = new JsonBuilder(jsonBadObj).toString()
+
+		def qsFile = new File("schemas/QuerySchema.json")
+
+		final JsonSchemaFactory factory = JsonSchemaFactory.byDefault()
+		final JsonSchema schema = factory.getJsonSchema(qsFile.toURI().toString().concat("#/definitions/metadata/items/0"))
+
+		ProcessingReport report
+		report = schema.validate(JsonLoader.fromString(jsonString))
+
+		assert report.success
+
+		ProcessingReport report2
+		report2 = schema.validate(JsonLoader.fromString(badJsonString))
+
+		assert report2.success == false
+		assert report2[0].message.contains("property \"value\"")
+		assert report2[0].message.contains("requires [\"name\"]")
+		assert report2[0].message.contains("missing: [\"name\"]")
+
+//			return false
+
 	}
 }
