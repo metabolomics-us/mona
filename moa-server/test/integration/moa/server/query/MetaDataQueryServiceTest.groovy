81c90b30b363d333ae65074d629d2a02a226bc60
Diego Pedrosa <linuxmant@gmail.com>
2015-04-06 15:07:13 -0700

    add check for 'unit' key inside metadata.value add check for 'compound.id' being a map containing an operator refactor spectra and metadata query service tests from JUnit to spock
    Former-commit-id: d35ca0f65d04c52f449c20f80556e6d0de82d862

--- moa-server/test/integration/moa/server/query/MetaDataQueryServiceTest.groovy
+++ moa-server/test/integration/moa/server/query/MetaDataQueryServiceTest.groovy
@@ -1,201 +1,88 @@
 package moa.server.query
 
-import com.github.fge.jackson.JsonLoader
-import com.github.fge.jsonschema.core.report.ProcessingReport
-import com.github.fge.jsonschema.main.JsonSchema
-import com.github.fge.jsonschema.main.JsonSchemaFactory
-import groovy.json.JsonBuilder
-import moa.MetaDataValue
+import grails.test.spock.IntegrationSpec
 import org.apache.log4j.Logger
-import org.junit.Before
+import spock.lang.Unroll
 /**
  * Created by diego on 3/31/15.
  */
-class MetaDataQueryServiceTest extends GroovyTestCase {
+class MetaDataQueryServiceTest extends IntegrationSpec {
 	private Logger log = Logger.getLogger(this.class.name)
 
-	def MetaDataQueryService metaDataQueryService
-
-	@Before
-	void setUp() {
-		metaDataQueryService = new MetaDataQueryService()
-	}
+	def MetaDataQueryService metaDataQueryService = new MetaDataQueryService()
 
 	void testQueryNullJson() {
-		def params = [:]
 
-		// should throw an Exception
-		shouldFail(Exception) {
-			metaDataQueryService.query(null, params)
-		}
+		when:
+		metaDataQueryService.query(null, [:])
+
+		then: "the service throws an exception"
+		Exception e = thrown()
 
 	}
 
-	void testQueryByIdShort() {
-		//getting id
-		long id = MetaDataValue.first().id
+	@Unroll
+	def "query spectra.metadata with #clazz #oper #value (max #size results)"() {
 
-
-		MetaDataValue res = metaDataQueryService.query(id)
+		expect:
+		def res = metaDataQueryService.query([(clazz): [(oper): value]])
+		println "Total: ${res.size()}"
 
 		assert res != null
-		assert res.id == id
+		assert res.size() >= size
+
+		where:
+		oper    | clazz  | value     | size | total
+		"eq"    | "name" | "ms type" | 5    | 5
+		"like"  | "name" | "ms type" | 5    | 5
+		"ilike" | "name" | "ms type" | 5    | 5
+		"gt"    | "name" | "ms type" | 5    | 5
+		"lt"    | "name" | "ms type" | 5    | 5
+		"ge"    | "name" | "ms type" | 5    | 5
+		"le"    | "name" | "ms type" | 5    | 5
+		"ne"    | "name" | "ms type" | 5    | 5
+//		"in"    | "name"     | "ms type"             | 5    | 5
+		"eq" | "value" | "MS2" | 5 | 5    //should fail -- doesn't match schema (needs 'name')
+		"like" | "value" | "MS2" | 5 | 5
+		"ilike" | "value" | "MS2" | 5 | 5
+		"gt" | "value" | "MS2" | 5 | 5
+		"lt" | "value" | "MS2" | 5 | 5
+		"ge" | "value" | "MS2" | 5 | 5
+		"le" | "value" | "MS2" | 5 | 5
+		"ne" | "value" | "MS2" | 5 | 5
+//		"in"    | "value"    | "MS2"                 | 5    | 5
+		"eq" | "category" | "spectral properties" | 5 | 5
+		"like" | "category" | "spectral properties" | 5 | 5
+		"ilike" | "category" | "spectral properties" | 5 | 5
+		"gt" | "category" | "spectral properties" | 0 | 5
+		"lt" | "category" | "spectral properties" | 5 | 5
+		"ge" | "category" | "spectral properties" | 5 | 5
+		"le" | "category" | "spectral properties" | 5 | 5
+		"ne" | "category" | "spectral properties" | 5 | 5
+//		"in"    | "category" | "spectral properties" | 5    | 5
+		"eq" | "id" | 58 | 5 | 5
+		"gt" | "id" | 58 | 5 | 5
+		"lt" | "id" | 58 | 5 | 5
+		"ge" | "id" | 58 | 5 | 5
+		"le" | "id" | 58 | 5 | 5
+		"ne" | "id" | 58 | 5 | 5
 	}
 
-	void testQueryByIdLong() {
-		//getting id
-		long id = MetaDataValue.first().id
+	@Unroll
+	def "query metadata with limit of #limit"() {
 
-		def json = [id: ["like": id]]
-
-		MetaDataValue res = metaDataQueryService.query(id)
+		expect:
+		def res = metaDataQueryService.query([(clazz): [(oper1): value]], [max: limit])
+		println "Total: ${res.size()}"
 
 		assert res != null
-		assert res.id == id
+		assert res.size() == limit
+
+		where:
+		oper1 | clazz      | value                 | size | limit
+		"eq"  | "name"     | "ms type"             | 5    | 3
+		"eq"  | "category" | "spectral properties" | 5    | 3
+		"eq"  | "id"       | 58                    | 5    | 3
 	}
 
-	void testQueryShortJson() {
-		def json = [name:"ms type", value:"MS2"]
-		def params = [max: 10, offset: 5]
-
-		def res = metaDataQueryService.query(json, params)
-
-		assert res != null
-		assert res.size() == 10
-		assert (res['stringValue'] as HashSet<String>).contains("MS2")
-		assert (res['stringValue'] as HashSet<String>).size() == 1
-	}
-
-	void testQueryLongJson() {
-		def json = [name:["like":"ms type"], value:["eq":"MS2"]]
-		def params = [max:10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		assert res != null
-		assert res.size() == 10
-		assert (res['stringValue'] as HashSet<String>).contains("MS2")
-		assert (res['stringValue'] as HashSet<String>).size() == 1
-	}
-
-	void testQueryJsonBetween() {
-		def json = [value: [between: ["0", "1000"]]]
-		def params = [max: 10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		assert res != null
-		assert res.size() == 10
-	}
-
-	void testQueryCategoryShort() {
-		def json = [category: "spectral properties"]
-		def params = [max: 10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		assert res != null
-		assert res.size() == 10
-		assert "143".equals(res[0].value)
-		assert "num peaks".equals(res[0].metaData.name)
-	}
-
-	void testQueryCategoryLong() {
-		def json = [category: [eq: "spectral properties"]]
-		def params = [max: 10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		assert res != null
-		assert res.size() == 10
-		assert "143".equals(res[0].value)
-		assert "num peaks".equals(res[0].metaData.name)
-	}
-
-	void testQueryUnitLong() {
-		def json = [unit: [eq: "eV"]]
-		def params = [max: 10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		assert res != null
-		assert res.size() == 0 // units not implemented yet, change to 10 after implementation
-	}
-
-	void testQueryUnitShort() {
-		def json = [unit: "eV"]
-		def params = [max: 10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		assert res != null
-		assert res.size() == 0 // units not implemented yet, change to 10 after implementation
-	}
-
-	void testQueryUnitInValueLong() {
-		def json = [value: [ne: "10", unit: "eV"]]
-		def params = [max: 10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		assert res != null
-		assert res.size() == 0 // units not implemented yet, change to 10 after implementation
-	}
-
-	void testEmptyQuery() {
-		def json = [:]
-		def params = [max: 10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		log.debug("res: $res")
-		shouldFail {}
-		assert res != null
-		assert res.size() == 10
-	}
-
-	void testSimplestQuery() {
-		def json = [name: "ms type"]
-		def params = [max: 10]
-
-		def res = metaDataQueryService.query(json, params)
-
-		log.debug("res: $res")
-		assert res != null
-		assert res.size() == 10
-	}
-
-	void testValidCheck() {
-		def jsonObj = [name: ["like": "ms type"], value: ["eq": "MS2"]]
-		def jsonBadObj = [value: ["eq": "MS2"]]
-
-		/**
-		 * checks the json object against a schema
-		 * @param jsonObj map containing the json object
-		 * @return true if the json object is valid according to the schema, false other way.
-		 */
-		def jsonString = new JsonBuilder(jsonObj).toString()
-		def badJsonString = new JsonBuilder(jsonBadObj).toString()
-
-		def qsFile = new File("schemas/QuerySchema.json")
-
-		final JsonSchemaFactory factory = JsonSchemaFactory.byDefault()
-		final JsonSchema schema = factory.getJsonSchema(qsFile.toURI().toString().concat("#/definitions/metadata/items/0"))
-
-		ProcessingReport report
-		report = schema.validate(JsonLoader.fromString(jsonString))
-
-		assert report.success
-
-		ProcessingReport report2
-		report2 = schema.validate(JsonLoader.fromString(badJsonString))
-
-		assert report2.success == false
-		assert report2[0].message.contains("property \"value\"")
-		assert report2[0].message.contains("requires [\"name\"]")
-		assert report2[0].message.contains("missing: [\"name\"]")
-
-//			return false
-
-	}
 }
