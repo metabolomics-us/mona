73b152b0f95d0871bb07f48cb9635462be4dd281
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-05-01 11:26:41 -0700

    optimized authentication in upload wizard page
    Former-commit-id: 4d1b088f4d3a7157ea7e68c491ddb99de729bfec

--- moa-client/app/scripts/controllers/upload/spectraUploadController.js
+++ moa-client/app/scripts/controllers/upload/spectraUploadController.js
@@ -4,15 +4,6 @@
 'use strict';
 
 moaControllers.SpectraUploadController = function ($scope, $rootScope, $modal, AuthenticationService) {
-    $scope.isLoggedIn = function() {
-        return AuthenticationService.isLoggedIn();
-    };
-
-    $scope.login = function() {
-        $rootScope.$broadcast('auth:login');
-    };
-
-
     /**
      * Initializes our spectra upload dialog
      */
@@ -35,9 +26,9 @@ moaControllers.SpectraUploadController = function ($scope, $rootScope, $modal, A
      * Check whether the user is uploading, and open the upload dialog accordingly
      */
     (function() {
-        if ($scope.isLoggedIn() && !$scope.spectraUploaded) {
+        if (AuthenticationService.isLoggedIn() && !$scope.spectraUploaded) {
             $scope.uploadSpectraDialog()
-        } else if(!$scope.isLoggedIn() && !AuthenticationService.isLoggingIn()) {
+        } else if(!AuthenticationService.isLoggedIn() && !AuthenticationService.isLoggingIn()) {
             $rootScope.$broadcast('auth:login');
         }
     })();
