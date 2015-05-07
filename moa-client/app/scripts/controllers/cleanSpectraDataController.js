65b8a1fa12aba323b0fa04c4be47b4d2f1bd68ea
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-05-07 15:36:39 -0700

    updates to the mass spec cleaner
    Former-commit-id: 7b3d6c09ec44f7c93545f5c66cdea833a20ce108

--- moa-client/app/scripts/controllers/cleanSpectraDataController.js
+++ moa-client/app/scripts/controllers/cleanSpectraDataController.js
@@ -54,55 +54,9 @@ moaControllers.CleanSpectraDataController = function ($scope, UploadLibraryServi
     };
 
 
-    $scope.parseFile = function(files) {
-        $scope.spectraLoaded = 1;
-
-        UploadLibraryService.loadSpectraFile(files[0],
-            function(data, origin) {
-                UploadLibraryService.processData(data, function(spectrum) {
-                    // Create list of ions
-                    spectrum.basePeak = 0;
-
-                    spectrum.ions = spectrum.spectrum.split(' ').map(function(x) {
-                        x = x.split(':');
-                        var annotation = '';
-
-                        for (var i = 0; i < spectrum.meta.length; i++) {
-                            if (spectrum.meta[i].category == 'annotation' && spectrum.meta[i].value == x[0]) {
-                                annotation = spectrum.meta[i].name;
-                            }
-                        }
-
-                        var intensity = parseFloat(x[1]);
-
-                        if(intensity > spectrum.basePeak) {
-                            spectrum.basePeak = intensity;
-                        }
-
-                        return {
-                            ion: parseFloat(x[0]),
-                            intensity: intensity,
-                            annotation: annotation,
-                            selected: true
-                        }
-                    });
-
-                    // Add an empty metadata field if none exist
-                    if (spectrum.meta.length == 0) {
-                        spectrum.meta.push({name: '', value: ''});
-                    }
-
-                    $scope.$apply(function() {
-                        $scope.spectra.push(spectrum);
-                        $scope.spectraLoaded = 2;
-                        setSpectrum(0);
-                    });
-                }, origin);
-            },
-            function(progress) {}
-        );
-    };
-
+    /**
+     * Perform mass spectrum trimming
+     */
 
     $scope.performIonCuts = function(index) {
         if (angular.isUndefined(index)) {
@@ -132,7 +86,7 @@ moaControllers.CleanSpectraDataController = function ($scope, UploadLibraryServi
             }
         }
 
-        if(angular.isDefined($scope.ionCuts.nIons) && $scope.spectra[index].ions.length - cutIons.length > $scope.ionCuts.nIons) {
+        if(angular.isDefined($scope.ionCuts.nIons) && retainedIons.length > $scope.ionCuts.nIons) {
             retainedIons.sort(function(a, b) {
                 return $scope.spectra[index].ions[b].intensity - $scope.spectra[index].ions[a].intensity;
             });
@@ -156,9 +110,18 @@ moaControllers.CleanSpectraDataController = function ($scope, UploadLibraryServi
     };
 
 
+    /**
+     * Add a new name to the list
+     */
+
     $scope.addName = function() {
         $scope.currentSpectrum.names.push('');
     };
+
+
+    /**
+     * Handle metadata functionality
+     */
 
     $scope.addMetadataField = function() {
         $scope.currentSpectrum.meta.push({name: '', value: ''});
@@ -184,5 +147,140 @@ moaControllers.CleanSpectraDataController = function ($scope, UploadLibraryServi
         $scope.spectraLoaded = 0;
         $scope.spectraIndex = 0;
         $scope.spectra = [];
-    }
+    };
+
+
+    /**
+     * Parse spectra
+     * @param files
+     */
+    $scope.parseFile = function(files) {
+        $scope.spectraLoaded = 1;
+
+        for (var i = 0; i < files.length; i++) {
+            UploadLibraryService.loadSpectraFile(files[i],
+                function (data, origin) {
+                    UploadLibraryService.processData(data, function (spectrum) {
+                        // Create list of ions
+                        spectrum.basePeak = 0;
+
+                        spectrum.ions = spectrum.spectrum.split(' ').map(function (x) {
+                            x = x.split(':');
+                            var annotation = '';
+
+                            for (var i = 0; i < spectrum.meta.length; i++) {
+                                if (spectrum.meta[i].category == 'annotation' && spectrum.meta[i].value == x[0]) {
+                                    annotation = spectrum.meta[i].name;
+                                }
+                            }
+
+                            var intensity = parseFloat(x[1]);
+
+                            if (intensity > spectrum.basePeak) {
+                                spectrum.basePeak = intensity;
+                            }
+
+                            return {
+                                ion: parseFloat(x[0]),
+                                intensity: intensity,
+                                annotation: annotation,
+                                selected: true
+                            }
+                        });
+
+                        // Remove annotations and origin from metadata
+                        spectrum.meta = spectrum.meta.filter(function(metadata) {
+                            if (metadata.name == 'origin') {
+                                spectrum.origin = metadata.name;
+                                return false;
+                            } else {
+                                return angular.isUndefined(metadata.category) || metadata.category != 'annotation';
+                            }
+                        });
+
+                        // Add an empty metadata field if none exist
+                        if (spectrum.meta.length == 0) {
+                            spectrum.meta.push({name: '', value: ''});
+                        }
+
+                        $scope.$apply(function () {
+                            $scope.spectra.push(spectrum);
+                            $scope.spectraLoaded = 2;
+                            setSpectrum(0);
+                        });
+                    }, origin);
+                },
+                function (progress) {}
+            );
+        }
+    };
+
+
+    /**
+     *
+     */
+    $scope.exportFile = function() {
+        var msp = '';
+
+        for (var i = 0; i < $scope.spectra.length; i++) {
+            // Add separator
+            if (i > 0) {
+                msp += '\n\n';
+            }
+
+            // Add names
+            msp += 'Name: '+ ($scope.spectra[i].names.length == 0 ? 'Unknown' : $scope.spectra[i].names[0]) +'\n';
+
+            if (angular.isDefined($scope.spectra[i].inchiKey) && $scope.spectra[i].inchiKey != '') {
+                msp += 'InChIKey: '+ $scope.spectra[i].inchiKey +'\n';
+            }
+
+            if (angular.isDefined($scope.spectra[i].inchi) && $scope.spectra[i].inchi != '') {
+                msp += 'InChI: '+ $scope.spectra[i].inchi +'\n';
+            }
+
+            if (angular.isDefined($scope.spectra[i].smiles) && $scope.spectra[i].smiles != '') {
+                msp += 'SMILES: '+ $scope.spectra[i].smiles +'\n';
+            }
+
+            for (var j = 1; j < $scope.spectra[i].names.length; j++) {
+                msp += 'Synon: '+ $scope.spectra[i].names[j] +'\n';
+            }
+
+            // Add metadata
+            for (var j = 0; j < $scope.spectra[i].meta.length; j++) {
+                msp += $scope.spectra[i].meta[j].name +': '+ $scope.spectra[i].meta[j].value +'\n';
+            }
+
+            // Add mass spectrum
+            var ions = [];
+
+            for (var j = 0; j < $scope.spectra[i].ions.length; j++) {
+                if ($scope.spectra[i].ions[j].selected) {
+                    ions.push($scope.spectra[i].ions[j]);
+                }
+            }
+
+            ions.sort(function(a, b) {
+                return a[0] - b[0];
+            });
+
+            msp += 'Num Peaks: '+ ions.length +'\n';
+
+            for (var j = 0; j < ions.length; j++) {
+                msp += ions[j].ion +' '+ ions[j].intensity + (ions[j].annotation != '' ? ' '+ ions[j].annotation : '') +'\n';
+            }
+        }
+
+        // Export file
+        // http://stackoverflow.com/a/18197341/406772
+        var pom = document.createElement('a');
+        pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(msp));
+        pom.setAttribute('download', 'export.msp');
+        pom.style.display = 'none';
+
+        document.body.appendChild(pom);
+        pom.click();
+        document.body.removeChild(pom);
+    };
 };
