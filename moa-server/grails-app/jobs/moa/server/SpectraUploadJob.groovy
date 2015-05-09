83433a2c2c939c086dac28f0f7eff3abb8c07ef4
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2015-05-09 02:03:03 -0700

    trying to fix mona issues
    Former-commit-id: 1fdd39ba0c3028adbf5212ad49616149a4603754

--- moa-server/grails-app/jobs/moa/server/SpectraUploadJob.groovy
+++ moa-server/grails-app/jobs/moa/server/SpectraUploadJob.groovy
@@ -15,7 +15,7 @@ class SpectraUploadJob {
     /**
      * do we automatically want to resubmit failed jobs
      */
-    def resubmit = false
+    def resubmit = true
 
     /**
      * should this run concurrent over the whole cluster
