f9592cd6ae553533422fdaa02cef4f7cc88ff4e7
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2015-05-14 17:31:53 -0700

    added statistics for time use, etc
    Former-commit-id: 0da1529067891756090622c88dd01323e473c42c

---
+++ moa-client/app/scripts/controllers/statistics/statisticsController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Gert on 5/28/2014.
+ */
+'use strict';
+
+moaControllers.StatisticsController = function ($scope, StatisticsService, $modal,statistics) {
+
+
+    $scope.data = statistics
+};
