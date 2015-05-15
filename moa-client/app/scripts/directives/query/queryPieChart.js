6dfcc81b2511f506db6c608694e03c2b680e6d15
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-05-15 15:21:41 -0700

    added line charts to statistics page
    Former-commit-id: f9cf7368829a75b6d323eba8fe80c595475eea09

--- moa-client/app/scripts/directives/query/queryPieChart.js
+++ moa-client/app/scripts/directives/query/queryPieChart.js
@@ -75,3 +75,55 @@ app.directive('queryPie', function(){
         }
     };
 });
+
+
+app.directive('queryLineChart', function(){
+    return{
+        restrict: 'E',
+        require: 'ngModel',
+
+        replace: 'true',
+        template:
+        '<div style="width: 100%; height: 100%; display: inline-block;">'+
+        '    <div class="statistics-chart" style="width: 100%; height: 100%"></div>'+
+        '</div>',
+
+        link: function(scope, elem, attrs) {
+            var opts  = {
+                //series: {
+                //    pie: {
+                //        show: true
+                //    }
+                //},
+                xaxis: {
+                    mode: "time"
+                },
+                grid: {
+                    hoverable: true
+                }
+            };
+
+            var placeholder = $(elem).find(".statistics-chart");
+            var chart = null;
+
+            scope.$watch(attrs.ngModel, function(v) {
+                if(angular.isDefined(v)) {
+                    var plotData = [];
+
+                    for(var i = 0; i < v.length; i++) {
+                        plotData.push([Date.parse(v[i].date), v[i].count]);
+                    }
+
+                    if(!chart) {
+                        $.plot(placeholder, [plotData], opts);
+                        placeholder.show();
+                    } else {
+                        chart.setData([plotData]);
+                        chart.setupGrid();
+                        chart.draw();
+                    }
+                }
+            }, true);
+        }
+    };
+});
