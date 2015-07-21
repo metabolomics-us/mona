c1a7160fa1b6a6204fbe701f3bf56a01e275138d
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2015-07-21 16:45:58 -0700

    working on documentation and to visualize better how you can get to more spectra information
    Former-commit-id: d55e998bcfc42c8d3bd48bbd4514d42e4a532e83

--- moa-server/grails-app/conf/QuartzConfig.groovy
+++ moa-server/grails-app/conf/QuartzConfig.groovy
@@ -24,12 +24,14 @@ quartz {
         scheduler.skipUpdateCheck = true
         scheduler.instanceName = 'mon'
         scheduler.instanceId = 'AUTO'
-        scheduler.idleWaitTime = 1000
+        scheduler.idleWaitTime = 5000
+        scheduler.makeSchedulerThreadDaemon = true
 
         threadPool.'class' = 'org.quartz.simpl.SimpleThreadPool'
         //use n-2 threads or 2 otherwise
         threadPool.threadCount = Runtime.getRuntime().availableProcessors() > 2 ? Runtime.getRuntime().availableProcessors() -2 : 2
         threadPool.threadPriority = 7
+        threadPool.makeThreadsDaemons = true
 
         jobStore.misfireThreshold = 60000
 
@@ -39,7 +41,9 @@ quartz {
         jobStore.useProperties = false
         jobStore.tablePrefix = 'QRTZ_'
         jobStore.isClustered = true
-        jobStore.clusterCheckinInterval = 1000
+        jobStore.clusterCheckinInterval = 15000
+
+        jobStore.txIsolationLevelSerializable = true
 
         plugin.shutdownhook.'class' = 'org.quartz.plugins.management.ShutdownHookPlugin'
         plugin.shutdownhook.cleanShutdown = true
