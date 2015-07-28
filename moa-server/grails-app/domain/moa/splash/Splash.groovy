426819ca9f68240fea198497c81746e2ec5557ea
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2015-07-28 14:51:08 -0700

    added current splash implementation
    Former-commit-id: d6bcedd33d01828b65a6ba6e35fa7502fe602407

---
+++ moa-server/grails-app/domain/moa/splash/Splash.groovy
@@ -0,0 +1,19 @@
+package moa.splash
+
+import moa.Spectrum
+
+/**
+ * defines the splash, as outlined here
+ *
+ * splash.fiehnlab.ucdavis.edu
+ *
+ */
+class Splash {
+
+    static constraints = {
+    }
+
+    String splash
+
+    Spectrum spectrum
+}
