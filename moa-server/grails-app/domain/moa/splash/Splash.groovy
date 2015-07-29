6e7343dc8f030273f602aae96d83be0b6b844402
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2015-07-29 11:26:02 -0700

    modified splash class a little bit
    Former-commit-id: 724631abc6e798f653f18d8312500536e368b61f

--- moa-server/grails-app/domain/moa/splash/Splash.groovy
+++ moa-server/grails-app/domain/moa/splash/Splash.groovy
@@ -10,10 +10,43 @@ import moa.Spectrum
  */
 class Splash {
 
+
     static constraints = {
     }
 
+    static mapping = {
+        block1 nullable:true
+        block2 nullable:true
+        block3 nullable:true
+        block4 nullable:true
+
+    }
     String splash
 
+    String block1
+
+    String block2
+
+    String block3
+
+    String block4
+
     Spectrum spectrum
+
+    def beforeValidate() {
+
+        if(splash!= null){
+            String[] blocks = splash.split("-");
+
+            if(blocks.size() == 4){
+                block1 = blocks[0]
+                block2 = blocks[1]
+                block3 = blocks[2]
+                block4 = blocks[3]
+            }
+            else{
+                throw new RuntimeException("invalid number of blocks!")
+            }
+        }
+    }
 }
