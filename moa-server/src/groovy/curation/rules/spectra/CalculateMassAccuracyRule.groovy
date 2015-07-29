f260471ff708cf8839c4f2f5b1678fb202fab225
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2015-07-29 11:36:06 -0700

    adjusted tiny ion rule
    Former-commit-id: 688670cc01b67b96dd4405f4fd605aca8c7314f1

--- moa-server/src/groovy/curation/rules/spectra/CalculateMassAccuracyRule.groovy
+++ moa-server/src/groovy/curation/rules/spectra/CalculateMassAccuracyRule.groovy
@@ -1,8 +1,47 @@
 package curation.rules.spectra
 
+import curation.AbstractCurationRule
+import curation.CurationObject
+import edu.ucdavis.fiehnlab.spectra.hash.core.types.SpectraType
+import edu.ucdavis.fiehnlab.spectra.hash.core.util.SplashUtil
+import moa.Spectrum
+import moa.splash.Splash
+
+import javax.sql.DataSource
+
 /**
  * computes the mass accuracy of the spectra,
  * based on the exactmass and theoretical mass
  */
-class CalculateMassAccuracyRule {
+class CalculateMassAccuracyRule extends AbstractCurationRule {
+
+
+    boolean executeRule(CurationObject toValidate) {
+
+        Spectrum spectrum = toValidate.getObjectAsSpectra()
+
+        Splash splash = spectrum.getSplash();
+
+        if(splash == null){
+            splash = new Splash();
+            splash.spectrum = spectrum
+            spectrum.splash = splash
+        }
+
+        splash.splash = (SplashUtil.splash(spectrum.getSpectrum(),SpectraType.MS))
+
+        splash.save()
+
+        return true
+    }
+
+    @Override
+    boolean ruleAppliesToObject(CurationObject toValidate) {
+        return toValidate.isSpectra()
+    }
+
+    @Override
+    String getDescription() {
+        return "calculates the mass accuracy of the given spectra and add's this as metadata field"
+    }
 }
