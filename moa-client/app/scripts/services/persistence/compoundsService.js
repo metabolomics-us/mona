95ab8ec51b1d4bdbd49b01ca88b5ce7ac4ed3f00
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-10-15 16:17:51 -0700

    fixed offset handling in componud broweser for consistency
    Former-commit-id: 05d1cd4cca9918f0dad2b4c398c2facded7139d2

--- moa-client/app/scripts/services/persistence/compoundsService.js
+++ moa-client/app/scripts/services/persistence/compoundsService.js
@@ -3,12 +3,8 @@
  */
 'use strict';
 
-app.factory('Compound', function ($resource, REST_BACKEND_SERVER, MAX_COMPOUNDS, $http) {
-
-    //$http.defaults.useXDomain = true;
-
-    return $resource(
-            REST_BACKEND_SERVER + '/rest/compounds/:id?max='+ MAX_COMPOUNDS +':offset',
+app.factory('Compound', function ($resource, REST_BACKEND_SERVER, MAX_COMPOUNDS) {
+    return $resource(REST_BACKEND_SERVER + '/rest/compounds/:id?max='+ MAX_COMPOUNDS,
         {id: "@id", offset: "@offset"},
         {
             'update': {
