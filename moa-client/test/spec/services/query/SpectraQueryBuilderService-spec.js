46b473136b2b492278f028dcce814f40ab52db7b
sc-bnguyen <bnguyen@stonecobra.com>
2015-10-30 22:36:00 -0700

    saving work
    Former-commit-id: a76f79d9efc43cbc6c276b5abb440e8f64183469

--- moa-client/test/spec/services/query/SpectraQueryBuilderService-spec.js
+++ moa-client/test/spec/services/query/SpectraQueryBuilderService-spec.js
@@ -3,23 +3,30 @@
 describe('service: Spectra Query Builder Service', function() {
   beforeEach(module('moaClientApp'));
 
-  var queryCache,mDataService,specQueryBuilder;
+  var queryCache,mDataService,specQueryBuilder,rootScope;
 
   beforeEach(function() {
-    angular.mock.inject(function(_QueryCache_,_MetadataService_,_SpectraQueryBuilderService_) {
+    angular.mock.inject(function($injector,_QueryCache_,_MetadataService_,_SpectraQueryBuilderService_) {
       queryCache = _QueryCache_;
       mDataService = _MetadataService_;
       specQueryBuilder = _SpectraQueryBuilderService_;
+      rootScope = $injector.get('$rootScope');
+
+      var query = {
+        id: [1],
+        compound: {test: 'test'},
+        metadata: [],
+        tags: ['testTag','testTag2']
+      };
+      queryCache.setSpectraQuery(query);
+
     });
   });
 
+  var hash = 'splash10-dz40000000-9ff3eaf3411278ba18fd';
+  var spectra = 'test Spectra';
+
   //mock queries
-  var query = {
-    id: [1],
-    compound: {test: 'test'},
-    metadata: [],
-    tags: ['testTag','testTag2']
-  };
 
   it('returns a Query Object', function() {
     var q = specQueryBuilder.getQuery();
@@ -29,13 +36,19 @@ describe('service: Spectra Query Builder Service', function() {
   });
 
   it('complies a query', function() {
+    var query = {
+      id: [1],
+      compound: {test: 'test'},
+      metadata: [],
+      tags: ['testTag','testTag2']
+    };
+
     var qToCompile = specQueryBuilder.compileQuery(query,'sampleTag');
     var compiled = queryCache.getSpectraQuery();
     expect(qToCompile).toEqual(compiled);
   });
 
   it('removes Tags from Queries', function() {
-    queryCache.setSpectraQuery(query);
     specQueryBuilder.removeTagFromQuery('testTag');
     var qWithCleanedTags = queryCache.getSpectraQuery().tags;
     expect(qWithCleanedTags).toEqual(['testTag2']);
@@ -56,9 +69,9 @@ describe('service: Spectra Query Builder Service', function() {
   });
 
   it('adds Spectra Id to Query', function() {
-    queryCache.setSpectraQuery(query);
+    //queryCache.setSpectraQuery(query);
     specQueryBuilder.addSpectraIdToQuery(2);
-    expect(query.id[1]).toEqual(2);
+    expect(queryCache.getSpectraQuery().id[1]).toEqual(2);
   });
 
   it('creates a Spectra Id array when adding id to Queries with no Spectra Ids', function() {
@@ -67,4 +80,61 @@ describe('service: Spectra Query Builder Service', function() {
     specQueryBuilder.addSpectraIdToQuery(2);
     expect(q.id[0]).toEqual(2);
   });
+
+  it('validates a spectra and add similar spectra to a query', function () {
+    var query = {};
+    query.match = {};
+    query.match.spectra = '';
+    queryCache.setSpectraQuery(query);
+    rootScope.spectra = spectra;
+    specQueryBuilder.addSimilarSpectraToQuery('1234','test Spectra');
+    var qWithMatchingSpectra = queryCache.getSpectraQuery().match.spectra;
+    expect(qWithMatchingSpectra).toEqual('test Spectra');
+  });
+
+  it('adds a matching histogram to query', function() {
+    specQueryBuilder.addMatchingHistogramToQuery(hash);
+    var res = queryCache.getSpectraQuery();
+    expect(res.match.histogram).toEqual('dz40000000');
+  });
+
+  it('adds exact spectra search to query', function() {
+    specQueryBuilder.addExactSpectraSearchToQuery(hash);
+    var res = queryCache.getSpectraQuery();
+    expect(res.match.exact).toEqual(hash);
+  });
+
+  it('adds top 10 ions search to query', function () {
+    specQueryBuilder.addTop10IonsSearchToQuery(hash);
+    var res = queryCache.getSpectraQuery();
+    expect(res.match.top10).toEqual(hash);
+  });
+
+  it('removes a spectra id from query', function() {
+    specQueryBuilder.removeSpectraIdFromQuery(1);
+    var res = queryCache.getSpectraQuery();
+    expect(res.id.length).toEqual(0);
+  });
+
+  it('adds a tag to query', function() {
+    var tag = 'massbank';
+    specQueryBuilder.addTagToQuery(tag);
+    var res = queryCache.getSpectraQuery();
+    expect(res.tags[2].name.eq).toEqual(tag);
+  });
+
+  it('adds a tag that is of a compound type', function() {
+    var compoundTag = 'water';
+    specQueryBuilder.addTagToQuery(compoundTag,true);
+    var res = queryCache.getSpectraQuery();
+    //console.log(res);
+    //expect(res.compounds.tags[0]).toEqual(compoundTag);
+
+
+  });
+
+
+
+
+
 });
