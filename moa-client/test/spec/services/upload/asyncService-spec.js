a409072285cee6bf4d31e8d75bd446d19fcd5f26
sc-bnguyen <bnguyen@stonecobra.com>
2015-11-02 21:01:34 -0800

    saving work
    Former-commit-id: 410a86817812ec0240e820050c2abd1bd3f51f42

---
+++ moa-client/test/spec/services/upload/asyncService-spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+describe('service: Async Service', function() {
+  beforeEach(module('moaClientApp'));
+
+  var ASyncService,ApplicationError,log,q,interval;
+
+  beforeEach(function() {
+    angular.mock.inject(function($injector,_ApplicationError_,_AsyncService_) {
+      ApplicationError = _ApplicationError_;
+      ASyncService = _AsyncService_;
+      log = $injector.get('$log');
+      q = $injector.get('$q');
+      interval = $injector.get('$interval');
+    });
+  });
+
+  var data = {"name": "ABI Chem",
+    "value": {
+      "eq": "AC1Q2J5R"
+    }};
+
+  var f = function(data) {return data};
+
+  it('adds a function and object to pool', function() {
+    ASyncService.addToPool(f,data);
+    expect(ASyncService.hasPooledTasks()).toEqual(true);
+  });
+
+  it('resets the pool', function () {
+    ASyncService.resetPool();
+    expect(ASyncService.hasPooledTasks()).toEqual(false);
+  });
+
+  it('starts the pool', function() {
+    ASyncService.addToPool(f,data);
+    //var test = ASyncService.returnTimeout();
+    //console.log(test);
+  });
+
+
+});
