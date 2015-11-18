947500da10b3cba4e7f6ea4af14ac5eb0dab9b32
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-11-18 11:11:15 -0800

    query update service and job
    Former-commit-id: 5d5dfa0f4c56867ea956daa6277f0cc885582bda

---
+++ moa-server/grails-app/jobs/moa/server/UpdatePredefinedQueryCountJob.groovy
@@ -0,0 +1,24 @@
+package moa.server
+
+import moa.server.query.PredefinedQueryService
+
+/**
+ * Created by sajjan on 10/1/15.
+ */
+class UpdatePredefinedQueryCountJob {
+    def concurrent = false
+
+    def group = "update"
+
+    def description = "schedules repository dumps"
+
+    static triggers = {
+        cron name: 'updateQueryCounts', cronExpression: '0 0 0 * * ?', priority: 10
+    }
+
+    PredefinedQueryService predefinedQueryService
+
+    def execute() {
+        predefinedQueryService.updateQueryCounts()
+    }
+}
