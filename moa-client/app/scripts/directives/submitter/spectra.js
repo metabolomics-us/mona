aac51c989cf0f160724dc76a952b36cd01065ef3
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-11-30 11:40:52 -0800

    small edits to hall of fame
    Former-commit-id: c5ee40279a605e043fd6bc7dd0858112c0f595e2

--- moa-client/app/scripts/directives/submitter/spectra.js
+++ moa-client/app/scripts/directives/submitter/spectra.js
@@ -2,22 +2,22 @@
 /**
  * provides us with some feedback how many spectra a certain person uploaded
  */
-app.directive('spectraCountForUser', function($compile,StatisticsService){
+app.directive('spectraCountForUser', function($compile, StatisticsService){
     return {
         replace: true,
+
         scope: {
             user: '=user'
         },
 
         controller: function ($scope) {
-
-            $scope.spectraCount = "loading...";
-
+            $scope.spectraCount = "Loading...";
         },
-        link: function ($scope, element, attrs, ngModel) {
-            StatisticsService.spectraCount({id:$scope.user.id},
-                function(data){
-                    $scope.spectraCount = data.count;
+
+        link: function (scope, element, attrs, ngModel) {
+            StatisticsService.spectraCount({id: scope.user.id},
+                function(data) {
+                    scope.spectraCount = data.count;
                 }
             );
         },
@@ -26,20 +26,20 @@ app.directive('spectraCountForUser', function($compile,StatisticsService){
     }
 });
 
-app.directive('spectraScoreForUser', function($compile,StatisticsService){
+app.directive('spectraScoreForUser', function($compile, StatisticsService){
     return {
         replace: true,
+
         scope: {
             user: '=user'
         },
 
-        controller: function ($scope) {
+        controller: function ($scope) {},
 
-        },
-        link: function ($scope, element, attrs, ngModel) {
-            StatisticsService.spectraScore({id:$scope.user.id},
-                function(data){
-                    $scope.score = data.score;
+        link: function (scope, element, attrs, ngModel) {
+            StatisticsService.spectraScore({id: scope.user.id},
+                function(data) {
+                    scope.score = data.score;
                 }
             );
         },
@@ -48,26 +48,26 @@ app.directive('spectraScoreForUser', function($compile,StatisticsService){
     }
 });
 
-app.directive('spectraTopScoresForUsers', function($compile,StatisticsService, Submitter){
+app.directive('spectraTopScoresForUsers', function($compile, StatisticsService, Submitter){
     return {
         replace: true,
 
         scope: {
             limit: '@'
         },
-        controller: function ($scope) {
 
-        },
-        link: function ($scope, element, attrs, ngModel) {
-            StatisticsService.spectraTopScores({max:$scope.limit},
-                function(data){
+        controller: function ($scope) {},
+
+        link: function (scope, element, attrs, ngModel) {
+            StatisticsService.spectraTopScores({max: scope.limit},
+                function(data) {
                     var scores = data;
 
-                    angular.forEach(scores,function(score){
-                        score.submitter = Submitter.get({id:score.submitter});
+                    angular.forEach(scores,function(score) {
+                        score.submitter = Submitter.get({id: score.submitter});
                     });
-                    $scope.scores = scores;
 
+                    scope.scores = scores;
                 }
             );
         },
