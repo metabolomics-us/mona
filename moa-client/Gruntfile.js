0371cf93b055a60923bdc7ad570a34e8fb4c3d6c
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-11-30 10:49:37 -0800

    temporarily removed renaming of images in grunt rev
    Former-commit-id: 8c40afb05ba4d03285eb0457181dbce365f6e0f3

--- moa-client/Gruntfile.js
+++ moa-client/Gruntfile.js
@@ -222,7 +222,8 @@ module.exports = function (grunt) {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
                         '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                        // Temporary measure until image references are removed from angular controller
+                        //'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
                         '<%= yeoman.dist %>/styles/fonts/*'
                     ]
                 }
