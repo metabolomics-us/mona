f92711b26095f16a5875a78ea7c3dd4f34a872e4
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2015-12-02 14:08:27 -0800

    updated directive tests and dependencies for karma
    Former-commit-id: c0b507e921c9637ac8e4711377df9eea6b2fd5dd

--- moa-client/test/spec/directives/spectra/spectraPanelDirective-spec.js
+++ moa-client/test/spec/directives/spectra/spectraPanelDirective-spec.js
@@ -1,8 +1,26 @@
 'use strict'
 
 describe('Directive: Spectra Panel Directive', function() {
-    beforeEach(module('moaClientApp'));
+    beforeEach(module('moaClientApp'), function($httpProvider) {
+        $httpProvider.interceptors.push('moaClientApp');
+    });
 
-    var scope,spectraPanel;
-    
+    beforeEach(module('views/spectra/display/template/panel.html'));
+
+    var scope,element,httpBackend;
+
+    beforeEach(inject(function($rootScope,$compile,$injector) {
+        httpBackend = $injector.get('$httpBackend');
+        httpBackend.expectGET('views/main.html').respond(200);
+        scope = $rootScope.$new();
+        element = angular.element("<display-spectra-panel></display-spectra-panel>");
+        $compile(element)(scope);
+        httpBackend.flush();
+        scope.$digest();
+
+    }));
+
+    it('should load the template', function() {
+        expect(element.length).toBe(1);
+    });
 });
