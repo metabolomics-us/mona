e67d6af39eea5ccdac887a67f80b287580d78ddd
sc-bnguyen <bnguyen@stonecobra.com>
2015-12-06 22:07:15 -0800

    factored filter from controller, note spectraDBIndex controller not working after refactoring, working to fix
    Former-commit-id: bf0e66ea37ae78f76fc1b39762533d56ada5229a

--- moa-client/app/scripts/controllers/browser/spectraDatabaseIndexController.js
+++ moa-client/app/scripts/controllers/browser/spectraDatabaseIndexController.js
@@ -1,115 +1,103 @@
 /**
  * Created by sajjan on 11/6/14.
  */
-'use strict';
+(function() {
+    'use strict';
 
-moaControllers.SpectraDatabaseIndexController = function($scope, $http, $location, SpectraQueryBuilderService, MetadataService, REST_BACKEND_SERVER) {
-    /**
-     * Metadata that we wish to display
-     * @type {string[]}
-     */
-    $scope.fields = [
-        'instrument type',
-        'ms type',
-        'ion mode',
-        'derivative type',
-        'precursor type'
-    ];
+    moaControllers.SpectraDatabaseIndexController = ['$scope', '$http', '$location', 'SpectraQueryBuilderService', 'MetadataService', 'REST_BACKEND_SERVER',
+        function($scope, $http, $location, SpectraQueryBuilderService, MetadataService, REST_BACKEND_SERVER) {
+            /**
+             * Metadata that we wish to display
+             * @type {string[]}
+             */
+            $scope.fields = [
+                'instrument type',
+                'ms type',
+                'ion mode',
+                'derivative type',
+                'precursor type'
+            ];
 
-    /**
-     * Loaded unique metadata values
-     * @type {{}}
-     */
-    $scope.fieldData = {};
+            /**
+             * Loaded unique metadata values
+             * @type {{}}
+             */
+            $scope.fieldData = {};
 
-    /**
-     *
-     * @type {{}}
-     */
-    $scope.totalData = {};
-
-
-
-    /**
-     * Query all metadata values for a given metadata name
-     * @param id
-     */
-    var queryMetadataValues = function(id) {
-        $http.get(REST_BACKEND_SERVER + '/rest/statistics/meta/spectra/count/'+ id)
-            .success(function(data) {
-                if(data.length > 0) {
-                    $scope.fieldData[data[0].name] = data;
-                }
-            });
-    };
-
-    /**
-     * Query for total statistics
-     */
-    var queryTotalStatistics = function() {
-        return $http.get(REST_BACKEND_SERVER + '/rest/statistics/countAll/')
-            .success(function(data) {
-                $scope.totalData = data;
-            });
-    };
+            /**
+             *
+             * @type {{}}
+             */
+            $scope.totalData = {};
 
 
-    /**
-     * Submit query from clicked metadata link
-     * @param name
-     * @param value
-     */
-    $scope.submitQuery = function(name, value) {
-        var query = {};
-        query[name] = value;
+            /**
+             * Query all metadata values for a given metadata name
+             * @param id
+             */
+            var queryMetadataValues = function(id) {
+                $http.get(REST_BACKEND_SERVER + '/rest/statistics/meta/spectra/count/' + id)
+                  .success(function(data) {
+                      if (data.length > 0) {
+                          $scope.fieldData[data[0].name] = data;
+                      }
+                  });
+            };
 
-        SpectraQueryBuilderService.compileQuery(query);
-        $location.path("/spectra/browse/");
-    };
+            /**
+             * Query for total statistics
+             */
+            var queryTotalStatistics = function() {
+                return $http.get(REST_BACKEND_SERVER + '/rest/statistics/countAll/')
+                  .success(function(data) {
+                      $scope.totalData = data;
+                  });
+            };
 
 
-    /**
-     * initialization and population of metadata values
-     */
-    (function list() {
-        //MetadataService.metadata(
-        //    function(data) {
-        //        for(var i = 0; i < data.length; i++) {
-        //            for(var j = 0; j < $scope.fields.length; j++) {
-        //                if(data[i].name == $scope.fields[j]) {
-        //                    queryMetadataValues(data[i].id);
-        //                    break;
-        //                }
-        //            }
-        //        }
-        //    },
-        //    function (error) {
-        //        $log.error('metadata failed: ' + error);
-        //    }
-        //);
+            /**
+             * Submit query from clicked metadata link
+             * @param name
+             * @param value
+             */
+            $scope.submitQuery = function(name, value) {
+                var query = {};
+                query[name] = value;
 
-        // Temporary fix
-        queryMetadataValues(112); // ion mode
-        queryMetadataValues(107); // instrument type
-        queryMetadataValues(676); // ms type
-        queryMetadataValues(117); // precursor type
-        queryMetadataValues(2079592); // derivative type
+                SpectraQueryBuilderService.compileQuery(query);
+                $location.path("/spectra/browse/");
+            };
 
-        queryTotalStatistics();
-    })();
-};
 
-app.filter('titlecase', function() {
-    return function(s) {
-        s = ( angular.isUndefined(s) || s === null ) ? '' : s.toString();
+            /**
+             * initialization and population of metadata values
+             */
+            (function list() {
+                //MetadataService.metadata(
+                //    function(data) {
+                //        for(var i = 0; i < data.length; i++) {
+                //            for(var j = 0; j < $scope.fields.length; j++) {
+                //                if(data[i].name == $scope.fields[j]) {
+                //                    queryMetadataValues(data[i].id);
+                //                    break;
+                //                }
+                //            }
+                //        }
+                //    },
+                //    function (error) {
+                //        $log.error('metadata failed: ' + error);
+                //    }
+                //);
 
-        if(s.toUpperCase() === s) {
-            return s;
-        } else {
-            return s.toLowerCase().replace(/\b([a-z])/g, function (ch) {
-                return ch.toUpperCase();
-            });
-        }
-    };
-});
+                // Temporary fix
+                queryMetadataValues(112); // ion mode
+                queryMetadataValues(107); // instrument type
+                queryMetadataValues(676); // ms type
+                queryMetadataValues(117); // precursor type
+                queryMetadataValues(2079592); // derivative type
+
+                queryTotalStatistics();
+            })();
+        }];
+})();
 
