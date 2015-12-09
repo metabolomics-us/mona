9b3d7a54b80cd1b70ac3b8424565289387f6ebe7
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2015-12-09 13:14:30 -0800

    refactored remaining directives and filters
    Former-commit-id: 7067f104fbccc183716c2a78bfc8479b7ab19c0d

--- moa-client/app/scripts/directives/statisticsDirective.js
+++ moa-client/app/scripts/directives/statisticsDirective.js
@@ -2,66 +2,80 @@
  * Created by wohlgemuth on 5/14/15.
  */
 
-app.directive('statistics', function ($compile, $filter, StatisticsService, $log) {
-    return {
-        //must be an attribute
-        replace: false,
-        scope: {
-            timeframe: '@',
-            query: '@'
-        },
 
-        /**
-         * watches for changes and is used to modify the query terms on the fly
-         * @param $scope
-         * @param QueryCache
-         * @param $log
-         * @param $rootScope
-         */
-        controller: function ($scope) {
+(function() {
+    'use strict';
 
-            $scope.executionTime = "loading...";
-            $scope.unit = "ms";
+    angular.module('moaClientApp')
+      .directive('statistics', statistics);
 
-        },
+    statistics.$inject = ['StatisticsService'];
 
-        //decorate our elements based on there properties
-        link: function ($scope, element, attrs, ngModel) {
+    function statistics($compile, $filter, StatisticsService, $log) {
+        var directive = {
+            restrict: 'AE',
+            replace: false,
+            template: '<span>{{executionTime | number:0}} {{unit}} over the last {{timeframe}}</span>',
+            scope: {
+                timeframe: '@',
+                query: '@'
+            },
 
-            StatisticsService.executionTime({time:$scope.timeframe,method:$scope.query,max:1},
-                function(data){
-                    if(data.length) {
-                        $scope.executionTime = data[0].avg;
+            /**
+             * watches for changes and is used to modify the query terms on the fly
+             * @param $scope
+             * @param QueryCache
+             * @param $log
+             * @param $rootScope
+             */
+            controller: statisticsController,
 
-                        if ($scope.executionTime > 1000) {
-                            $scope.executionTime = $scope.executionTime / 1000;
-                            $scope.unit = "s";
+            //decorate our elements based on there properties
+            link: function($scope, element, attrs, ngModel) {
 
+                StatisticsService.executionTime({time: $scope.timeframe, method: $scope.query, max: 1},
+                  function(data) {
+                      if (data.length) {
+                          $scope.executionTime = data[0].avg;
 
-                            if ($scope.executionTime > 90) {
-                                $scope.executionTime = $scope.executionTime / 60;
-                                $scope.unit = "min";
-
-
-                                if ($scope.executionTime > 90) {
-                                    $scope.executionTime = $scope.executionTime / 60;
-                                    $scope.unit = "h";
+                          if ($scope.executionTime > 1000) {
+                              $scope.executionTime = $scope.executionTime / 1000;
+                              $scope.unit = "s";
 
 
-                                    if ($scope.executionTime > 24) {
-                                        $scope.executionTime = $scope.executionTime / 24;
-                                        $scope.unit = "d";
-                                    }
-                                }
-                            }
-                        }
-                    } else {
-                        $scope.executionTime = 0;
-                    }
-                }
-            );
-        },
+                              if ($scope.executionTime > 90) {
+                                  $scope.executionTime = $scope.executionTime / 60;
+                                  $scope.unit = "min";
 
-        template: '<span>{{executionTime | number:0}} {{unit}} over the last {{timeframe}}</span>'
+
+                                  if ($scope.executionTime > 90) {
+                                      $scope.executionTime = $scope.executionTime / 60;
+                                      $scope.unit = "h";
+
+
+                                      if ($scope.executionTime > 24) {
+                                          $scope.executionTime = $scope.executionTime / 24;
+                                          $scope.unit = "d";
+                                      }
+                                  }
+                              }
+                          }
+                      } else {
+                          $scope.executionTime = 0;
+                      }
+                  }
+                );
+            }
+        };
+
+        return directive;
     }
-});
+
+    statisticsController.$inject = ['$scope'];
+
+    function statisticsController($scope) {
+        $scope.executionTime = "loading...";
+        $scope.unit = "ms";
+    }
+
+})();
--- moa-client/app/scripts/filters/titleCaseFilter.js
+++ moa-client/app/scripts/filters/titleCaseFilter.js
@@ -1,17 +1,18 @@
 (function() {
     'use strict';
 
-    app.filter('titlecase', function() {
-        return function(s) {
-            s = ( angular.isUndefined(s) || s === null ) ? '' : s.toString();
+    angular.module('moaClientApp')
+      .filter('titlecase', function() {
+          return function(s) {
+              s = ( angular.isUndefined(s) || s === null ) ? '' : s.toString();
 
-            if(s.toUpperCase() === s) {
-                return s;
-            } else {
-                return s.toLowerCase().replace(/\b([a-z])/g, function (ch) {
-                    return ch.toUpperCase();
-                });
-            }
-        };
-    });
+              if (s.toUpperCase() === s) {
+                  return s;
+              } else {
+                  return s.toLowerCase().replace(/\b([a-z])/g, function(ch) {
+                      return ch.toUpperCase();
+                  });
+              }
+          };
+      });
 })();
