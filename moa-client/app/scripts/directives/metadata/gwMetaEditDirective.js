926bbe88e415182d306eb0e562523eb49fd151e1
sc-bnguyen <bnguyen@stonecobra.com>
2015-12-08 16:45:18 -0800

    refactored metadata directives
    Former-commit-id: 723a7546e67f851f258061d3b8182258f253a793

---
+++ moa-client/app/scripts/directives/metadata/gwMetaEditDirective.js
@@ -0,0 +1,69 @@
+/**
+ * allows us to easily modify meta data on the fly
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('moaClientApp')
+      .directive('gwMetaEdit', gwMetaEdit);
+
+
+    function gwMetaEdit() {
+        var directive = {
+            restrict: 'A',
+            templateUrl: '/views/templates/meta/editMeta.html',
+            replace: true,
+            transclude: true,
+            scope: {
+                value: '=value'
+            },
+            link: linkFunc,
+            controller: gwMetaEditController
+        };
+
+        return directive;
+    }
+
+
+    function linkFunc($scope, element, attrs, ngModel) {
+
+    }
+
+    //controller to handle building new queries
+    gwMetaEditController.$inject = ['$scope', '$element', 'MetaData', '$filter'];
+
+    function gwMetaEditController($scope, $element, MetaData, $filter) {
+
+        //receive a click
+        $scope.hide = function() {
+
+            MetaData.get({id: $scope.value.metaDataId}, function(value) {
+
+                value.hidden = true;
+
+                console.log($filter('json')(value));
+
+                value.$update(function() {
+                    $scope.value.hidden = true;
+
+                });
+            });
+        };
+
+        //receive a click
+        $scope.unhide = function() {
+
+            MetaData.get({id: $scope.value.metaDataId}, function(value) {
+
+                value.hidden = false;
+
+                console.log($filter('json')(value));
+
+                value.$update(function() {
+                    $scope.value.hidden = false;
+                });
+            });
+        };
+    }
+})();
---
+++ moa-client/app/scripts/directives/metadata/gwSpectraIdQueryDirective.js
@@ -0,0 +1,73 @@
+/**
+ * adds the given id to the query or removes it
+ */
+
+(function() {
+    'use strict';
+
+    angular.module('moaClientApp')
+      .directive('gwSpectraIdQuery', gwSpectraIdQuery)
+
+    function gwSpectraIdQuery() {
+        var directive = {
+            restrict: 'A',
+            templateUrl: '/views/templates/spectra/spectraHashQuery.html',
+            replace: true,
+            transclude: true,
+            scope: {
+                value: '=value'
+            },
+            link: linkFunc,
+            controller: gwSpectraIdQueryController
+        };
+
+        return directive;
+    }
+
+    function linkFunc($scope, element, attrs, ngModel) {
+
+    }
+
+    //controller to handle building new queries
+    gwSpectraIdQueryController.$inject = ['$scope', '$element', 'SpectraQueryBuilderService', '$location'];
+
+    function gwSpectraIdQueryController($scope, $element, SpectraQueryBuilderService, $location) {
+
+        //receive a click
+        $scope.newQuery = function() {
+            //build a mona query based on this label
+            SpectraQueryBuilderService.prepareQuery();
+
+            //add it to query
+            SpectraQueryBuilderService.addSpectraIdToQuery($scope.value.hash);
+
+            //assign to the cache
+
+            //run the query and show it's result in the spectra browser
+            $location.path("/spectra/browse/");
+        };
+
+        //receive a click
+        $scope.addToQuery = function() {
+            SpectraQueryBuilderService.addSpectraIdToQuery($scope.value.hash);
+            $location.path("/spectra/browse/");
+        };
+
+        //finds related spectra to this spectra
+        $scope.findSimilarSpectra = function() {
+            SpectraQueryBuilderService.prepareQuery();
+
+            SpectraQueryBuilderService.addSimilarSpectraToQuery($scope.value.hash, $scope.value.spectrum);
+            $location.path("/spectra/browse/");
+        };
+
+        //receive a click
+        $scope.removeFromQuery = function() {
+            //build a mona query based on this label
+            SpectraQueryBuilderService.removeSpectraIdFromQuery($scope.value.hash);
+
+            //run the query and show it's result in the spectra browser
+            $location.path("/spectra/browse/");
+        };
+    }
+})();
