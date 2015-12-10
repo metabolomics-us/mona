08c45bd26dcf6b9e9fe055b8dfb165fd6db0f16c
sc-bnguyen <bnguyen@stonecobra.com>
2015-12-09 21:32:38 -0800

    saving work on optimization
    Former-commit-id: 04a30162c8145c460ccdf5e8d2ebb18b0850a61d

--- moa-client/app/scripts/directives/statisticsDirective.js
+++ moa-client/app/scripts/directives/statisticsDirective.js
@@ -2,87 +2,6 @@
  * Created by wohlgemuth on 5/14/15.
  */
 
-
-(function() {
-    'use strict';
-
-    angular.module('moaClientApp')
-      .directive('statistics', statistics);
-
-    statistics.$inject = ['$compile','$filter','StatisticsService'];
-
-    function statistics($compile, $filter, StatisticsService, $log) {
-        var directive = {
-            restrict: 'AE',
-            replace: false,
-            template: '<span>{{executionTime | number:0}} {{unit}} over the last {{timeframe}}</span>',
-            scope: {
-                timeframe: '@',
-                query: '@'
-            },
-
-            /**
-             * watches for changes and is used to modify the query terms on the fly
-             * @param $scope
-             * @param QueryCache
-             * @param $log
-             * @param $rootScope
-             */
-            controller: statisticsController,
-
-            //decorate our elements based on there properties
-            link: function($scope, element, attrs, ngModel) {
-
-                StatisticsService.executionTime({time: $scope.timeframe, method: $scope.query, max: 1},
-                  function(data) {
-                      if (data.length) {
-                          $scope.executionTime = data[0].avg;
-
-                          if ($scope.executionTime > 1000) {
-                              $scope.executionTime = $scope.executionTime / 1000;
-                              $scope.unit = "s";
-
-
-                              if ($scope.executionTime > 90) {
-                                  $scope.executionTime = $scope.executionTime / 60;
-                                  $scope.unit = "min";
-
-
-                                  if ($scope.executionTime > 90) {
-                                      $scope.executionTime = $scope.executionTime / 60;
-                                      $scope.unit = "h";
-
-
-                                      if ($scope.executionTime > 24) {
-                                          $scope.executionTime = $scope.executionTime / 24;
-                                          $scope.unit = "d";
-                                      }
-                                  }
-                              }
-                          }
-                      } else {
-                          $scope.executionTime = 0;
-                      }
-                  }
-                );
-            }
-        };
-
-        return directive;
-    }
-
-    statisticsController.$inject = ['$scope'];
-
-    function statisticsController($scope) {
-        $scope.executionTime = "loading...";
-        $scope.unit = "ms";
-    }
-
-})();
-
-/*
-    **** refactored code using bindToController instead of linkFunction. Performance is slow compared to Mona site, it's not
-    * because of removal of linkFunc, but of refactoring the whole application. Looking into why.
 (function() {
     'use strict';
 
@@ -100,10 +19,7 @@
             },
             controller: statisticsController,
             controllerAs: 'ctrl',
-
-
-             // bind scope to variable, we no longer need $compile or link function, and we can input data into template
-
+            // bind scope to variable, we no longer need $compile or link function, and we can input data into template
             bindToController: true
         };
 
@@ -117,7 +33,18 @@
         ctrl.executionTime = "loading...";
         ctrl.unit = "ms";
 
-        StatisticsService.executionTime({time: this.timeframe, method: this.query, max: 1},
+        ctrl.test = null;
+
+
+        getExecutionTime();
+
+        function getExecutionTime() {
+            return StatisticsService.executionTime({time: ctrl.timeframe, method: ctrl.query, max: 1}, function(data) {
+                ctrl.test = data[0].avg;
+            })
+        }
+
+        StatisticsService.executionTime({time: ctrl.timeframe, method: ctrl.query, max: 1},
           function(data) {
               if (data.length) {
                   ctrl.executionTime = data[0].avg;
@@ -151,4 +78,85 @@
         );
     }
 })();
-  */
+
+
+/*
+ (function() {
+ 'use strict';
+
+ angular.module('moaClientApp')
+ .directive('statistics', statistics);
+
+ statistics.$inject = ['$compile', '$filter', 'StatisticsService'];
+
+ function statistics($compile, $filter, StatisticsService, $log) {
+ var directive = {
+ restrict: 'AE',
+ replace: false,
+ template: '<span>{{executionTime | number:0}} {{unit}} over the last {{timeframe}}</span>',
+ scope: {
+ timeframe: '@',
+ query: '@'
+ },
+
+ /**
+ * watches for changes and is used to modify the query terms on the fly
+ * @param $scope
+ * @param QueryCache
+ * @param $log
+ * @param $rootScope
+
+ controller: statisticsController,
+
+ //decorate our elements based on there properties
+ link: function($scope, element, attrs, ngModel) {
+
+ StatisticsService.executionTime({time: $scope.timeframe, method: $scope.query, max: 1},
+ function(data) {
+ if (data.length) {
+ $scope.executionTime = data[0].avg;
+
+ if ($scope.executionTime > 1000) {
+ $scope.executionTime = $scope.executionTime / 1000;
+ $scope.unit = "s";
+
+
+ if ($scope.executionTime > 90) {
+ $scope.executionTime = $scope.executionTime / 60;
+ $scope.unit = "min";
+
+
+ if ($scope.executionTime > 90) {
+ $scope.executionTime = $scope.executionTime / 60;
+ $scope.unit = "h";
+
+
+ if ($scope.executionTime > 24) {
+ $scope.executionTime = $scope.executionTime / 24;
+ $scope.unit = "d";
+ }
+ }
+ }
+ }
+ } else {
+ $scope.executionTime = 0;
+ }
+ }
+ );
+ }
+ };
+
+ return directive;
+ }
+
+ statisticsController.$inject = ['$scope'];
+
+ function statisticsController($scope) {
+ $scope.executionTime = "loading...";
+ $scope.unit = "ms";
+ }
+
+ })();
+ */
+
+
