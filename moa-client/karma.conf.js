a5415d0adea492103db3d185a902e04d4eb8829f
benn7 <bdnguyen7@gmail.com>
2015-12-22 11:40:03 -0800

    revert to PhantomJS, v2 was breaking tests, cleaned karma.conf
    Former-commit-id: 721fafb96da5475af9ecfba2cc3d4c4f1ddc8798

--- moa-client/karma.conf.js
+++ moa-client/karma.conf.js
@@ -1,70 +1,53 @@
 // Karma configuration
-// Generated on Wed Jun 11 2014 20:13:48 GMT-0700 (PDT)
+// Generated on Tue Dec 22 2015 08:07:34 GMT-0800 (Pacific Standard Time)
 
 module.exports = function(config) {
     config.set({
 
-        // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
-
-
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
 
-
-        // list of files / patterns to load in the browser
         files: [
             'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-jquery/dist/angular-jquery.js',
+
+            // moaClientApp modules
+            'app/bower_components/angular-mocks/angular-mocks.js',
             'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-resource/angular-resource.js',
             'app/bower_components/angular-cookies/angular-cookies.js',
+            'app/bower_components/angular-animate/angular-animate.js',
             'app/bower_components/angular-sanitize/angular-sanitize.js',
+            'app/bower_components/angular-bootstrap/ui-bootstrap.js',
+            'app/bower_components/angular-filter/dist/angular-filter.js',
             'app/bower_components/angular-dialog-service/dist/dialogs.js',
             'app/bower_components/angular-dialog-service/dist/dialogs-default-translations.js',
+            'app/bower_components/ng-tags-input/ng-tags-input.js',
             'app/bower_components/angular-msp-parser/service.js',
             'app/bower_components/angular-mgf-parser/service.js',
             'app/bower_components/angular-massbank-parser/service.js',
-            'app/bower_components/angular-animate/angular-animate.js',
             'app/bower_components/angular-cts-service/service.js',
+            'app/bower_components/ng-file-upload/angular-file-upload.js',
             'app/bower_components/angular-masspec-plotter/angular-masspec-plotter.js',
             'app/bower_components/ngInfiniteScroll/build/ng-infinite-scroll.js',
-            'app/bower_components/ng-file-upload/angular-file-upload.js',
             'app/bower_components/angular-bootstrap-affix/dist/angular-bootstrap-affix.js',
-            'app/bower_components/angular-jquery/dist/angular-jquery.js',
             'app/bower_components/angular-translate/angular-translate.js',
-            'app/bower_components/angular-resource/angular-resource.js',
-            'app/bower_components/angular-mocks/angular-mocks.js',
-            'app/bower_components/angular-bootstrap/ui-bootstrap.js',
-            'app/bower_components/ng-tags-input/ng-tags-input.js',
-            //'app/views/spectra/display/template/*.html'
+
+            // source and test files
             'app/scripts/**/*.js',
-            //'test/spec/**/*.js'
-            'test/spec/controllers/authentication/*.js'
+            'test/spec/app-spec.js'
+            //'test/spec/controllers/authentication/authenticationController-spec.js'
         ],
-
-
-        // list of files to exclude
-        exclude: [
-            'app/scripts/directives/metadata/metadataValueDirective.js',
-            'app/scripts/directives/metadata/tagDirective.js',
-            'app/scripts/directives/query/queryPieChart.js',
-            'app/scripts/directives/submitter/spectra.js',
-            'app/scripts/theme.js'
-        ],
+        exclude: ['app/scripts/theme.js'],
 
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
         preprocessors: {
-            //'app/views/spectra/display/template/*.html' : 'ng-html2js',
-            //'**/scripts/**/*.js' : 'coverage'
             '**/scripts/controllers/**/*.js': 'coverage',
             '**/scripts/directives/**/*.js': 'coverage'
         },
 
-        ngHtml2JsPreprocessor: {
-            stripPrefix: 'app/'
-        },
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
@@ -80,23 +63,17 @@ module.exports = function(config) {
         colors: true,
 
 
-        // level of logging
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_DEBUG,
+        logLevel: config.LOG_INFO,
 
 
         // enable / disable watching file and executing tests whenever any file changes
         autoWatch: true,
+        browsers: ['PhantomJS'],
+        singleRun: false,
 
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        //browsers: ['Chrome'],
-        browsers: ['PhantomJS2'],
-
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
+        // Concurrency level
+        // how many browser should be started simultanous
+        concurrency: Infinity
     });
-};
+}
