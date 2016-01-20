246d513cac7db367f8299d9241f518a96df9751e
Sajjan Singh Mehta <ssmehta@ucdavis.edu>
2016-01-20 13:10:44 -0800

    updated serverDeploy rest path
    Former-commit-id: 273324ea82dd5228e2cf630c19082d96c98ac507

--- moa-client/serverDeploy.js
+++ moa-client/serverDeploy.js
@@ -4,4 +4,8 @@
  * where can we find our rest connections
  */
 
-app.constant('REST_BACKEND_SERVER', 'http://mona.fiehnlab.ucdavis.edu');
+(function() {
+    'use strict';
+    angular.module('moaClientApp')
+        .constant('REST_BACKEND_SERVER', 'http://mona.fiehnlab.ucdavis.edu');
+})();
