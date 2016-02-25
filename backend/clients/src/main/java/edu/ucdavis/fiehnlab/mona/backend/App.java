e542ffb9e8762a97cb65f0d8e98a6349c4864049
Gert <wohlgemuth@ucdavis.edu>
2016-02-25 12:08:52 -0800

    working on the persistence layer
    Former-commit-id: 1a2dd60548914fff7f74cac53922ee3110448845

---
+++ backend/clients/src/main/java/edu/ucdavis/fiehnlab/mona/backend/App.java
@@ -0,0 +1,13 @@
+package edu.ucdavis.fiehnlab.mona.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ backend/services/webhooks/src/main/java/edu/ucdavis/fiehnlab/mona/backend/services/App.java
@@ -0,0 +1,13 @@
+package edu.ucdavis.fiehnlab.mona.backend.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
