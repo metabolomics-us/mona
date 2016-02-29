41b3712122659387a3cc4c775b6aea110ca87917
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-02-29 10:39:37 -0800

    finished basic repository, cahcing will be done externally
    Former-commit-id: 4ef0fb37fda996c2c49a1922374e810ea8a84bf7

--- backend/core/persistence/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/ISpectrumRepositoryCustom.scala
+++ backend/core/persistence/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/ISpectrumRepositoryCustom.scala
@@ -1,13 +1,12 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo
 
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.data.domain.{Sort, Pageable, Page}
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.repository.{PagingAndSortingRepository, CrudRepository}
 
 /**
   * Created by wohlgemuth on 2/26/16.
   */
-trait ISpectrumRepositoryCustom extends PagingAndSortingRepository[Spectrum, String] with SpectrumRepositoryCustom {
-
-
-}
+trait ISpectrumRepositoryCustom extends PagingAndSortingRepository[Spectrum, String] with SpectrumRepositoryCustom
