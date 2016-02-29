6b23ab26d211cd59d3165eb2fce81a21e05fb602
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-02-29 10:27:09 -0800

    finished basic repository including caching support
    Former-commit-id: 6714f5a5183595d8bb5787903086e122af43236b

--- backend/core/persistence/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/SpectrumRepositoryCustom.scala
+++ backend/core/persistence/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/SpectrumRepositoryCustom.scala
@@ -1,6 +1,7 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo
 
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
+import org.springframework.data.domain.{Pageable, Page}
 import org.springframework.data.mongodb.core.query.{BasicQuery, Query}
 
 /**
@@ -17,11 +18,30 @@ trait SpectrumRepositoryCustom {
   def executeQuery(query: Query): List[Spectrum]
 
   /**
+    * pagination based approach to query the system
+    * @param query
+    * @param pageable
+    * @return
+    */
+  def executeQuery(query: Query, pageable: Pageable): Page[Spectrum]
+
+
+  /**
     * simple wrapper, so we don't have to use a query object
+    *
     * @param query
     * @return
     */
   def executeQuery(query: String): List[Spectrum] = {
     executeQuery(new BasicQuery(query))
+  }
+
+  /**
+    *
+    * @param query
+    * @return
+    */
+  def executeQuery(query: String, pageable: Pageable): Page[Spectrum] = {
+    executeQuery(new BasicQuery(query), pageable)
   }
 }
