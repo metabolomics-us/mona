3011460d34aab5033621438cc14e91102f017b3b
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-02-29 07:49:49 -0800

    added support for pagination
    Former-commit-id: ac5bca076a5086b1d9a4999322ff9b395c771916

--- backend/core/persistence/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/ISpectrumRepositoryCustom.scala
+++ backend/core/persistence/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/ISpectrumRepositoryCustom.scala
@@ -1,12 +1,13 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo
 
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.repository.{PagingAndSortingRepository, CrudRepository}
 
 /**
   * Created by wohlgemuth on 2/26/16.
   */
-trait ISpectrumRepositoryCustom extends CrudRepository[Spectrum, String] with SpectrumRepositoryCustom {
+trait ISpectrumRepositoryCustom extends PagingAndSortingRepository[Spectrum, String] with SpectrumRepositoryCustom {
 
 
 }
