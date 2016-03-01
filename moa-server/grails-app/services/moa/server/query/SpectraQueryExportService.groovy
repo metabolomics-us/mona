767abb6140084645ede294812dbdff2258604658
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-03-01 15:04:39 -0800

    switching to java zip library for query exports
    Former-commit-id: 743babe4619b2266d73051cbf27eea9d6e528be6

--- moa-server/grails-app/services/moa/server/query/SpectraQueryExportService.groovy
+++ moa-server/grails-app/services/moa/server/query/SpectraQueryExportService.groovy
@@ -7,11 +7,12 @@ import moa.query.Query
 import moa.server.convert.SpectraConversionService
 import moa.server.mail.EmailService
 import org.apache.commons.io.FileUtils
-import org.apache.tools.zip.ZipEntry
-import org.apache.tools.zip.ZipOutputStream
 import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.codehaus.groovy.grails.web.json.JSONObject
 import org.hibernate.SessionFactory
+
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 /**
  * Created by sajjan on 8/31/15.
@@ -209,7 +210,6 @@ class SpectraQueryExportService {
         }
 
         FileUtils.moveFile(compressedTemporaryFile, compressedFile)
-
 
         // Log filesize
         queryDownload.exportSize = compressedFile.length()
