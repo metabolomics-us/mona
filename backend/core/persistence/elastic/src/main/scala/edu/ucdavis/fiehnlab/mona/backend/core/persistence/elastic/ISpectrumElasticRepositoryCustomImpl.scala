d93185728ecf3dade3c7c501e1eda68ac2780818
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-09 13:30:51 -0800

    minor refacturing to ensure that elastic search api and mongo api will be compatible
    Former-commit-id: 686a393cbaf8e5bdefcd6b133bea6fdcd94fb26d

--- backend/core/persistence/elastic/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/elastic/ISpectrumElasticRepositoryCustomImpl.scala
+++ backend/core/persistence/elastic/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/elastic/ISpectrumElasticRepositoryCustomImpl.scala
@@ -2,11 +2,15 @@ package edu.ucdavis.fiehnlab.mona.backend.core.persistence.elastic
 
 import java.util
 
+import com.github.rutledgepaulv.qbuilders.visitors.{ElasticsearchVisitor, MongoVisitor}
+import com.github.rutledgepaulv.rqe.pipes.QueryConversionPipeline
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
+import org.elasticsearch.index.query.QueryBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.{Page, Pageable}
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate
-import org.springframework.data.elasticsearch.core.query.{NativeSearchQueryBuilder, NativeSearchQuery}
+import org.springframework.data.elasticsearch.core.query.{CriteriaQuery, NativeSearchQueryBuilder, NativeSearchQuery}
+import org.springframework.data.mongodb.core.query.Query
 
 /**
   * Created by wohlg_000 on 3/3/2016.
@@ -17,24 +21,32 @@ class ISpectrumElasticRepositoryCustomImpl extends SpectrumElasticRepositoryCust
   val elasticsearchTemplate:ElasticsearchTemplate = null
 
   /**
-    * simple wrapper, so we don't have to use a query object
+    * @param query
+    * @return
+    */
+  override def nativeQuery(query: String): util.List[Spectrum] = ???
+
+  /**
+    *
+    * @param query
+    * @return
+    *
+    */
+  override def nativeQuery(query: String, pageable: Pageable): Page[Spectrum] = ???
+
+  /**
+    * converts the RSQL String for us to a Query Object
     *
     * @param query
     * @return
     */
-  override def executeQuery(query: String): util.List[Spectrum] = ???
+  override def buildRSQLQuery(query: String): String = ???
+
   /**
     * executes a query against the system and returns the count
     *
     * @param query
     * @return
     */
-  override def executeQueryCount(query: String): Long = ???
-
-  /**
-    *
-    * @param query
-    * @return
-    */
-  override def executeQuery(query: String, pageable: Pageable): Page[Spectrum] = ???
+  override def nativeQueryCount(query: String): Long = ???
 }
