2284b8fc3669ece8fa170f7beb987f3a95253c97
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-09 12:38:47 -0800

    refactured generic rest controller
    Former-commit-id: cb6859f39f6e62260fc898659335e461afa0769a

--- backend/core/rest/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/controller/SubmitterRestController.scala
+++ backend/core/rest/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/controller/SubmitterRestController.scala
@@ -1,8 +1,19 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.controller
 
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Submitter
+import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+
 /**
   * Created by wohlgemuth on 3/7/16.
   */
-class SubmitterRestController {
-
+@RestController
+@RequestMapping(Array("/rest/submitter"))
+class SubmitterRestController extends GenericRESTController[Submitter]{
+  /**
+    * utilized repository
+    *
+    * @return
+    */
+  override def getRepository: PagingAndSortingRepository[Submitter, String] = ???
 }
