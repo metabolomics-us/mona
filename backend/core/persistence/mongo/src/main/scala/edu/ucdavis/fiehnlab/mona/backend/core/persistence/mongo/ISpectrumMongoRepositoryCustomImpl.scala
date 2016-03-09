bcf7fda8f47062940e6f92f31446302c73a4838b
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-09 09:41:43 -0800

    had to outsource buildQueryMethod
    Former-commit-id: 5b89a8b41dcf2abfa09e801be52bf25bf32f313d

--- backend/core/persistence/mongo/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/ISpectrumMongoRepositoryCustomImpl.scala
+++ backend/core/persistence/mongo/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/ISpectrumMongoRepositoryCustomImpl.scala
@@ -2,16 +2,19 @@ package edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo
 
 import java.util
 
+import com.github.rutledgepaulv.qbuilders.visitors.MongoVisitor
 import com.github.rutledgepaulv.rqe.pipes.QueryConversionPipeline
 import com.github.rutledgepaulv.rqe.pipes.QueryConversionPipeline
 import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
+import jdk.nashorn.internal.ir.visitor.NodeVisitor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.{Pageable, PageImpl, Page}
 import org.springframework.data.mongodb.core.MongoOperations
-import org.springframework.data.mongodb.core.query.{BasicQuery, Query}
+import org.springframework.data.mongodb.core.query.{Criteria, BasicQuery, Query}
 import org.springframework.stereotype.Repository
 import scala.collection.JavaConverters._
+import scala.reflect.ClassTag
 
 /**
   * Created by wohlgemuth on 2/26/16.
@@ -57,4 +60,19 @@ class ISpectrumMongoRepositoryCustomImpl extends SpectrumMongoRepositoryCustom w
     */
   override def nativeQueryCount(query: Query): Long = mongoOperations.count(query, classOf[Spectrum])
 
+  /**
+    * converts the RSQL String for us to a Query Object
+    *
+    * @param query
+    * @return
+    */
+  override def buildRSQLQuery(query: String): Query = {
+    val pipeline = QueryConversionPipeline.defaultPipeline()
+    val condition = pipeline.apply(query, classOf[Spectrum])
+    val criteria = condition.query(new MongoVisitor())
+    val toExecute = new Query()
+    toExecute.addCriteria(criteria)
+
+    toExecute
+  }
 }
