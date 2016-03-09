506795e3257736bc7d8660cf317b6d92480f326f
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-09 12:38:47 -0800

    refactured generic rest controller
    Former-commit-id: cb6859f39f6e62260fc898659335e461afa0769a

--- backend/core/rest/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/controller/SpectrumRestController.scala
+++ backend/core/rest/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/controller/SpectrumRestController.scala
@@ -8,50 +8,21 @@ import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo.ISpectrumMongoRepositoryCustom
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.http.{HttpStatus, ResponseEntity}
 import org.springframework.scheduling.annotation.{AsyncResult, Async}
 import org.springframework.web.bind.annotation._
-import scala.collection.JavaConverters._
 
-/**
-  * This is the main spectrum related REST controller and should be utilized for most operations
-  * interacting with the system.
-  *
-  * It allows you to perform the standard operations, you would like todo on a spectrum
-  *
-  */
+
 @RestController
 @RequestMapping(Array("/rest/spectra"))
-class SpectrumRestController {
+class SpectrumRestController extends GenericRESTController[Spectrum] {
 
   /**
     * this is the utilized repository, doing all the heavy lifting
     */
   @Autowired
-  val spectrumRepository: ISpectrumMongoRepositoryCustom = null
-
-  /**
-    * this will return all the specified data in the system
-    * please be aware that this can cause out of memory issues
-    * and should be always utilized with pagination
-    *
-    * @return
-    */
-  @RequestMapping(path = Array(""), method = Array(RequestMethod.GET))
-  @Async
-  def listAllSpectra(@RequestParam(value = "page", required = false) page: Integer, @RequestParam(value = "size", required = false) size: Integer): Future[Iterable[Spectrum]] = new AsyncResult[Iterable[Spectrum]](
-    if (size != null) {
-      if (page != null) {
-        spectrumRepository.findAll(new PageRequest(page, size)).getContent
-      }
-      else {
-        spectrumRepository.findAll(new PageRequest(0, size)).getContent
-      }
-    }
-    else {
-      spectrumRepository.findAll()
-    }
-  )
+  val spectrumMongoRepository: ISpectrumMongoRepositoryCustom = null
 
   /**
     * this executes a search against the reposiroty and can cause out of memory errors. We recommend to utilize this method with
@@ -62,14 +33,14 @@ class SpectrumRestController {
     */
   @RequestMapping(path = Array("/search"), method = Array(RequestMethod.POST))
   @Async
-  def searchSpectra(@RequestParam(value = "page", required = false) page: Integer, @RequestParam(value = "size", required = false) size: Integer, @RequestBody query: WrappedString): Future[java.util.List[Spectrum]] = new AsyncResult[java.util.List[Spectrum]](
+  def search(@RequestParam(value = "page", required = false) page: Integer, @RequestParam(value = "size", required = false) size: Integer, @RequestBody query: WrappedString): Future[java.util.List[Spectrum]] = new AsyncResult[java.util.List[Spectrum]](
     if (size != null)
       if (page != null)
-        spectrumRepository.nativeQuery(query.string, new PageRequest(page, size)).getContent
+        getRepository.nativeQuery(query.string, new PageRequest(page, size)).getContent
       else
-        spectrumRepository.nativeQuery(query.string, new PageRequest(0, size)).getContent
+        getRepository.nativeQuery(query.string, new PageRequest(0, size)).getContent
     else
-      spectrumRepository.nativeQuery(query.string)
+      getRepository.nativeQuery(query.string)
   )
 
   /**
@@ -81,14 +52,14 @@ class SpectrumRestController {
     */
   @RequestMapping(path = Array("/search"), method = Array(RequestMethod.GET))
   @Async
-  def RSQL(@RequestParam(value = "page", required = false) page: Integer, @RequestParam(value = "size", required = false) size: Integer, @RequestParam(value = "query", required = true) query: WrappedString): Future[java.util.List[Spectrum]] = new AsyncResult[java.util.List[Spectrum]](
+  def searchRSQL(@RequestParam(value = "page", required = false) page: Integer, @RequestParam(value = "size", required = false) size: Integer, @RequestParam(value = "query", required = true) query: WrappedString): Future[java.util.List[Spectrum]] = new AsyncResult[java.util.List[Spectrum]](
     if (size != null)
       if (page != null)
-        spectrumRepository.rsqlQuery(query.string, new PageRequest(page, size)).getContent
+        getRepository.rsqlQuery(query.string, new PageRequest(page, size)).getContent
       else
-        spectrumRepository.rsqlQuery(query.string, new PageRequest(0, size)).getContent
+        getRepository.rsqlQuery(query.string, new PageRequest(0, size)).getContent
     else
-      spectrumRepository.rsqlQuery(query.string)
+      getRepository.rsqlQuery(query.string)
   )
 
 
@@ -99,66 +70,10 @@ class SpectrumRestController {
     */
   @RequestMapping(path = Array("/count"), method = Array(RequestMethod.POST))
   @Async
-  def searchSpectraCount(@RequestBody query: WrappedString): Future[Long] = {
-    new AsyncResult[Long](spectrumRepository.nativeQueryCount(query.string))
+  def searchCount(@RequestBody query: WrappedString): Future[Long] = {
+    new AsyncResult[Long](getRepository.nativeQueryCount(query.string))
   }
 
-  /**
-    * this method returns the complete count of spectra in the system
-    *
-    * @return
-    */
-  @RequestMapping(path = Array("/count"), method = Array(RequestMethod.GET))
-  @Async
-  def searchSpectraCount: Future[Long] = {
-    new AsyncResult[Long](spectrumRepository.count())
-  }
-
-
-  /**
-    * saves a spectra or updates it. This will depend on the utilized repository
-    *
-    * @param spectrum
-    * @return
-    */
-  @Async
-  @RequestMapping(path = Array(""), method = Array(RequestMethod.POST))
-  def save(@RequestBody spectrum: Spectrum) = new AsyncResult[Spectrum](
-    spectrumRepository.save(spectrum)
-  )
-
-  /**
-    * looks for the exact spectra
-    *
-    * @param spectrum
-    * @return
-    */
-  @Async
-  @RequestMapping(path = Array("/{id}"), method = Array(RequestMethod.GET))
-  def get(@PathVariable("id") spectrum: String): Future[ResponseEntity[Spectrum]] = {
-
-    if (spectrumRepository.exists(spectrum)) {
-      new AsyncResult[ResponseEntity[Spectrum]](
-        new ResponseEntity[Spectrum](spectrumRepository.findOne(spectrum), HttpStatus.OK)
-      )
-    }
-    else {
-      new AsyncResult[ResponseEntity[Spectrum]](
-        new ResponseEntity[Spectrum](HttpStatus.NOT_FOUND)
-      )
-    }
-  }
-
-
-  /**
-    * this methods removes the specified method from the system
-    *
-    * @param spectrum
-    * @return
-    */
-  @Async
-  @RequestMapping(path = Array("/{id}"), method = Array(RequestMethod.DELETE))
-  def delete(@PathVariable("id") spectrum: String) = spectrumRepository.delete(spectrum)
 
   /**
     * saves the provided spectrum at the given path
@@ -173,15 +88,15 @@ class SpectrumRestController {
 
     if (id == spectrum.id) {
       new AsyncResult[Spectrum](
-        spectrumRepository.save(spectrum.copy(id = id))
+        getRepository.save(spectrum.copy(id = id))
       )
 
     }
     else {
-      spectrumRepository.delete(spectrum.id)
+      getRepository.delete(spectrum.id)
 
       val newSpectrum = spectrum.copy(id = id)
-      val result = spectrumRepository.save(newSpectrum)
+      val result = getRepository.save(newSpectrum)
 
       new AsyncResult[Spectrum](
         result
@@ -189,4 +104,10 @@ class SpectrumRestController {
     }
   }
 
+  /**
+    * utilized repository
+    *
+    * @return
+    */
+  override def getRepository: PagingAndSortingRepository[Spectrum, String] with ISpectrumMongoRepositoryCustom = spectrumMongoRepository
 }
