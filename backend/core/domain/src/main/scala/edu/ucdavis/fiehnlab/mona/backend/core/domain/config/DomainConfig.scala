9e626f9c567f3728f5ee28260bccb0b4a63a87bc
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-10 11:05:29 -0800

    working on elastic search implementation
    Former-commit-id: 1d388bb5df267658a76b1881de0578893e4343b2

---
+++ backend/core/domain/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/domain/config/DomainConfig.scala
@@ -0,0 +1,17 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.domain.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.MonaMapper
+import org.springframework.context.annotation.{Configuration, Primary, Bean}
+
+/**
+  * Created by wohlgemuth on 3/10/16.
+  */
+@Configuration
+class DomainConfig {
+
+  @Bean
+  @Primary
+  def objectMapper : ObjectMapper = MonaMapper.create
+
+}
