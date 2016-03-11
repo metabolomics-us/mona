69b830b05ff79b1f919064cd8556b068a3309d0a
Gert <wohlgemuth@ucdavis.edu>
2016-03-10 20:46:20 -0800

    working on custom serialization methods for elastic search
    Former-commit-id: 67ca832f9f58e649cb13b99b7d7fbd34298aa5c8

---
+++ backend/core/persistence/elastic/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/elastic/mapper/ElasticMedaDataDeserializerTest.scala
@@ -0,0 +1,14 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.persistence.elastic.mapper
+
+import org.scalatest.FunSuite
+
+/**
+  * Created by wohlg_000 on 3/10/2016.
+  */
+class ElasticMedaDataDeserializerTest extends FunSuite {
+
+  test("testDeserialize") {
+
+  }
+
+}
