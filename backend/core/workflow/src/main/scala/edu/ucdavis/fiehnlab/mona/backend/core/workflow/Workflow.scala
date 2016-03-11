c830a0ee327d5aaf9b3ac21b859b5096180e79f5
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-11 10:24:00 -0800

    started working on the workflow concepts
    Former-commit-id: 80dee414d92499819b7359ec27843dfe194e4345

---
+++ backend/core/workflow/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/workflow/Workflow.scala
@@ -0,0 +1,16 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.workflow
+
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
+
+/**
+  * defines a standard processing workflow
+  */
+class Workflow {
+
+  /**
+    * executes the workflow for the given spectrum
+    * @param spectrum
+    * @return
+    */
+  def run(spectrum:Spectrum) = ???
+}
---
+++ backend/core/workflow/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/workflow/processor/RemoveComputedMetaDataTest.scala
@@ -0,0 +1,57 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.workflow.processor
+
+import java.io.FileReader
+
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.JSONDomainReader
+import org.scalatest.WordSpec
+
+/**
+  * Created by wohlgemuth on 3/11/16.
+  */
+class RemoveComputedMetaDataTest extends WordSpec {
+
+  val reader = JSONDomainReader.create[Spectrum]
+
+  "this processor" when {
+
+    val processor = new RemoveComputedMetaData
+
+    val input = new FileReader("src/test/resources/monaRecord.json")
+
+    val spectrumGiven: Spectrum = reader.read(input)
+
+    "given a spectra" must {
+
+      val processedSpectrum = processor.process(spectrumGiven)
+
+      "remove the computed metadata for spectra" in {
+
+        processedSpectrum.metaData.foreach { metaData =>
+          assert(metaData.computed == false)
+        }
+      }
+
+      "remove the computed metadata for biological compound" in {
+
+        processedSpectrum.biologicalCompound.metaData.foreach { metaData =>
+          assert(metaData.computed == false)
+        }
+
+      }
+
+      "remove the computed metadata for chemical compound" in {
+
+        processedSpectrum.chemicalCompound.metaData.foreach { metaData =>
+          assert(metaData.computed == false)
+        }
+
+      }
+
+      "and remove the predicted compound" in {
+        assert(processedSpectrum.predictedCompound == null)
+      }
+
+    }
+  }
+}
