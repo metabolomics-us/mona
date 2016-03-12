2483a202a44960d460cd1727d5a7e84338982e71
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-12 12:08:29 -0800

    optimizing configurations accross the projects, to remove autconfigurations as much as possible
    Former-commit-id: a47bca6651fcf67ca84b59a8308396c4642bdb0d

--- backend/core/domain/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/domain/config/DomainConfig.scala
+++ backend/core/domain/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/domain/config/DomainConfig.scala
@@ -2,11 +2,12 @@ package edu.ucdavis.fiehnlab.mona.backend.core.domain.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.MonaMapper
-import org.springframework.context.annotation.{Configuration, Primary, Bean}
+import org.springframework.context.annotation.{ComponentScan, Configuration, Primary, Bean}
 
 /**
   * Created by wohlgemuth on 3/10/16.
   */
+
 @Configuration
 class DomainConfig {
 
