9a90b0354ef773892cf68bd7a8fbf241e5b5fe29
locker47 <locker47@gmail.com>
2016-03-17 12:04:42 -0700

    Added aggregation query for metadata (note: pagination/sorting failing)
    Former-commit-id: a56ad22ee5833fb485acd15b4794dcf3f1940021

---
+++ backend/core/persistence/mongo/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/MetadataMongoRepositoryCustom.scala
@@ -0,0 +1,20 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo
+
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Types.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rsql.RSQLRepositoryCustom
+import org.springframework.data.mongodb.core.query.Query
+
+trait MetadataMongoRepositoryCustom extends RSQLRepositoryCustom[Spectrum,Query] {
+  /**
+    * Return the aggregated list of unique values for a given metadata field. The results are sorted
+    * in descending order. By default, this method returns the top 10 results from the base metadata
+    * group; to specify custom values, supply `numResults` and `metaDataGroup`, respectively.
+    *
+    * @param name The target metadata field
+    * @param skip The number of results to drop
+    * @param limit The number of results to take
+    * @param metaDataGroup The metadata group to inspect
+    * @return
+    */
+  def listTopAggregates(name: String, skip: Int = 0, limit: Long = 10, metaDataGroup: Option[String] = None): Seq[(AnyVal, Int)]
+}
