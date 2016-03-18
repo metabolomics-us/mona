3353510c9c2bcb0d9f4d7ec0b053816cbd4cc20c
locker47 <locker47@gmail.com>
2016-03-18 15:24:10 -0700

    Removed metadata statistics from persistence module
    Former-commit-id: 21971ae084974823a2fe77d9b08c4c251df6b18e

--- backend/core/persistence/mongo/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/repository/ISpectrumMongoRepositoryCustom.scala
+++ backend/core/persistence/mongo/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/mongo/repository/ISpectrumMongoRepositoryCustom.scala
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Repository
   * Created by wohlgemuth on 2/26/16.
   */
 @Repository("spectrumMongoRepository")
-trait ISpectrumMongoRepositoryCustom extends PagingAndSortingRepository[Spectrum, String]
-  with SpectrumMongoRepositoryCustom
-  with MetadataMongoRepositoryCustom
+trait ISpectrumMongoRepositoryCustom extends PagingAndSortingRepository[Spectrum, String] with SpectrumMongoRepositoryCustom
