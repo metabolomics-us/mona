30a3da26f595b63bd127cf2a6a884034ac20f363
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-03-23 16:42:36 -0700

    added constants of common tags and metadata
    Former-commit-id: eb49885213e45f668e87e95ef41fbd9c6acc31fb

---
+++ backend/core/curation/src/main/scala/edu.ucdavis.fiehnlab.mona.backend.curation/common/CommonTags.scala
@@ -0,0 +1,14 @@
+package edu.ucdavis.fiehnlab.mona.backend.curation.common
+
+/**
+  * Created by sajjan on 3/23/16.
+  */
+object CommonTags {
+  val LCMS_SPECTRUM: String = "LCMS"
+
+  val GCMS_SPECTRUM: String = "GCMS"
+
+  val VIRTUAL_COMPOUND: String = "In-Silico"
+
+  val ANNOTATED_SPECTRUM: String = "Annotated"
+}
