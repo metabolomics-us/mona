bda0cef928c3a5b7f9cee9aebbc30ccd24dfe287
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-03-23 16:41:46 -0700

    improved the remove computed data curation rule
    Former-commit-id: c24e000265cebc69ec591601fd28944c3042b561

---
+++ backend/core/curation/src/test/scala/edu.ucdavis.fiehnlab.mona.backend.curation/processor/RemoveComputedDataTest.scala
@@ -0,0 +1,65 @@
+package edu.ucdavis.fiehnlab.mona.backend.curation.processor
+
+import java.io.InputStreamReader
+
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.JSONDomainReader
+import org.scalatest.WordSpec
+
+/**
+  * Created by wohlgemuth on 3/11/16.
+  */
+class RemoveComputedDataTest extends WordSpec {
+
+  val reader = JSONDomainReader.create[Spectrum]
+
+  "this processor" when {
+
+    val processor = new RemoveComputedData
+
+    val input = new InputStreamReader(getClass.getResourceAsStream("/monaRecord.json"))
+
+    val spectrumGiven: Spectrum = reader.read(input)
+
+    "given a spectra" must {
+
+      val processedSpectrum = processor.process(spectrumGiven)
+
+      "remove the computed metadata for the spectrum" in {
+        assert(processedSpectrum.metaData.forall(!_.computed))
+      }
+
+      "remove the computed metadata for the biological compound" in {
+        assert(processedSpectrum.biologicalCompound.metaData.forall(!_.computed))
+      }
+
+      "remove the computed metadata for the chemical compound" in {
+        assert(processedSpectrum.chemicalCompound.metaData.forall(!_.computed))
+      }
+
+      "remove the predicted compound" in {
+        assert(processedSpectrum.predictedCompound == null)
+      }
+
+      "remove the computed names for the biological compound" in {
+        assert(processedSpectrum.biologicalCompound.names.forall(!_.computed))
+      }
+
+      "remove the computed names for the chemical compound" in {
+        assert(processedSpectrum.chemicalCompound.names.forall(!_.computed))
+      }
+
+      "remove the computed tags for the spectrum" in {
+        assert(processedSpectrum.tags.forall(!_.ruleBased))
+      }
+
+      "remove the computed tags for the biological compound" in {
+        assert(processedSpectrum.biologicalCompound.tags.forall(!_.ruleBased))
+      }
+
+      "remove the computed tags for the chemical compound" in {
+        assert(processedSpectrum.chemicalCompound.tags.forall(!_.ruleBased))
+      }
+    }
+  }
+}
