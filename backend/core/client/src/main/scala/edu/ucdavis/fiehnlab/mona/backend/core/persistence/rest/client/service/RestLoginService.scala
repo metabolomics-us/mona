e2671b7b12a5364e82e5ce65a9c5a239d3576260
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-27 14:43:30 -0700

    added small rest client implementation of the auth service and qualfiied the REST bean as delagte to allow multiple instances
    Former-commit-id: 7e651f19c491150fd1020ccff8ba7e16ecb9798c

---
+++ backend/core/client/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/client/service/RestLoginService.scala
@@ -0,0 +1,23 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.client.service
+
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.HelperTypes.{LoginRequest, LoginResponse}
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.servcie.LoginService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.client.RestOperations
+
+/**
+  * this is a simple rest based implementation of our login service
+  */
+class RestLoginService(val remoteServer:String, val remotePort:Int, val protocol:String = "http") extends LoginService{
+
+  @Autowired
+  val restOperations:RestOperations = null
+
+  /**
+    * does a login for the current user
+    * and returns a response or throws a related exception
+    *
+    * @return
+    */
+  override def login(request: LoginRequest): LoginResponse = restOperations.postForEntity(s"${protocol}://${remoteServer}:${remotePort}/rest/auth/login",request,classOf[LoginResponse]).asInstanceOf[LoginResponse]
+}
