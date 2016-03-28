04f1d8ba97e7307e7aa6302d08272386f4060a50
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-27 17:20:44 -0700

    added working test client implmentation, backed by several http authorization
    Former-commit-id: 4d0c0b4417d61c244b2aeaae5658de116fcc57b8

--- backend/core/auth/rest/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/config/AuthSecurityConfig.scala
+++ backend/core/auth/rest/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/config/AuthSecurityConfig.scala
@@ -2,19 +2,22 @@ package edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.config
 
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.controller.LoginController
 import org.springframework.context.annotation.{ComponentScan, Configuration}
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.builders.{HttpSecurity, WebSecurity}
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 /**
   * Created by wohlg on 3/27/2016.
   */
+@Order(1)
 @Configuration
 @ComponentScan(basePackageClasses = Array(classOf[LoginController]))
 class AuthSecurityConfig extends WebSecurityConfigurerAdapter {
 
   /**
     * ignore post requests to the auth url
+    *
     * @param web
     */
   override def configure(web: WebSecurity): Unit = {
@@ -22,4 +25,6 @@ class AuthSecurityConfig extends WebSecurityConfigurerAdapter {
       .antMatchers(HttpMethod.POST, "/rest/auth/**")
 
   }
+
+  override def configure(http: HttpSecurity): Unit = http.csrf().disable()
 }
--- backend/core/client/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/client/config/RestClientTestConfig.scala
+++ backend/core/client/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/client/config/RestClientTestConfig.scala
@@ -1,18 +1,25 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.client.config
 
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.service.MongoLoginService
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.config.AuthSecurityConfig
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.servcie.LoginService
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.client.api.GenericRestClient
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.client.service.RestLoginService
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.config.EmbeddedRestServerConfig
-import org.springframework.context.annotation.{Bean, Import}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.{Bean, Import, Primary}
 
 /**
   * Created by wohlgemuth on 3/15/16.
   */
-@Import(Array(classOf[RestClientConfig],classOf[EmbeddedRestServerConfig]))
+@Import(Array(classOf[RestClientConfig],classOf[EmbeddedRestServerConfig],classOf[AuthSecurityConfig]))
 class RestClientTestConfig{
 
   @Bean
-  def spectrumRestClient: GenericRestClient[Spectrum, String] = {
-    new GenericRestClient[Spectrum, String]("rest/spectra")
-  }
+  @Primary
+  def loginService:LoginService = new RestLoginService("localhost",44444)
+
+  @Bean
+  def loginServiceDelegate:LoginService = new MongoLoginService
 }
