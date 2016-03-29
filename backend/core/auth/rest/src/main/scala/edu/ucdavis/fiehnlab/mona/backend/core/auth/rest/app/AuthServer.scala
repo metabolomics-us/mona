95c9a88997c5d8f585d7043771328cdcad2d6290
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-29 13:37:24 -0700

    added support for configuration server and the possibility to create a default admin user during startup
    Former-commit-id: 41390ef53e05ab1cf32e832c9323df9d33437475

--- backend/core/auth/rest/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/app/AuthServer.scala
+++ backend/core/auth/rest/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/app/AuthServer.scala
@@ -1,14 +1,21 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.app
 
+import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.config.JWTAuthenticationConfig
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.repository.UserRepository
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.service.MongoLoginService
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.types.TokenSecret
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.config.AuthSecurityConfig
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.types.{Role, User}
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.servcie.LoginService
-import org.springframework.boot.SpringApplication
+import org.springframework.beans.factory.annotation.{Autowired, Value}
+import org.springframework.boot.{CommandLineRunner, SpringApplication}
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.{Bean, Import}
+import org.springframework.stereotype.Component
+
+import scala.collection.JavaConverters._
 
 /**
   * Created by wohlgemuth on 3/28/16.
@@ -17,6 +24,9 @@ import org.springframework.context.annotation.{Bean, Import}
 @EnableDiscoveryClient
 @Import(Array(classOf[AuthSecurityConfig],classOf[JWTAuthenticationConfig]))
 class AuthServer {
+
+  @Value("${mona.security.secret}")
+  val secret:String = null
 
   /**
     * the service which actually does the login for us
@@ -33,12 +43,30 @@ class AuthServer {
     * @return
     */
   @Bean
-  def tokenSecret: TokenSecret = TokenSecret("sadaskdkljsalkd")
-
+  def tokenSecret: TokenSecret = TokenSecret(secret)
 
 }
 
+@Component
+class AuthCommandRunner extends CommandLineRunner with LazyLogging{
+
+  @Value("${mona.security.auth.admin.username}")
+  val adminUser:String = null
+
+  @Value("${mona.security.auth.admin.password}")
+  val adminPassword:String = null
+
+  @Autowired
+  val userRepository:UserRepository = null
+
+  override def run(strings: String*): Unit = {
+    val user = userRepository.save(User(adminUser,adminPassword, List(Role("admin")).asJava))
+    logger.info(s"created default user: ${user}")
+  }
+
+}
+
+
 object AuthServer extends App{
-  System.setProperty("spring.config.name", "auth-service");
   new SpringApplication(classOf[AuthServer]).run()
 }
