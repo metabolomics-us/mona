6eafb3428f6b3a4264675fe325b40e8101118551
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-03-30 11:58:12 -0700

    added support for writes which attempt to recover as well as outsourcing the event listeners to a scheduler for easer refactoring later
    Former-commit-id: 3222722a4a2f7ef3fe9457032abcc190b8dcc2ea

--- backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/writer/RestRepositoryWriter.scala
+++ backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/writer/RestRepositoryWriter.scala
@@ -7,12 +7,14 @@ import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.client.api.MonaSpectrumRestClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpServerErrorException
 
 /**
-  * Created by wohlgemuth on 3/11/16.
+  * A writer to persiste values to the repository, it has a silent retry feature and will attempt to retry saving the result
+  * in case of error N times before it surrenders and throws an exceptions
   */
 @Component
-class RestRepositoryWriter(val loginToken:String) extends WriterAdapter with LazyLogging {
+class RestRepositoryWriter(val loginToken:String, val retrySilently:Boolean = true, val maxRetries:Int = 10) extends WriterAdapter with LazyLogging {
 
   @Autowired
   val monaSpectrumRestClient: MonaSpectrumRestClient = null
@@ -23,6 +25,14 @@ class RestRepositoryWriter(val loginToken:String) extends WriterAdapter with Laz
     monaSpectrumRestClient.login(loginToken)
   }
 
+  private var counter:Int = 0
+
+  /**
+    * how many tries are left for the current data set
+    */
+  private var retriesLeft = maxRetries
+
+
   /**
     * attempts to write all these spectra to the repository
     * and saves them if they have an id or updates them if they do not have an id
@@ -31,14 +41,49 @@ class RestRepositoryWriter(val loginToken:String) extends WriterAdapter with Laz
     */
   override def write(spectrum: Spectrum): Unit = {
 
-    if (spectrum.id == null) {
-      logger.debug("adding spectra to server")
-      monaSpectrumRestClient.add(spectrum)
-    }
-    else {
-      logger.debug(s"updating spectra on server ${spectrum.id}")
-      monaSpectrumRestClient.update(spectrum, spectrum.id)
-    }
+    try {
+      if (spectrum.id == null) {
+        logger.debug("adding spectra to server")
+        monaSpectrumRestClient.add(spectrum)
+      }
+      else {
+        logger.debug(s"updating spectra on server ${spectrum.id}")
 
+        val s = monaSpectrumRestClient.get(spectrum.id)
+
+        if (s != null) {
+          monaSpectrumRestClient.updateAsync(spectrum, spectrum.id)
+        }
+        else {
+          logger.debug("server was not aware of id, assuming it's a backup and adding it instead")
+          monaSpectrumRestClient.add(spectrum)
+        }
+
+      }
+
+      counter = counter + 1
+
+      if (counter % 1000 == 1) {
+        logger.info(s"written ${counter} spectra to the repository")
+      }
+    }
+    catch {
+      case e:HttpServerErrorException =>
+        logger.warn(e.getMessage)
+
+        if(retrySilently && retriesLeft > 0){
+          retriesLeft = retriesLeft -1
+          logger.debug(s"attempting recovery ${maxRetries - retriesLeft} out of ${maxRetries}")
+
+          Thread.currentThread().wait(1000)
+          write(spectrum)
+
+          //success time to reset the retires
+          retriesLeft = maxRetries
+        }
+        else{
+          throw e
+        }
+    }
   }
 }
