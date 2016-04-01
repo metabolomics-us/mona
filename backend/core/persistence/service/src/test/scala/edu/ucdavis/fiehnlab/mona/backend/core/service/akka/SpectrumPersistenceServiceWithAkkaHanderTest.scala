c8c96e328541a4e03c5d2d24aeb383a8ae91e885
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-04-01 09:27:58 -0700

    added akka based scheduler implementation
    Former-commit-id: 24db09dc7b7c70abc97f7af778cc28c0fa2ff3ec

---
+++ backend/core/persistence/service/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/service/akka/SpectrumPersistenceServiceWithAkkaHanderTest.scala
@@ -0,0 +1,38 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.service.akka
+
+import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.service.{AbstractSpectrumPersistenceServiceTest, EmbeddedServiceConfig}
+import edu.ucdavis.fiehnlab.mona.backend.core.service.listener.AkkaEventScheduler
+import org.junit.runner.RunWith
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.context.annotation.{Bean, Configuration}
+import org.springframework.test.context.TestContextManager
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+/**
+  * Created by wohlgemuth on 4/1/16.
+  */
+@RunWith(classOf[SpringJUnit4ClassRunner])
+@SpringApplicationConfiguration(classes = Array(classOf[EmbeddedServiceConfig]))
+class SpectrumPersistenceServiceWithAkkaHanderTest extends AbstractSpectrumPersistenceServiceTest with LazyLogging {
+  new TestContextManager(this.getClass()).prepareTestInstance(this)
+
+  "we must ensure" when {
+    "that our event scheduler " should {
+      "be of type AkkaEventScheduler" in {
+        assert(spectrumPersistenceService.eventScheduler.isInstanceOf[AkkaEventScheduler[Spectrum]])
+      }
+    }
+  }
+
+}
+
+@Configuration
+class AkkaTestConfiguration {
+
+  @Bean
+  def eventScheduler:AkkaEventScheduler[Spectrum] = {
+    new AkkaEventScheduler[Spectrum]
+  }
+}
