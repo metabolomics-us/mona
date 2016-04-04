7e84b7e0bd044fdff736bab3c29bf1a557e85d86
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2016-04-04 15:41:28 -0700

    removed redirect to index.html on error
    Former-commit-id: ab134a76fd046d9e596531997b2634321fd8a6be

--- moa-client/app/scripts/controllers/main.js
+++ moa-client/app/scripts/controllers/main.js
@@ -24,7 +24,7 @@
                         var status = curError.status;
 
                         var message = '<strong>Unable to</strong> ' + method + ' from ' + url + ' Status: ' + status;
-                        var id = Flash.create('danger', message, 10000, {class: 'custom-class', id: 'custom-id'}, true);
+                        //var id = Flash.create('danger', message, 10000, {class: 'custom-class', id: 'custom-id'}, true);
                         /* First argument (string) is the type of the flash alert.
                          * Second argument (string) is the message displays in the flash alert (HTML is ok).
                          * Third argument (number, optional) is the duration of showing the flash. 0 to not automatically hide flash (user needs to click the cross on top-right corner).
@@ -32,6 +32,7 @@
                          * Fifth argument (boolean, optional) is the visibility of close button for this flash.
                          * Returns the unique id of flash message that can be used to call Flash.dismiss(id); to dismiss the flash message.
                          */
+                        console.log(message);
                     }
                 }
             })();
