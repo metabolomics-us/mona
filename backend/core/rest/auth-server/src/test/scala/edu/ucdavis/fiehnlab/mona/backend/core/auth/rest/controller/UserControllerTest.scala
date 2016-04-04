b1fa80806883640c96d7cafaae4df7128def2c62
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-04-04 15:44:42 -0700

    added user controller and related tests, moving stuff around to reduce code duplication
    Former-commit-id: 5cf58132835c3800bfe5931480acb4ed7971242a

---
+++ backend/core/rest/auth-server/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/controller/UserControllerTest.scala
@@ -0,0 +1,48 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.controller
+
+import java.io.InputStreamReader
+import java.util.Collections
+
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.config.JWTAuthenticationConfig
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.repository.UserRepository
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.config.AuthSecurityConfig
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.types.{Role, User}
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.{Spectrum, Submitter}
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.JSONDomainReader
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo.repository.ISubmitterMongoRepository
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.controller.AbstractGenericRESTControllerTest
+import org.junit.runner.RunWith
+import org.scalatest.WordSpec
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.{SpringApplicationConfiguration, WebIntegrationTest}
+import org.springframework.test.context.TestContextManager
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+import scala.collection.JavaConverters._
+
+/**
+  * Created by wohlgemuth on 4/4/16.
+  */
+@RunWith(classOf[SpringJUnit4ClassRunner])
+@SpringApplicationConfiguration(classes = Array(classOf[JWTAuthenticationConfig], classOf[TestConfig],classOf[AuthSecurityConfig]))
+@WebIntegrationTest(Array("server.port=0"))
+class UserControllerTest extends AbstractGenericRESTControllerTest[User]("/users") {
+
+  //required for spring and scala tes
+  new TestContextManager(this.getClass()).prepareTestInstance(this)
+
+  /**
+    * object to use for gets
+    *
+    * @return
+    */
+  override def getValue: User = User("admi312312n", "a13123dmin", List[Role](Role("admin")).asJava)
+
+  /**
+    * returns an id for us for testing
+    *
+    * @return
+    */
+  override def getId: String = getValue.username
+
+}
