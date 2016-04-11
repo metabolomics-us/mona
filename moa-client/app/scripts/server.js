41f9d3b575b56c6b6871c312cf9309a2fea59c27
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2016-04-11 11:50:59 -0700

    updated new backend settings
    Former-commit-id: bedbf99c001370fc84028ce987c1922699e8d28e

--- moa-client/app/scripts/server.js
+++ moa-client/app/scripts/server.js
@@ -2,10 +2,13 @@
  * Created by wohlgemuth on 2/12/15.
  *
  * where can we find our rest connections
+ * DEV address: http://cream.fiehnlab.ucdavis.edu:8080
+ * alternative production: http://cream.fiehnlab.ucdavis.edu:9292/mona.fiehnlab.ucdavis.edu
+ * http://0.0.0.0:9292/mona.fiehnlab.ucdavis.edu
  */
 
 (function() {
     'use strict';
     angular.module('moaClientApp')
-        .constant('REST_BACKEND_SERVER', 'http://mona.fiehnlab.ucdavis.edu');
+        .constant('REST_BACKEND_SERVER', 'http://gose.fiehnlab.ucdavis.edu:8080');
 })();
