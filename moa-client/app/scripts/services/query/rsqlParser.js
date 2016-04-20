b36c9f8aae9c95e46c09345d7059724e2584a425
benn7 <bdnguyen7@gmail.com>
2016-04-19 17:53:46 -0700

    created rsqlParser factory and refactor code, added parseTags
    Former-commit-id: 04d24d72c7157bb67ca77d847f6ffcc2222eb483

---
+++ moa-client/app/scripts/services/query/rsqlParser.js
@@ -0,0 +1,91 @@
+/**
+ * This factory will parse the query Modal object and returns an RSQL Query String
+ *
+ */
+
+(function () {
+    'use strict';
+    angular.module('moaClientApp')
+        factory(rsqlParser, rsqlParser);
+
+    /* @ngInject */
+    function rsqlParser($log) {
+        var service = {
+            parseRSQL: parseRSQL
+        };
+        return service;
+
+        /**
+         * parses a query object and returns a RSQL Query String
+         * @param query
+         * @return query
+         */
+        function parseRSQL(query) {
+            var queryString = "";
+
+            if (Object.keys(query.compound).length !== 0 && JSON.stringify(query.compound) !== JSON.stringify({})) {
+                queryString += buildCompoundQueryString(query.compound);
+            }
+
+            if (query.metadata.length !== 0) {
+                queryString += buildMetaDataQueryString(query.metadata);
+            }
+
+            if (query.tags.length !== 0) {
+                queryString += buildTagsQueryString(query.tags);
+            }
+
+            $log.info(query);
+            $log.info(queryString);
+
+        }
+
+        function buildTagsQueryString(tagQuery) {
+            var queryString = "";
+            for (var i = 0, l = tagQuery.length; i < l; i++) {
+                if (i > 0) {
+                    queryString += ' and ';
+                }
+                queryString += "tags=q='name.eq==" + tagQuery.name.eq + '\"\'';
+
+            }
+            return queryString;
+        }
+
+        function buildMetaDataQueryString(metaDataQuery) {
+            var queryString = "";
+            for (var i = 0, l = metaDataQuery.length; i < l; i++) {
+                var object = metaDataQuery[i];
+                var operator = object.value.eq ? "==" : "!=" ;
+
+                if (i > 0) {
+                    queryString += ' and ';
+                }
+
+                queryString += "metaData=q='name" + operator + '\"' + object.value.eq || object.value.ne + '\"\'';
+            }
+            return queryString;
+        }
+
+        function buildCompoundQueryString(compoundQuery) {
+            var bio = "";
+            var chem = "";
+
+            // handle compound name
+            if(typeof(compoundQuery.name) !== 'undefined') {
+                bio += "biologicalCompound.names=q='name==" + '\"' + compoundQuery.name + '\"\'';
+                chem += "chemicalCompound.names=q='name==" + '\"' + compoundQuery.name + '\"\'';
+
+            }
+
+            // handle compound inchiKey
+            if(typeof(compoundQuery.inchiKey) !== 'undefined') {
+                bio += " or biologicalCompound=q=inchiKey==" + '\"' + compoundQuery.inchiKey + '\"\'';
+                chem += " or chemicalCompound=q=inchiKey==" + '\"' + compoundQuery.inchiKey + '\"\'';
+            }
+            console.log( bio + ' or ' + chem);
+            return bio + ' or ' + chem;
+        }
+
+    }
+})();
