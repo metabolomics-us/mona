e8e3240ce9318d786fae08fb70dde26d27157201
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-04-21 16:20:39 -0700

    refined common metadata and tags
    Former-commit-id: ce7bc4593a280707df1319397fe972cabe8d89e4

--- backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/common/CommonTags.scala
+++ backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/common/CommonTags.scala
@@ -4,9 +4,9 @@ package edu.ucdavis.fiehnlab.mona.backend.curation.common
   * Created by sajjan on 3/23/16.
   */
 object CommonTags {
-  val LCMS_SPECTRUM: String = "LCMS"
+  val LCMS_SPECTRUM: String = "LC-MS"
 
-  val GCMS_SPECTRUM: String = "GCMS"
+  val GCMS_SPECTRUM: String = "GC-MS"
 
   val VIRTUAL_COMPOUND: String = "In-Silico"
 }
