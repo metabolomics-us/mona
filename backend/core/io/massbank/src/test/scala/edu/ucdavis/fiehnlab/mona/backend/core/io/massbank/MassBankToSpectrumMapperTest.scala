a9009fc74cca6d9a936271cdd3faf0035056818a
Akie Mejia <rfmejia@protonmail.com>
2016-04-26 10:28:42 -0700

    Added tests for special spectrum mappings
    Tests for specific field mappings other than direct assignments into
    base metadata group were added.
    
    
    Former-commit-id: e67654f5b86fc25e5abca2f337609bf5ba9cad34

--- backend/core/io/massbank/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/io/massbank/MassBankToSpectrumMapperTest.scala
+++ backend/core/io/massbank/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/io/massbank/MassBankToSpectrumMapperTest.scala
@@ -16,14 +16,38 @@ class MassBankToSpectrumMapperTest extends WordSpec with Matchers {
 
       "successfully parse the file" in {
         result shouldBe a[Success[_]]
+      }
 
-        import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.MonaMapper
-        println(MonaMapper.create.writeValueAsString(result.get))
-        fail("TODO Print out the Spectrum bean to check")
+      "assign ACCESSION as both spectrum ID and as base metadata" in {
+        result.get.id shouldBe "PR100162"
+        result.get.metaData.filter(m => m.name == "ACCESSION" && m.value == "PR100162") should not be empty
       }
 
       "format spectrum into string correctly" in {
         result.get.spectrum shouldBe "84.0444:772.6 102.0552:188.1 130.0502:187.7 148.061:86.1"
+      }
+
+      "extract CH$INCHI information into the biological compound metadata group" in {
+        result.get.biologicalCompound.inchi shouldBe "InChI=1S/C5H9NO4/c6-3(5(9)10)1-2-4(7)8/h3H,1-2,6H2,(H,7,8)(H,9,10)/t3-/m0/s1"
+      }
+
+      "extract CH$NAME information into the biological compound metadata group" in {
+        val expected =
+          """L-Glutamic acid
+            |Glu
+            |L-Glutamate
+            |alpha-Glutamic acid
+            |L-alpha-Aminoglutaric Acid
+            |(S)-2-Aminopentanedioic acid
+            |L-1-Aminopropane-1,3-dicarboxylic Acid
+            |1-Aminopropane-1,3-dicarboxylic acid
+            |Aciglut
+            |Glusate
+            |Glutacid
+            |Glutaminol
+            |Glutaton""".stripMargin
+        val names = result.get.biologicalCompound.names.map(_.name).mkString("\n")
+        names shouldBe expected
       }
     }
 
