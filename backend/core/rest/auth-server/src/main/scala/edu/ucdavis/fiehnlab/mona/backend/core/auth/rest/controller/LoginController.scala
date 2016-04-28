14f5c99b5c0693c811cadd27589b91151742817e
Diego Pedrosa <linuxmant@gmail.com>
2016-04-28 15:01:39 -0700

    fixed MassBankToSpectrumMapperTest name comparison, and changed response status on LoginController to 401 when invalid username/password are sent
    Former-commit-id: ad1a30b9cdd7e801b3cab57b4fead546fbe50a65

--- backend/core/rest/auth-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/controller/LoginController.scala
+++ backend/core/rest/auth-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/controller/LoginController.scala
@@ -9,6 +9,10 @@ import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.HelperTypes.{LoginRequest, LoginResponse}
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.service.LoginService
 import org.springframework.beans.factory.annotation.{Autowired, Qualifier}
+import org.springframework.http.{ResponseEntity, HttpStatus}
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.web.bind.annotation._
 
 /**
@@ -27,22 +31,36 @@ class LoginController extends LazyLogging {
 
   /**
     * preforms the actual login for us
+    * throws InvalidUserNameOrPasswordExecption (401 UNAUTHORIZED) if username or password are incorrect
+    *
     * @param request
     * @return
     */
   @RequestMapping(path = Array("/login"), method = Array(RequestMethod.POST))
   def login(@RequestBody request: LoginRequest): LoginResponse = {
     logger.debug(s"forwarding authentication request to: ${loginService}")
-    loginService.login(request)
+    try {
+      loginService.login(request)
+    } catch {
+      case ex: UsernameNotFoundException => throw new InvalidUserNameOrPasswordExecption
+      case ex: BadCredentialsException => throw new InvalidUserNameOrPasswordExecption
+    }
   }
 
   /**
     * provides us with public info about this token
+    *
     * @param request
     */
   @RequestMapping(path = Array("/info"), method = Array(RequestMethod.POST))
-  def tokenInfo(@RequestBody request:LoginResponse) = {
+  def tokenInfo(@RequestBody request: LoginResponse) = {
     loginService.info(request.token)
   }
 
 }
+
+/**
+  * Responds with a 401 instead of an exception when login with wrong credentials
+  */
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED, reason = "Invalid username or password")
+class InvalidUserNameOrPasswordExecption extends RuntimeException {}
