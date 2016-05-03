a3de867dd9a7c6d2f9ccfeadb374f850c886a995
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2016-05-03 15:22:09 -0700

    added new Angular components for search
    Former-commit-id: a411d7fd3f3f204153069b5f10bd8ecbc390cc51

---
+++ moa-client/app/scripts/controllers/query/searchFormController.js
@@ -0,0 +1,15 @@
+
+(function() {
+    'use strict';
+
+    angular.module('moaClientApp')
+        .controller('SearchFormController', SearchFormController);
+
+    /* @ngInject */
+    function SearchFormController($scope) {
+        $scope.query = {};
+
+
+    }
+
+})();
