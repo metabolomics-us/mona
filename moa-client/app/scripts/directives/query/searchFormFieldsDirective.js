be96d92da30dc3e95306fb61c0b40e05ad3791ee
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2016-05-05 15:15:56 -0700

    implementing checkbox selectiong controller
    Former-commit-id: 3917511d6543c927b4bc3d670e049333280039b0

--- moa-client/app/scripts/directives/query/searchFormFieldsDirective.js
+++ moa-client/app/scripts/directives/query/searchFormFieldsDirective.js
@@ -12,8 +12,12 @@
         return directive;
 
         function fieldsController($scope) {
+            $scope.selectAll ={
+                EI: false
+            };
+
             $scope.instrumentType = [
-                {EI: ['EI-B', 'EI-EBEB', 'GC-EI-QQ', 'GC-EI-TOF']},
+                {EI: ['EI-B', 'EI-EBEB', 'GC-EI-QQ', 'GC-EI-TOF'], selected: []},
                 {ESI: ['CE-ESI-TOF', 'ESI-FTICR', 'ESI-ITFT', 'ESI-ITTOF',
                        'ESI-QTOF', 'HPLC-ESI-TOF', 'LC-ESI-IT', 'LC-ESI-ITFT',
                        'LC-ESI-ITTOF', 'LC-ESI-Q', 'LC-ESI-QFT', 'LC-ESI-QIT',
@@ -23,11 +27,34 @@
                           'LC-APPI-QQ', 'MALDI-QIT', 'MALDI-TOF', 'MALDI-TOFTOF']}
             ];
 
+
             $scope.msType = ['MS', 'MS1', 'MS2', 'MS3', 'MS4'];
             $scope.selectMS = true;
 
             $scope.ionMode = ['Positive','Negative'];
             $scope.selectIon = true;
+
+            $scope.selectInstrumentType = function(index, instrument) {
+
+                if ($scope.selectAll.EI) {
+                    $scope.instrumentType[0].selected = $scope.instrumentType[0].EI;
+                }
+
+
+                else {
+                    var idx = $scope.instrumentType[index].selected.indexOf(instrument);
+
+                    if (idx > -1) {
+                        $scope.instrumentType[index].selected.splice(idx, 1);
+                    }
+                    else {
+                        $scope.instrumentType[index].selected.push(instrument);
+                    }
+                }
+
+                console.log($scope.selectAll.EI);
+                console.log($scope.instrumentType[index].selected);
+            }
         }
 
     }
