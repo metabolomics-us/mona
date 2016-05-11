d66f9fb75fb7263e41bd55a86aabe4c95fc0be3a
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-05-11 14:49:58 -0700

    working on curation runner tasks
    Former-commit-id: f6dc0b5b05ec2093c322c1de03ce4fa1c791503e

--- backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/compound/classifier/ClassifierProcessor.scala
+++ backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/compound/classifier/ClassifierProcessor.scala
@@ -2,16 +2,18 @@ package edu.ucdavis.fiehnlab.mona.backend.curation.processor.compound.classifier
 
 import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.{Compound, MetaData, Spectrum}
+import edu.ucdavis.fiehnlab.mona.backend.core.workflow.annotations.Step
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.{HttpStatus, ResponseEntity}
-import org.springframework.web.client.{RestOperations, RestTemplate}
+import org.springframework.web.client.{HttpClientErrorException, RestOperations, RestTemplate}
 
 import scala.collection.mutable.ArrayBuffer
 
 /**
   * this class connects to the external classifier processor and computes classification information for this spectra
   */
+@Step(description = "run's classification rules from the wishart's lab classyfire tool")
 class ClassifierProcessor extends ItemProcessor[Spectrum, Spectrum] with LazyLogging {
 
   @Autowired
@@ -40,62 +42,77 @@ class ClassifierProcessor extends ItemProcessor[Spectrum, Spectrum] with LazyLog
   def classify(compound: Compound): Compound = {
     val url = s"http://classyfire.wishartlab.com/entities/${compound.inchiKey}.json"
     logger.info(s"invoking url ${url}")
-    val result: ResponseEntity[Classifier] = restOperations.getForEntity(url, classOf[Classifier])
+    try {
+      val result: ResponseEntity[Classifier] = restOperations.getForEntity(url, classOf[Classifier])
 
-    if (result.getStatusCode == HttpStatus.OK) {
-      logger.debug("operation was successful, do something now...")
-      val classifier: Classifier = result.getBody
+      if (result.getStatusCode == HttpStatus.OK) {
+        logger.debug("operation was successful, do something now...")
+        val classifier: Classifier = result.getBody
 
-      val buffer: ArrayBuffer[MetaData] = ArrayBuffer()
+        val buffer: ArrayBuffer[MetaData] = ArrayBuffer()
 
-      buffer += MetaData("classification", computed = true, hidden = false, "kingdom", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.kingdom.name)
-      buffer += MetaData("classification", computed = true, hidden = false, "class", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.`class`.name)
-      buffer += MetaData("classification", computed = true, hidden = false, "subclass", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.subclass.name)
-      buffer += MetaData("classification", computed = true, hidden = false, "superclass", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.superclass.name)
+        if (classifier.kingdom != null)
+          buffer += MetaData("classification", computed = true, hidden = false, "kingdom", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.kingdom.name)
 
-      if(classifier.alternative_parents != null) {
-        classifier.alternative_parents.foreach { parent =>
-          buffer += MetaData("classification", computed = true, hidden = false, "alternative parent", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", parent.name)
+        if (classifier.`class` != null)
+          buffer += MetaData("classification", computed = true, hidden = false, "class", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.`class`.name)
+
+        if (classifier.subclass != null)
+          buffer += MetaData("classification", computed = true, hidden = false, "subclass", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.subclass.name)
+
+        if (classifier.superclass != null)
+          buffer += MetaData("classification", computed = true, hidden = false, "superclass", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.superclass.name)
+
+        if (classifier.alternative_parents != null) {
+          classifier.alternative_parents.foreach { parent =>
+            buffer += MetaData("classification", computed = true, hidden = false, "alternative parent", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", parent.name)
+          }
         }
-      }
 
-      if(classifier.intermediate_nodes != null) {
-        var level = 0
-        classifier.intermediate_nodes.foreach { parent =>
-          buffer += MetaData("classification", computed = true, hidden = false, s"direct parent level ${level}", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", parent.name)
-          level = level + 1
+        if (classifier.intermediate_nodes != null) {
+          var level = 0
+          classifier.intermediate_nodes.foreach { parent =>
+            buffer += MetaData("classification", computed = true, hidden = false, s"direct parent level ${level}", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", parent.name)
+            level = level + 1
+          }
         }
-      }
 
-      if(classifier.predicted_lipidmaps_terms != null) {
-        classifier.predicted_lipidmaps_terms.foreach{term =>
-          buffer += MetaData("classification", computed = true, hidden = false, "predicted lipidmaps", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", term)
+        if (classifier.predicted_lipidmaps_terms != null) {
+          classifier.predicted_lipidmaps_terms.foreach { term =>
+            buffer += MetaData("classification", computed = true, hidden = false, "predicted lipidmaps", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", term)
+          }
         }
-      }
 
 
-      if(classifier.substituents != null) {
-        classifier.substituents.foreach{term =>
-          buffer += MetaData("classification", computed = true, hidden = false, "substituents", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", term)
+        if (classifier.substituents != null) {
+          classifier.substituents.foreach { term =>
+            buffer += MetaData("classification", computed = true, hidden = false, "substituents", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", term)
+          }
         }
-      }
 
 
-      buffer += MetaData("classification", computed = true, hidden = false, "direct parent", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.direct_parent.name)
+        buffer += MetaData("classification", computed = true, hidden = false, "direct parent", null, null, "http://classyfire.wishartlab.com/entities/QASFUMOKHFSJGL-LAFRSMQTSA-N", classifier.direct_parent.name)
 
 
-      if(compound.classification != null) {
-        compound.classification.foreach {
-          buffer += _
+        if (compound.classification != null) {
+          compound.classification.foreach {
+            buffer += _
+          }
         }
+
+        compound.copy(classification = buffer.toArray)
+
       }
-
-      compound.copy(classification = buffer.toArray)
-
+      else {
+        logger.warn(s"received status code ${result.getStatusCode} for inchi ${compound.inchiKey}")
+        compound
+      }
     }
-    else {
-      logger.warn(s"received status code ${result.getStatusCode} for inchi ${compound.inchiKey}")
-      compound
+
+    catch {
+      case x: HttpClientErrorException =>
+        logger.warn(s"received status code ${x.getStatusCode} for inchi ${compound.inchiKey}")
+        compound
     }
   }
 }
