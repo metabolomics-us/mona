1d4cfdabb80cb7e3a33dc923ec6dd3c9cd3a28de
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-05-12 14:08:59 -0700

    working on massbank integration and related services
    Former-commit-id: b21f846a0b0e1bed284eaf190d262baaff495e45

---
+++ backend/core/rest/persistence-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/controller/massbank/MassbankController.scala
@@ -0,0 +1,44 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.controller.massbank
+
+import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.io.massbank.MassBankToSpectrumMapper
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo.repository.ISubmitterMongoRepository
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.service.persistence.SpectrumPersistenceService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.scheduling.annotation.Async
+import org.springframework.web.bind.annotation._
+
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
+
+/**
+  * Created by wohlgemuth on 5/12/16.
+  */
+@CrossOrigin
+@RestController
+@RequestMapping(Array("/rest/upload/massbank"))
+class MassbankController extends LazyLogging{
+
+  @Autowired
+  val spectrumPersistenceService: SpectrumPersistenceService = null
+
+  @Autowired
+  val submitterMongoRepository: ISubmitterMongoRepository = null
+
+  @RequestMapping(path = Array(""), method = Array(RequestMethod.POST))
+  @Async
+  def submit(@RequestHeader("Authorization") token:String, @RequestBody content:String  ) = {
+    val src: Source = Source.fromString(content)
+
+    val result: Try[Spectrum] = MassBankToSpectrumMapper.parse(src)
+
+    result match {
+      case Success(spectrum) =>
+        spectrumPersistenceService.save(spectrum)
+      case Failure(e) =>
+        logger.info(s"error parsing content: ${e.getMessage}",e)
+    }
+
+  }
+}
---
+++ backend/core/rest/persistence-server/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/controller/massbank/MassbankControllerTest.scala
@@ -0,0 +1,17 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.controller.massbank
+
+import org.scalatest.WordSpec
+
+/**
+  * Created by wohlgemuth on 5/12/16.
+  */
+class MassbankControllerTest extends WordSpec {
+
+  "MassbankControllerTest" should {
+
+    "submit" in {
+
+    }
+
+  }
+}
