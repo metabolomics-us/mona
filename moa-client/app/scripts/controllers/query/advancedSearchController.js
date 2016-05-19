bf3eca8182d66d1261a9179769533322f65109cb
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2016-05-19 12:14:27 -0700

    refactor keywordSearch
    Former-commit-id: 11b72f0fa1aea09261025e09a1436dfe3ffe5215

--- moa-client/app/scripts/controllers/query/advancedSearchController.js
+++ moa-client/app/scripts/controllers/query/advancedSearchController.js
@@ -5,29 +5,68 @@
         .controller('AdvancedSearchController', AdvancedSearchController);
 
     /* @ngInject */
-    function AdvancedSearchController($scope, $log, rsqlService, $http) {
+    function AdvancedSearchController($scope, $log, $http) {
 
         initForm();
+        $scope.queryStrings = [];
+
         function initForm() {
             $scope.compoundQuery = {
                 metadata: [],
-                operator: ['AND', 'AND', 'AND', 'AND']
+                operator: ['AND', 'AND', 'AND']
             };
 
             $scope.metadataQuery = {
                 metadata: [],
                 operator: 'AND'
+            };
+        }
+
+
+
+        $scope.submitAdvQuery = function () {
+
+            // compile and submit if user search for both compound and metadata
+            if (isCompoundQuery() && isMetaQuery()) {
+                // filter compound
+                filterCompound();
+                filterMetadata();
+
+
+            }
+            else if (isCompoundQuery) {
+
+            }
+            else if (isMetaQuery) {
+
+            }
+            else {
+                // inform user query is empty
+            }
+
+        };
+
+        function filterCompound() {
+            // filter compound
+            if (/^([A-Z]{14}-[A-Z]{10}-[A-Z,0-9])+$/.test($scope.compoundQuery.compound.name)) {
+                $scope.compoundQuery.compound.inchiKey = $scope.compoundQuery.compound.name;
+                delete $scope.compoundQuery.compound.name;
+            }
+            else {
+                delete $scope.compoundQuery.compound.inchiKey;
             }
         }
 
-        $scope.isMetaQuery = function () {
+        function filterMetadata() {
+            // it's already filtered
+        }
+
+        function isMetaQuery() {
             return $scope.metadataQuery.metadata.length > 1;
-        };
+        }
 
-        $scope.submitAdvQuery = function () {
-           $log.info($scope.queryOptions);
-        };
-
-
+        function isCompoundQuery() {
+            return typeof($scope.compoundQuery.name) === 'string';
+        }
     }
 })();
