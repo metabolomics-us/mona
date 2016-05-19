fb0bea582180cb761d771d0e237c23ca2b1531af
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2016-05-19 12:14:27 -0700

    refactor keywordSearch
    Former-commit-id: 11b72f0fa1aea09261025e09a1436dfe3ffe5215

--- moa-client/app/scripts/controllers/query/keywordSearchController.js
+++ moa-client/app/scripts/controllers/query/keywordSearchController.js
@@ -30,23 +30,22 @@
 
             $scope.instrumentType = [
                 {
-                    EI: [{name: 'EI-B'}, {name: 'EI-EBEB'}, {name: 'GC-EI-QQ'}, {name: 'GC-EI-TOF'}]
+                    SI: [{name: 'Liquid Chromatography (LC)'},
+                        {name: 'Gas Chromatography (GC)'},
+                        {name: 'Direct Injection/Infusion (DI)'},
+                        {name: 'Capillary Electrophoresis (CE)'}]
                 },
                 {
-                    ESI: [{name: 'CE-ESI-TOF'}, {name: 'ESI-FTICR'}, {name: 'ESI-ITFT'}, {name: 'ESI-ITTOF'},
-                        {name: 'ESI-QTOF'}, {name: 'HPLC-ESI-TOF'}, {name: 'LC-ESI-IT'}, {name: 'LC-ESI-ITFT'},
-                        {name: 'LC-ESI-ITTOF'}, {name: 'LC-ESI-Q'}, {name: 'LC-ESI-QFT'}, {name: 'LC-ESI-QIT'},
-                        {name: 'LC-ESI-QQ'}, {name: 'LC-ESI-QTOF'}, {name: 'LC-ESI-TOF'}, {name: 'UPLC-ESI-QTOF'}]
-                },
-                {
-                    Others: [{name: 'APCI-ITFT'}, {name: 'APCI-ITTOF'}, {name: 'CI-B'}, {name: 'FAB-B'},
-                        {name: 'FAB-EB'}, {name: 'FAB-EBEB'}, {name: 'FD-B'}, {name: 'FI-B'},
-                        {name: 'LC-APCI-Q'}, {name: 'LC-APCI-QTOF'}, {name: 'LC-APPI-QQ'},
-                        {name: 'MALDI-QIT'}, {name: 'MALDI-TOF'}, {name: 'MALDI-TOFTOF'}]
+                    IM: [{name: 'Atmospheric Pressure Chemical Ionization (APCI'},
+                        {name: 'Chemical Ionization (CI)'},
+                        {name: 'Electron Impact (EI)'},
+                        {name: 'Electrospray Ionization (ESI)'},
+                        {name: 'Fast Atom Bombardment (FAB)'},
+                        {name: 'Matrix Assisted Laser Desorption Ionization (MALDI)'}]
                 }
             ];
 
-            $scope.msType = [{name: 'MS'}, {name: 'MS1'}, {name: 'MS2'}, {name: 'MS3'}, {name: 'MS4'}];
+            $scope.msType = [{name: 'MS1'}, {name: 'MS2'}, {name: 'MS3'}, {name: 'MS4'}];
             $scope.ionMode = [{name: 'Positive'}, {name: 'Negative'}];
         }
 
@@ -58,20 +57,6 @@
 
         $scope.showSplash = function() {
             $scope.searchSplash = true;
-        };
-
-        /**
-         * handles when user check select all in UI. Our implementation in searchForm.html
-         * uses ng-model and ng-change. Since ng-change will updates the 'selected' property
-         * of the instrument name, we do not need to update on single selection. When user
-         * clicks submit, we will loop through instrument type, and add selected==true to query
-         */
-        $scope.insTypeSelectAll = function (index, insCategory) {
-            var curIns = $scope.instrumentType[index];
-
-            angular.forEach(curIns[insCategory], function (value, key) {
-                value.selected = curIns.selectAll;
-            });
         };
 
 
@@ -96,5 +81,4 @@
         };
 
     }
-
 })();
