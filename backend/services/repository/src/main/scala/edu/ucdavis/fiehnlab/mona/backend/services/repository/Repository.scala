01eeb320322bb1abd827b81ab488d8150606a5e7
Gert <wohlgemuth@ucdavis.edu>
2016-05-19 14:52:56 -0700

    working on directory listings
    Former-commit-id: 0936fa58bb6d97d382ca63616d8ce13c46f36b36

--- backend/services/repository/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/Repository.scala
+++ backend/services/repository/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/Repository.scala
@@ -10,8 +10,12 @@ import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.SwaggerConfig
 import edu.ucdavis.fiehnlab.mona.backend.services.repository.layout.{FileLayout, InchiKeyLayout}
 import edu.ucdavis.fiehnlab.mona.backend.services.repository.listener.RepositoryListener
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.handler.{ResourceHandler, ContextHandler}
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.embedded.{ConfigurableEmbeddedServletContainer, EmbeddedServletContainerCustomizer}
+import org.springframework.boot.context.embedded.jetty.{JettyServerCustomizer, JettyEmbeddedServletContainerFactory}
 import org.springframework.context.annotation.{Bean, Import}
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpMethod
@@ -24,20 +28,46 @@ import org.springframework.security.config.annotation.web.configuration.{EnableW
 @Order(1)
 @EnableWebSecurity
 @SpringBootApplication
-@Import(Array(classOf[MonaEventBusConfiguration],classOf[MonaNotificationBusConfiguration],classOf[SwaggerConfig]))
-class Repository extends WebSecurityConfigurerAdapter with LazyLogging{
+@Import(Array(classOf[MonaEventBusConfiguration], classOf[MonaNotificationBusConfiguration], classOf[SwaggerConfig]))
+class Repository extends WebSecurityConfigurerAdapter with LazyLogging {
 
   @Value("${mona.repository:#{systemProperties['java.io.tmpdir']}}")
-  val dir:String = ""
+  val dir: String = null
 
   @Bean
-  def fileLayout:FileLayout = {
-    val dir =new File(this.dir)
-    if(!dir.exists()) {
+  def fileLayout: FileLayout = {
+    val dir = new File(this.dir)
+    if (!dir.exists()) {
       logger.info(s"creating new mona repository ${dir} directory")
       dir.mkdirs()
     }
     new InchiKeyLayout(dir)
+  }
+
+  @Bean
+  def jettyCustomizer(jetty: JettyEmbeddedServletContainerFactory): EmbeddedServletContainerCustomizer = {
+    logger.info("configured jetty")
+    new EmbeddedServletContainerCustomizer() {
+
+      override def customize(configurableEmbeddedServletContainer: ConfigurableEmbeddedServletContainer): Unit = {
+        val container = configurableEmbeddedServletContainer.asInstanceOf[JettyEmbeddedServletContainerFactory]
+
+        container.addServerCustomizers(new JettyServerCustomizer {
+          override def customize(server: Server): Unit = {
+
+            val contextHandler = new ContextHandler("/repository")
+
+            val handler = new ResourceHandler()
+            handler.setDirectoriesListed(true)
+            handler.setResourceBase(dir)
+            contextHandler.setHandler(handler)
+
+            server.setHandler(contextHandler)
+            logger.info("configured embedded servlet container")
+          }
+        })
+      }
+    }
   }
 
   override def configure(web: WebSecurity): Unit = {
@@ -45,11 +75,12 @@ class Repository extends WebSecurityConfigurerAdapter with LazyLogging{
       //get is always available
       .antMatchers(HttpMethod.GET, "/repository/**")
   }
+
   @Bean
-  def repositoryListener(eventBus:EventBus[Spectrum], layout:FileLayout) : RepositoryListener = new RepositoryListener(eventBus,layout)
+  def repositoryListener(eventBus: EventBus[Spectrum], layout: FileLayout): RepositoryListener = new RepositoryListener(eventBus, layout)
 }
 
 
-object Repository{
+object Repository {
 
 }
