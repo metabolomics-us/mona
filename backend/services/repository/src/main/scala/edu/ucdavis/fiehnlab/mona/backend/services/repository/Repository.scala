63fba8a7b44e52c3a4f0b7aecaca1a91e93880c2
Gert <wohlgemuth@ucdavis.edu>
2016-05-18 20:53:32 -0700

    working on tests and renaming of classes
    Former-commit-id: 29e92cb8f2887eb5f6baa8b2b520667d3785d61e

---
+++ backend/services/repository/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/Repository.scala
@@ -0,0 +1,30 @@
+package edu.ucdavis.fiehnlab.mona.backend.services.repository
+
+import edu.ucdavis.fiehnlab.mona.backend.core.amqp.event.bus.EventBus
+import edu.ucdavis.fiehnlab.mona.backend.core.amqp.event.config.MonaEventBusConfiguration
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.client.config.RestClientConfig
+import edu.ucdavis.fiehnlab.mona.backend.curation.config.CurationConfig
+import edu.ucdavis.fiehnlab.mona.backend.services.repository.layout.{InchiKeyLayout, FileLayout}
+import edu.ucdavis.fiehnlab.mona.backend.services.repository.listener.RepositoryListener
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.{Import, Bean}
+import java.io.File
+
+/**
+  * Created by wohlg_000 on 5/18/2016.
+  */
+@SpringBootApplication
+@Import(Array(classOf[RestClientConfig],classOf[CurationConfig],classOf[MonaEventBusConfiguration]))
+class Repository {
+
+  @Bean
+  def fileLayout:FileLayout = new InchiKeyLayout(new File("target/testRepo"))
+
+  @Bean
+  def repositoryListener(eventBus:EventBus[Spectrum], layout:FileLayout) : RepositoryListener = new RepositoryListener(eventBus,layout)
+}
+
+object Repository{
+
+}
