4de2169f574e7b8b0ca8f44b2a3c95019c3fb7c9
Gert <wohlgemuth@ucdavis.edu>
2016-05-19 13:38:40 -0700

    working on directory listings
    Former-commit-id: 6aaa385029c2baa6e1e9ac73947a92ec62a9c00e

--- backend/core/rest/webhooks-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/webhooks/app/WebHookServer.scala
+++ backend/core/rest/webhooks-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/webhooks/app/WebHookServer.scala
@@ -1,16 +1,12 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.webhooks.app
 
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.config.JWTAuthenticationConfig
-import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.types.TokenSecret
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.SwaggerConfig
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.webhooks.config.WebHookSecurity
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.context.config.annotation.RefreshScope
-import org.springframework.context.annotation.{Bean, Import}
-import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.context.annotation.Import
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 /**
--- backend/services/repository/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/listener/RepositoryListenerTest.scala
+++ backend/services/repository/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/listener/RepositoryListenerTest.scala
@@ -2,32 +2,51 @@ package edu.ucdavis.fiehnlab.mona.backend.services.repository.listener
 
 import java.io.InputStreamReader
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.jayway.restassured.RestAssured
+import com.jayway.restassured.RestAssured._
+import com.jayway.restassured.config.ObjectMapperConfig
+import com.jayway.restassured.mapper.factory.Jackson2ObjectMapperFactory
+import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.event.Event
-import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.JSONDomainReader
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.{JSONDomainReader, MonaMapper}
 import edu.ucdavis.fiehnlab.mona.backend.services.repository.Repository
 import org.junit.runner.RunWith
 import org.scalatest.WordSpec
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.boot.test.{SpringApplicationConfiguration, WebIntegrationTest}
 import org.springframework.test.context.TestContextManager
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+import scala.util.Properties
+
 
 /**
   * Created by wohlg_000 on 5/18/2016.
   */
 @RunWith(classOf[SpringJUnit4ClassRunner])
 @SpringApplicationConfiguration(classes = Array(classOf[Repository]))
-class RepositoryListenerTest extends WordSpec {
+@WebIntegrationTest(Array("server.port=9999", "eureka.client.enabled:false"))
+class RepositoryListenerTest extends WordSpec with LazyLogging {
+
+  val port: Int = 9999
 
   @Autowired
   val repositoryListener: RepositoryListener = null
 
   val reader = JSONDomainReader.create[Spectrum]
+  val keepRunning = Properties.envOrElse("keep.server.running", "false").toBoolean
 
   new TestContextManager(this.getClass()).prepareTestInstance(this)
 
   "RepositoryListenerTest" must {
+
+    RestAssured.config = RestAssured.config().objectMapperConfig(ObjectMapperConfig.objectMapperConfig().jackson2ObjectMapperFactory(new Jackson2ObjectMapperFactory {
+      override def create(aClass: Class[_], s: String): ObjectMapper = MonaMapper.create
+    }))
+
+    RestAssured.baseURI = s"http://localhost:${port}/"
 
     val input = new InputStreamReader(getClass.getResourceAsStream("/monaRecord.json"))
     val spectrum: Spectrum = reader.read(input)
@@ -35,17 +54,38 @@ class RepositoryListenerTest extends WordSpec {
     "be able to receive data and " should {
 
       "create a file on an add event" in {
-        repositoryListener.received(Event(spectrum,eventType = Event.ADD))
+        repositoryListener.received(Event(spectrum, eventType = Event.ADD))
       }
 
       "create a file on an update event" in {
-        repositoryListener.received(Event(spectrum,eventType = Event.UPDATE))
+        repositoryListener.received(Event(spectrum, eventType = Event.UPDATE))
       }
 
       "delete a file on a delete event" in {
-        repositoryListener.received(Event(spectrum,eventType = Event.ADD))
-        repositoryListener.received(Event(spectrum,eventType = Event.DELETE))
+        repositoryListener.received(Event(spectrum, eventType = Event.ADD))
+        repositoryListener.received(Event(spectrum, eventType = Event.DELETE))
+      }
+    }
 
+    "be able to expose data as endpoint" should {
+
+      "be able to access /repository " in {
+        given().contentType("application/json; charset=UTF-8").when().log.all(true).get("/repository").then().statusCode(200)
+      }
+
+      "be able to access our spectra file" in {
+        repositoryListener.received(Event(spectrum, eventType = Event.ADD))
+        given().contentType("application/json; charset=UTF-8").when().log().all(true).get(s"/repository/${spectrum.compound(0).inchiKey}/${spectrum.id}.json").then().statusCode(200)
+      }
+
+
+      "if specified the server should stay online, this can be done using the env variable 'keep.server.running=true' " in {
+        if (keepRunning) {
+          while (keepRunning) {
+            logger.warn("waiting forever till you kill me!")
+            Thread.sleep(300000); // Every 5 minutes
+          }
+        }
       }
     }
   }
