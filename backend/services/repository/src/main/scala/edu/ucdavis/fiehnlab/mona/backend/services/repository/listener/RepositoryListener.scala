7f67c6f243d99c5fc6ae960565b96f5d6790072c
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-05-25 15:22:35 -0700

    added support for hosted git repository
    Former-commit-id: 383b437f212cb2867e1a879c60dd9bd953c7dbec

--- backend/services/repository/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/listener/RepositoryListener.scala
+++ backend/services/repository/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/listener/RepositoryListener.scala
@@ -6,6 +6,8 @@ import edu.ucdavis.fiehnlab.mona.backend.core.amqp.event.bus.{EventBus, EventBus
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.event.Event
 import edu.ucdavis.fiehnlab.mona.backend.services.repository.layout.FileLayout
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.Repository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.{ControllerAdvice, ExceptionHandl
 /**
   * Created by wohlg_000 on 5/18/2016.
   */
-class RepositoryListener @Autowired()(val bus: EventBus[Spectrum], val layout: FileLayout) extends EventBusListener[Spectrum](bus) {
+class RepositoryListener @Autowired()(val bus: EventBus[Spectrum], val layout: FileLayout, val git:Git) extends EventBusListener[Spectrum](bus) {
 
   /**
     * an element has been received from the bus and should be now processed
@@ -34,16 +36,42 @@ class RepositoryListener @Autowired()(val bus: EventBus[Spectrum], val layout: F
     logger.info(s"file is: ${file}")
     event.eventType match {
       //we only care about ADDs at this point in time
-      case (Event.ADD | Event.UPDATE) =>
+      case Event.ADD =>
         //writes the spectra to the while
         logger.info(s"\twriting spectrum with id ${event.content.id}")
         objectMapper.writeValue(file, event.content)
+        gitAdd(file)
+      case Event.UPDATE =>
+        //writes the spectra to the while
+        logger.info(s"\twriting spectrum with id ${event.content.id}")
+        objectMapper.writeValue(file, event.content)
+        gitUpdate(file)
+
       case Event.DELETE =>
         if (file.exists()) {
           logger.info(s"\tdeleted spectrum with id ${event.content.id}")
+          gitRemove(file)
           file.delete()
         }
       case _ => //ignore not of interest
     }
+  }
+
+  def gitAdd(file:File) = {
+    val spectrum:Spectrum = objectMapper.readValue(file,classOf[Spectrum])
+    git.add().addFilepattern(file.getAbsolutePath).call()
+    git.commit().setMessage(s"added spectra ${spectrum.id} to the repository").setAll(true).setCommitter(spectrum.submitter.emailAddress,spectrum.submitter.emailAddress).call()
+  }
+
+  def gitUpdate(file:File) = {
+    val spectrum:Spectrum = objectMapper.readValue(file,classOf[Spectrum])
+    git.add().addFilepattern(file.getAbsolutePath).call()
+    git.commit().setMessage(s"updated spectra ${spectrum.id} to the repository").setAll(true).setCommitter(spectrum.submitter.emailAddress,spectrum.submitter.emailAddress).call()
+  }
+
+  def gitRemove(file:File) = {
+    val spectrum:Spectrum = objectMapper.readValue(file,classOf[Spectrum])
+    git.rm().addFilepattern(file.getAbsolutePath).call()
+    git.commit().setMessage(s"removed spectra ${spectrum.id} from the repository").setAll(true).setCommitter(spectrum.submitter.emailAddress,spectrum.submitter.emailAddress).call()
   }
 }
