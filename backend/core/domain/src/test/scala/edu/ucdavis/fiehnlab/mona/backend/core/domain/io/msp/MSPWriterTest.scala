16060fb32106fe82d5edd3214ba95048c6de14ff
Gert <wohlgemuth@ucdavis.edu>
2016-05-27 16:32:26 -0700

    Added msp writer support
    Former-commit-id: 66999d7c6c91bbd25ed25dab47a724cfab3d42af

---
+++ backend/core/domain/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/domain/io/msp/MSPWriterTest.scala
@@ -0,0 +1,36 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.domain.io.msp
+
+import java.io.{StringReader, StringWriter, InputStreamReader}
+
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.{JSONDomainWriter, JSONDomainReader}
+import org.scalatest.WordSpec
+
+/**
+  * Created by wohlg_000 on 5/27/2016.
+  */
+class MSPWriterTest extends WordSpec {
+
+  "we should be able to create an instance of the writer" when {
+    val reader = JSONDomainReader.create[Spectrum]
+    val input = new InputStreamReader(getClass.getResourceAsStream("/monaRecord.json"))
+    val spectrum: Spectrum = reader.read(input)
+
+    val writer = new MSPWriter
+
+    "a writer" should {
+
+      val out = new StringWriter()
+
+      "write a spectrum out" in {
+
+        writer.write(spectrum,out)
+      }
+
+      "result is" in {
+        println(out.toString)
+      }
+
+    }
+  }
+}
