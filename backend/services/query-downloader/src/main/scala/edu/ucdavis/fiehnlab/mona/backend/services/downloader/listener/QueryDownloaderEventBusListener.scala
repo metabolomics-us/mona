fa111608030ac4c91e06a3193c2ee73a0e463564
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-05-31 14:30:07 -0700

    renamed query downloader listener
    Former-commit-id: 6ca4de7ec7d38301ecb35743f92a78fe5d978392

--- backend/services/query-downloader/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/listener/QueryDownloaderEventBusListener.scala
+++ backend/services/query-downloader/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/listener/QueryDownloaderEventBusListener.scala
@@ -3,12 +3,20 @@ package edu.ucdavis.fiehnlab.mona.backend.services.downloader.listener
 import edu.ucdavis.fiehnlab.mona.backend.core.amqp.event.bus.{EventBusListener, EventBus}
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.event.Event
+import edu.ucdavis.fiehnlab.mona.backend.services.downloader.service.QueryDownloaderService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
 
 /**
   * Created by sajjan on 5/16/16.
   */
-class QueryDownloaderListener @Autowired()(val bus: EventBus[Spectrum]) extends EventBusListener[Spectrum](bus) {
+@Component
+class QueryDownloaderEventBusListener @Autowired()(val bus: EventBus[Spectrum]) extends EventBusListener[Spectrum](bus) {
+
+  @Autowired
+  val queryDownloaderService: QueryDownloaderService = null
+
   /**
     * an element has been received from the bus and should be now processed
     *
