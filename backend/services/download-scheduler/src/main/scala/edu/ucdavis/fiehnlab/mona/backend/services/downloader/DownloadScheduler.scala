68a030c70941cc781701330dfbc48fe39a7aa8f0
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-06-07 14:06:16 -0700

    added download scheduler module
    Former-commit-id: 8e6620fc1adf9695dc2161f75f593384bdcf9633

---
+++ backend/services/download-scheduler/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/DownloadScheduler.scala
@@ -0,0 +1,56 @@
+package edu.ucdavis.fiehnlab.mona.backend.services.downloader
+
+import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.mona.backend.core.amqp.event.config.{MonaEventBusConfiguration, MonaNotificationBusConfiguration}
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.config.JWTAuthenticationConfig
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.service.RestSecurityService
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo.config.MongoConfig
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.SwaggerConfig
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Import
+import org.springframework.core.annotation.Order
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.{HttpSecurity, WebSecurity}
+import org.springframework.security.config.annotation.web.configuration.{WebSecurityConfigurerAdapter, EnableWebSecurity}
+import org.springframework.security.config.http.SessionCreationPolicy
+
+/**
+  * Created by sajjan on 5/25/16.
+  */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableWebSecurity
+@Order(5)
+@Import(Array(/*classOf[MonaEventBusConfiguration], classOf[MonaNotificationBusConfiguration], */classOf[MongoConfig],
+  classOf[JWTAuthenticationConfig], classOf[SwaggerConfig]))
+class DownloaderScheduler extends WebSecurityConfigurerAdapter with LazyLogging {
+
+  @Autowired
+  val restSecurityService: RestSecurityService = null
+
+  /**
+    * only authenticated users can schedule downloads from the system
+    *
+    * @param http
+    */
+  override final def configure(http: HttpSecurity): Unit = {
+    restSecurityService.prepare(http)
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+      .authorizeRequests()
+
+      // users need to be authenticated to schedule downloads
+      .antMatchers(HttpMethod.GET, "/rest/downloads/schedule/*").hasAuthority("USER")
+  }
+
+  override def configure(web: WebSecurity): Unit = {
+    web.ignoring().antMatchers(HttpMethod.GET, "/*")
+  }
+}
+
+object DownloadScheduler extends App {
+  new SpringApplication(classOf[DownloaderScheduler]).run()
+}
---
+++ backend/services/download-scheduler/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/controller/DownloadSchedulerControllerTest.scala
@@ -0,0 +1,56 @@
+package edu.ucdavis.fiehnlab.mona.backend.services.downloader.controller
+
+import com.jayway.restassured.RestAssured
+import com.jayway.restassured.RestAssured._
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.controller.AbstractSpringControllerTest
+import edu.ucdavis.fiehnlab.mona.backend.services.downloader.DownloaderScheduler
+import org.junit.runner.RunWith
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.TestContextManager
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+/**
+  * Created by sajjan on 5/26/16.
+  */
+@RunWith(classOf[SpringJUnit4ClassRunner])
+@SpringApplicationConfiguration(classes = Array(classOf[DownloaderScheduler]))
+class DownloadSchedulerControllerTest extends AbstractSpringControllerTest {
+
+  new TestContextManager(this.getClass).prepareTestInstance(this)
+
+  // Test query
+  val testQuery: String = "metaData=q='name==\"ion mode\" and value==negative'"
+
+
+  "DownloadControllerTest" should {
+    RestAssured.baseURI = s"http://localhost:$port/rest"
+
+    "fail if we are not logged in " must {
+      "schedule" in {
+        given().contentType("application/json; charset=UTF-8").when().get("/downloads/schedule?query=metaData=q='name==\"ion mode\" and value==negative'").then().statusCode(401)
+      }
+    }
+
+    "succeed if we are logged in " must {
+      "schedule by user" in {
+        val result = authenticate("test", "test-secret").contentType("application/json; charset=UTF-8").when().get(s"/downloads/schedule?query=$testQuery").then().statusCode(200).extract().body().as(classOf[DownloadJobScheduled])
+        assert(result.id == testQuery)
+      }
+
+      "scheduleByQuery by admin" in {
+        val result = authenticate().contentType("application/json; charset=UTF-8").when().get(s"/downloads/schedule?query=$testQuery").then().statusCode(200).extract().body().as(classOf[DownloadJobScheduled])
+        assert(result.id == testQuery)
+      }
+    }
+
+    "fail since we require a query to be passed " must {
+      "schedule by user" in {
+        authenticate("test", "test-secret").contentType("application/json; charset=UTF-8").when().get("/downloads/schedule").then().statusCode(400)
+      }
+
+      "scheduleByQuery by admin" in {
+        authenticate("test", "test-secret").contentType("application/json; charset=UTF-8").when().get("/downloads/schedule").then().statusCode(400)
+      }
+    }
+  }
+}
