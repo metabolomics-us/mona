173fff8a4e70a80d843e3f6c3ff6e30db01e2149
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-06-07 14:06:16 -0700

    added download scheduler module
    Former-commit-id: 8e6620fc1adf9695dc2161f75f593384bdcf9633

---
+++ backend/services/download-scheduler/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/TestConfig.scala
@@ -0,0 +1,17 @@
+package edu.ucdavis.fiehnlab.mona.backend.services.downloader
+
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.service.MongoLoginService
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.service.LoginService
+import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
+
+
+/**
+  * Created by sajjan on 5/26/16.
+  */
+@Configuration
+@ComponentScan(Array("edu/ucdavis/fiehnlab/mona/backend/services/downloader"))
+class TestConfig {
+
+  @Bean
+  def loginService: LoginService = new MongoLoginService
+}
