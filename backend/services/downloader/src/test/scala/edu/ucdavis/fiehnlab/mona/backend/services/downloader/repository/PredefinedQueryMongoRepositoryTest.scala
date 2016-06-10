c3f07681d3d59745575761e118865921b9287438
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-06-09 23:26:30 -0700

    test for predefined query mongo repository
    Former-commit-id: 38c29cefe69b47b29f06a22a2d7131db58323047

---
+++ backend/services/downloader/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/repository/PredefinedQueryMongoRepositoryTest.scala
@@ -0,0 +1,35 @@
+package edu.ucdavis.fiehnlab.mona.backend.services.downloader.repository
+
+import edu.ucdavis.fiehnlab.mona.backend.services.downloader.{Downloader, QueryExport}
+import org.junit.runner.RunWith
+import org.scalatest.WordSpec
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.{TestContextManager, TestPropertySource}
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+
+/**
+  * Created by sajjan on 6/9/16.
+  */
+@RunWith(classOf[SpringJUnit4ClassRunner])
+@TestPropertySource(locations=Array("classpath:application.properties"))
+@SpringApplicationConfiguration(classes = Array(classOf[Downloader]))
+class PredefinedQueryMongoRepositoryTest extends WordSpec {
+
+  @Autowired
+  val queryExportMongoRepository: QueryExportMongoRepository = null
+
+  new TestContextManager(this.getClass).prepareTestInstance(this)
+
+  "QueryExportMongoRepositoryTest" should {
+
+    "be able to save and retrive a QueryExport object" in {
+      queryExportMongoRepository.deleteAll()
+      queryExportMongoRepository.save(QueryExport("test", "", "json", 0, 0, null, null, null))
+
+      assert(queryExportMongoRepository.count() == 1)
+      assert(queryExportMongoRepository.findByLabel("test") != null)
+    }
+  }
+}
