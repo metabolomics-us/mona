825cde5994d7c566cda3b536d5588d08d918859a
Benjamin Nguyen <bnguyen@fry.genomecenter.ucdavis.edu>
2016-06-13 14:46:42 -0700

    working on queryStringbuilder tests
    Former-commit-id: 6fa739a6e2db39e2de8c40f0c457b2c64c6280f6

--- frontend/test/spec/services/query/rsqlParser-spec.js
+++ frontend/test/spec/services/query/rsqlParser-spec.js
@@ -1,15 +1,15 @@
 'use strict';
 
-describe('factory: Query String Builder - ', function() {
+describe('factory: Query String Builder - ', function () {
     beforeEach(module('moaClientApp'));
 
-    var rootScope, service;
+    var rootScope, service, cache;
 
-
-    beforeEach(inject(function(_queryStringBuilder_) {
+    beforeEach(inject(function (_queryStringBuilder_, $rootScope, _QueryCache_) {
         service = _queryStringBuilder_;
-
-        //rootScope = $injector.get('$rootScope');
+        rootScope = $rootScope;
+        cache = _QueryCache_;
+        this.query = {};
     }));
 
 
@@ -20,6 +20,7 @@ describe('factory: Query String Builder - ', function() {
             {name: 'meta-2', value: {ne: 'meta-2-value'}},
             {name: 'meta-3', value: {eq: 'meta-3-value'}}
         ],
+        operand: ['and', 'and'],
         tags: [
             {name: {eq: 'massbank'}},
             {name: {eq: 'GNPS'}},
@@ -27,58 +28,35 @@ describe('factory: Query String Builder - ', function() {
         ]
     };
 
-    it('can be inintialized', function() {
-        console.log(service);
-        expect(service).toBeDefined();
+
+    describe('it has 2 methods', function () {
+        it('has a build Query method', function () {
+            expect(service.buildQuery).toBeDefined();
+        });
+
+        it('has a build Advanced Query method', function () {
+            expect(service.buildAdvanceQuery).toBeDefined();
+        });
     });
 
-    it('can be inintialized', function() {
-        expect(query).toBeDefined();
+    describe('buildQuery parses query object and returns a RSQL string', function () {
+        beforeEach(function () {
+            this.query.compound = [{name: 'testCompoundName'}];
+            this.query.operand = ['and','and'];
+        });
+
+        it('throws a error when operands are not present', function() {
+            delete this.query.operand;
+            cache.setSpectraQuery(this.query);
+            expect(cache.setSpectraQuery).toThrow();
+        });
+
+        it('saves a query to the cache', function () {
+            cache.setSpectraQuery(this.query);
+            spyOn(cache, 'setSpectraQueryString').and.callThrough();
+            service.buildQuery();
+            expect(cache.setSpectraQueryString).toHaveBeenCalled();
+        });
     });
 
-    /*it('returns a query string for compound object', function() {
-        var result = rsqlParser.parseRSQL({compound: query.compound});
-        expect(result).toBe('biologicalCompound.names=q=\'name=="testCompoundName"\' or chemicalCompound.names=q=\'name=="testCompoundName"\'');
-    });
-
-    it('returns a query string for an inchiKey', function() {
-        var result = rsqlParser.parseRSQL({compound: {inchiKey: 1234567}});
-        expect(result).toBe('biologicalCompound=q=inchiKey=="1234567"\' or chemicalCompound=q=inchiKey=="1234567"\'');
-    });
-
-    it('returns a query string for compound AND inchiKey', function() {
-        var result = rsqlParser.parseRSQL({compound: {name: 'testCompoundName', inchiKey: 1234567}});
-        expect(result).toBe('biologicalCompound.names=q=\'name=="testCompoundName"\' or biologicalCompound=q=inchiKey=="1234567"\' or chemicalCompound.names=q=\'name=="testCompoundName"\' or chemicalCompound=q=inchiKey=="1234567"\'');
-    });
-
-    it('returns a query string for metadata object', function() {
-        var result =  rsqlParser.parseRSQL({metadata: query.metadata});
-        expect(result).toBe('metaData=q=\'name=="meta-1-value and metaData=q=\'name!="undefined and metaData=q=\'name=="meta-3-value');
-    });
-
-    it('returns a query string for tags object', function() {
-        var result = rsqlParser.parseRSQL({tags: query.tags});
-        expect(result).toBe('tags=q=\'name.eq==massbank"\' and tags=q=\'name.eq==GNPS"\' and tags=q=\'name.eq==LCMS"\'');
-    });
-
-    it ('returns a query string for compound AND metadata object', function() {
-        var result = rsqlParser.parseRSQL({compound: query.compound, metadata: query.metadata});
-        expect(result).toBe('biologicalCompound.names=q=\'name=="testCompoundName"\' or chemicalCompound.names=q=\'name=="testCompoundName"\' and metaData=q=\'name=="meta-1-value and metaData=q=\'name!="undefined and metaData=q=\'name=="meta-3-value');
-    });
-
-    it('returns a query string for compound AND tags object', function() {
-        var result = rsqlParser.parseRSQL({compound: query.compound, tags: query.tags});
-        expect(result).toBe('biologicalCompound.names=q=\'name=="testCompoundName"\' or chemicalCompound.names=q=\'name=="testCompoundName"\' and tags=q=\'name.eq==massbank"\' and tags=q=\'name.eq==GNPS"\' and tags=q=\'name.eq==LCMS"\'');
-    });
-
-    it('returns a query string for metadata AND tags object', function() {
-        var result = rsqlParser.parseRSQL({metadata: query.metadata, tags: query.tags});
-        expect(result).toBe('metaData=q=\'name=="meta-1-value and metaData=q=\'name!="undefined and metaData=q=\'name=="meta-3-value and tags=q=\'name.eq==massbank"\' and tags=q=\'name.eq==GNPS"\' and tags=q=\'name.eq==LCMS"\'');
-    });
-
-    it('returns a query string for compound AND metadata AND tags object', function() {
-        var result = rsqlParser.parseRSQL(query);
-        expect(result).toBe('biologicalCompound.names=q=\'name=="testCompoundName"\' or chemicalCompound.names=q=\'name=="testCompoundName"\' and metaData=q=\'name=="meta-1-value and metaData=q=\'name!="undefined and metaData=q=\'name=="meta-3-value and tags=q=\'name.eq==massbank"\' and tags=q=\'name.eq==GNPS"\' and tags=q=\'name.eq==LCMS"\'');
-    });
-*/
 });
