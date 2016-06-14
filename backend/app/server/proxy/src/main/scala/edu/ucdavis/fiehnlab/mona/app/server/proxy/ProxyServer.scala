8554c07083f157721213e14c23573decd7c5838a
Gert Wohlgemuth <wohlgemuth@ucdavis.edu>
2016-06-14 16:10:03 -0700

    finished the documentation proxy generation
    Former-commit-id: ce6f9e6828d667009748687b59f75728e26e1063

--- backend/app/server/proxy/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/proxy/ProxyServer.scala
+++ backend/app/server/proxy/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/proxy/ProxyServer.scala
@@ -1,19 +1,19 @@
 package edu.ucdavis.fiehnlab.mona.app.server.proxy
 
-import java.net.URL
-
 import com.netflix.zuul.ZuulFilter
-import com.netflix.zuul.context.RequestContext
 import com.typesafe.scalalogging.LazyLogging
-import org.springframework.beans.factory.annotation.Autowired
+import edu.ucdavis.fiehnlab.mona.app.server.proxy.documentation.SwaggerRedirectFilter
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.CustomErrorController
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
-import org.springframework.cloud.netflix.zuul.filters.ProxyRouteLocator
-import org.springframework.context.annotation.{Bean, Configuration}
-import org.springframework.stereotype.{Component, Controller}
+import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.stereotype.Controller
 import org.springframework.web.servlet.config.annotation.{CorsRegistry, WebMvcConfigurerAdapter}
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
@@ -26,42 +26,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @Controller
 @RefreshScope
 @EnableSwagger2
-class ProxyServer
+class ProxyServer{
+
+  @Bean
+  def rewriteFilter: ZuulFilter = {
+    new SwaggerRedirectFilter()
+  }
+}
 
 @Configuration
-class CorsConfig extends WebMvcConfigurerAdapter {
+class CorsConfig extends WebMvcConfigurerAdapter with LazyLogging {
   override def addCorsMappings(registry: CorsRegistry): Unit = registry.addMapping("/**")
-
 }
 
-@Component
-class RewriteFilter extends ZuulFilter with LazyLogging {
-  @Autowired
-  val routeLocator: ProxyRouteLocator = null
+@Configuration
+@Order(132)
+@ComponentScan(basePackageClasses = Array(classOf[CustomErrorController]))
+class Security extends WebSecurityConfigurerAdapter {
 
-
-  override def filterType(): String = "pre"
-
-  override def filterOrder(): Int = 100
-
-  override def shouldFilter(): Boolean = {
-    val request = RequestContext.getCurrentContext
-    request.get("requestURI") != null && request.get("requestURI").toString.startsWith("/documentation")
-
-  }
-
-  override def run(): AnyRef = {
-
-    val request = RequestContext.getCurrentContext
-    val uri = request.get("requestURI").toString
-
-    logger.info("need to redirect")
-
-    //HERE WE NEED TO REWRITE IT SOMEHOW
-    
-    null
+  override def configure(web: WebSecurity) {
+    web.ignoring.anyRequest()
   }
 }
+
 
 object ProxyServer extends App {
 
