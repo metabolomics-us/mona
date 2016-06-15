9554c0a73498ada281736dfda1d5eca24f74d661
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-06-15 14:12:50 -0700

    updated proxy server to fix 405 error messages (found in massbank uploader)
    Former-commit-id: a0acce71ec0ee7ec9d887a0a60bad2a4e2c712d4

--- backend/app/server/proxy/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/proxy/ProxyServer.scala
+++ backend/app/server/proxy/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/proxy/ProxyServer.scala
@@ -3,19 +3,14 @@ package edu.ucdavis.fiehnlab.mona.app.server.proxy
 import com.netflix.zuul.ZuulFilter
 import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.mona.app.server.proxy.documentation.SwaggerRedirectFilter
-import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.CustomErrorController
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.context.config.annotation.RefreshScope
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
-import org.springframework.context.annotation.{Bean, ComponentScan, Configuration}
-import org.springframework.core.annotation.Order
-import org.springframework.security.config.annotation.web.builders.WebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.stereotype.Controller
 import org.springframework.web.servlet.config.annotation.{CorsRegistry, WebMvcConfigurerAdapter}
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 /**
   * Created by wohlgemuth on 3/28/16.
@@ -25,8 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @EnableZuulProxy
 @Controller
 @RefreshScope
-@EnableSwagger2
-class ProxyServer{
+class ProxyServer {
 
   @Bean
   def rewriteFilter: ZuulFilter = {
@@ -39,21 +33,9 @@ class CorsConfig extends WebMvcConfigurerAdapter with LazyLogging {
   override def addCorsMappings(registry: CorsRegistry): Unit = registry.addMapping("/**")
 }
 
-@Configuration
-@Order(132)
-@ComponentScan(basePackageClasses = Array(classOf[CustomErrorController]))
-class Security extends WebSecurityConfigurerAdapter {
-
-  override def configure(web: WebSecurity) {
-    web.ignoring.anyRequest()
-  }
-}
-
 
 object ProxyServer extends App {
-
   // System.setProperty("spring.config.name", "proxy-service");
 
   new SpringApplication(classOf[ProxyServer]).run()
-
 }
