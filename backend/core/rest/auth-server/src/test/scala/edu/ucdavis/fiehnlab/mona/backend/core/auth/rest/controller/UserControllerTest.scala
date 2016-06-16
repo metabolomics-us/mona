6806945f45d8f74c71444b2d85cf34a8208cd8c6
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-06-15 19:08:06 -0700

    update UserRepository to add security to user creation and updating
    Former-commit-id: 3632cbf0868d500c71cbe3ddc06975d1b94267e2

--- backend/core/rest/auth-server/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/controller/UserControllerTest.scala
+++ backend/core/rest/auth-server/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/auth/rest/controller/UserControllerTest.scala
@@ -3,6 +3,8 @@ package edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.controller
 import java.io.InputStreamReader
 import java.util.Collections
 
+import com.jayway.restassured.RestAssured
+import com.jayway.restassured.RestAssured._
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.config.JWTAuthenticationConfig
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.repository.UserRepository
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.rest.config.AuthSecurityConfig
@@ -24,18 +26,18 @@ import scala.collection.JavaConverters._
   * Created by wohlgemuth on 4/4/16.
   */
 @RunWith(classOf[SpringJUnit4ClassRunner])
-@SpringApplicationConfiguration(classes = Array(classOf[JWTAuthenticationConfig], classOf[TestConfig],classOf[AuthSecurityConfig]))
+@SpringApplicationConfiguration(classes = Array(classOf[JWTAuthenticationConfig], classOf[TestConfig], classOf[AuthSecurityConfig]))
 class UserControllerTest extends AbstractGenericRESTControllerTest[User]("/users") {
 
   //required for spring and scala tes
-  new TestContextManager(this.getClass()).prepareTestInstance(this)
+  new TestContextManager(this.getClass).prepareTestInstance(this)
 
   /**
     * object to use for gets
     *
     * @return
     */
-  override def getValue: User = User("admi312312n", "a13123dmin", List[Role](Role("ADMIN")).asJava)
+  override def getValue: User = User("testadmin", "admin", List[Role](Role("ADMIN")).asJava)
 
   /**
     * returns an id for us for testing
@@ -44,5 +46,34 @@ class UserControllerTest extends AbstractGenericRESTControllerTest[User]("/users
     */
   override def getId: String = getValue.username
 
-  override val requiresAuthForAllRequestes: Boolean = true
+  override val requiresAuthForAllRequests: Boolean = true
+
+  override val saveRequiresAuthentication: Boolean = false
+
+
+  "UserController" should {
+    RestAssured.baseURI = s"http://localhost:$port/rest"
+
+    "save does not require authentication" in {
+      given().contentType("application/json; charset=UTF-8").body(getValue).when().post(s"/users").then().statusCode(200)
+    }
+
+    "save should not allow a user to create an admin user" in {
+      val user: User = given().contentType("application/json; charset=UTF-8").body(getValue).when().post(s"/users").then().statusCode(200).extract().body().as(classOf[User])
+
+      assert(user.roles.asScala.forall(_.name != "ADMIN"))
+    }
+
+    "put should not allow a non-admin to add an admin role" in {
+      val user: User = authenticate("testadmin", "admin").contentType("application/json; charset=UTF-8").body(getValue).when().put(s"/users/$getId").then().statusCode(200).extract().body().as(classOf[User])
+
+      assert(user.roles.asScala.forall(_.name != "ADMIN"))
+    }
+
+    "put should be able to add admin role to a user if initiated by admin" in {
+      val user: User = authenticate().contentType("application/json; charset=UTF-8").body(getValue).when().put(s"/users/$getId").then().statusCode(200).extract().body().as(classOf[User])
+
+      assert(user.roles.asScala.exists(_.name == "ADMIN"))
+    }
+  }
 }
