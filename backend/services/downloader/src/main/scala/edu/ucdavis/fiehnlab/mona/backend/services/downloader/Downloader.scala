d6ec03456f51e4d38b302f7aed939c1a878059d5
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-06-22 16:41:50 -0700

    added rabbitmq listener to downloader module
    Former-commit-id: 773592eb00e0b5f87651c9f6054921ef7a167686

--- backend/services/downloader/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/Downloader.scala
+++ backend/services/downloader/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/Downloader.scala
@@ -3,7 +3,12 @@ package edu.ucdavis.fiehnlab.mona.backend.services.downloader
 import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.mona.backend.core.amqp.event.config.{MonaEventBusConfiguration, MonaNotificationBusConfiguration}
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo.config.MongoConfig
+import edu.ucdavis.fiehnlab.mona.backend.services.downloader.listener.DownloadListener
 import edu.ucdavis.fiehnlab.mona.backend.services.downloader.repository.QueryExportMongoRepository
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
+import org.springframework.amqp.support.converter.MessageConverter
+import org.springframework.beans.factory.annotation.{Qualifier, Autowired}
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.{Bean, Import}
@@ -14,8 +19,26 @@ import org.springframework.context.annotation.{Bean, Import}
 @SpringBootApplication
 @Import(Array(classOf[MongoConfig], classOf[MonaEventBusConfiguration], classOf[MonaNotificationBusConfiguration]))
 class Downloader extends LazyLogging {
+
+  @Autowired
+  @Qualifier("spectra-download-queue")
+  val queueName: String = null
+
   @Bean
-  def queryExportMongoRepository: QueryExportMongoRepository = null
+  val queryExportMongoRepository: QueryExportMongoRepository = null
+
+  @Bean
+  val downloadListener: DownloadListener = new DownloadListener
+
+  @Bean
+  def container(connectionFactory: ConnectionFactory, listener: DownloadListener, messageConverter: MessageConverter): SimpleMessageListenerContainer = {
+    val container = new SimpleMessageListenerContainer()
+    container.setConnectionFactory(connectionFactory)
+    container.setMessageListener(listener)
+    container.setMessageConverter(messageConverter)
+    container.setQueueNames(queueName)
+    container
+  }
 }
 
 object Downloader extends App {
