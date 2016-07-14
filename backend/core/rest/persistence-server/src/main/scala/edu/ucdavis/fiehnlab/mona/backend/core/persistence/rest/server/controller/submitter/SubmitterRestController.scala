4ca7ea92dd02c17652b1b883c994344af4e4f996
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-07-14 13:44:14 -0700

    update submitter controller to prevent non-admin users from creating and updating submitter accounts other than their own
    Former-commit-id: aabe223e051617f458b95fd341bc1d7bdcac346f

--- backend/core/rest/persistence-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/controller/submitter/SubmitterRestController.scala
+++ backend/core/rest/persistence-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/controller/submitter/SubmitterRestController.scala
@@ -1,11 +1,20 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.controller.submitter
 
+import java.util.Collections
+import java.util.concurrent.Future
+import javax.servlet.http.HttpServletRequest
+
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.types.User
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.HelperTypes.LoginInfo
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Submitter
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.service.LoginService
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo.repository.ISubmitterMongoRepository
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.controller.GenericRESTController
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.web.bind.annotation.{CrossOrigin, RequestMapping, RestController}
+import org.springframework.http.{ResponseEntity, HttpStatus}
+import org.springframework.scheduling.annotation.{AsyncResult, Async}
+import org.springframework.web.bind.annotation._
 
 /**
   * Created by wohlgemuth on 3/7/16.
@@ -14,6 +23,60 @@ import org.springframework.web.bind.annotation.{CrossOrigin, RequestMapping, Res
 @RestController
 @RequestMapping(Array("/rest/submitters"))
 class SubmitterRestController extends GenericRESTController[Submitter] {
+
+  @Autowired
+  val httpServletRequest: HttpServletRequest = null
+
+  @Autowired
+  val loginService: LoginService = null
+
+  /**
+    * saves a spectra or updates it. This will depend on the utilized repository
+    *
+    * @param submitter
+    * @return
+    */
+  @Async
+  @RequestMapping(path = Array(""), method = Array(RequestMethod.POST))
+  @ResponseBody
+  override def save(@RequestBody submitter: Submitter): AsyncResult[Submitter] = {
+    val token: String = httpServletRequest.getHeader("Authorization").split(" ").last
+    val loginInfo: LoginInfo = loginService.info(token)
+
+    if(loginInfo.roles.contains("ADMIN")) {
+      super.save(submitter)
+    } else {
+      super.save(submitter.copy(emailAddress = loginInfo.username))
+    }
+  }
+
+
+  /**
+    * saves the provided submitter at the given path
+    *
+    * @param id
+    * @param submitter
+    * @return
+    */
+  @Async
+  @RequestMapping(path = Array("/{id}"), method = Array(RequestMethod.PUT))
+  @ResponseBody
+  override def put(id: String, submitter: Submitter): Future[ResponseEntity[Submitter]] = {
+    val token: String = httpServletRequest.getHeader("Authorization").split(" ").last
+    val loginInfo: LoginInfo = loginService.info(token)
+
+    if (loginInfo.roles.contains("ADMIN")) {
+      super.put(id, submitter)
+    } else {
+      val existingUser: Submitter = submitterMongoRepository.findOne(id)
+
+      if (loginInfo.username == existingUser.emailAddress) {
+        super.put(id, submitter)
+      } else {
+        new AsyncResult[ResponseEntity[Submitter]](new ResponseEntity[Submitter](HttpStatus.FORBIDDEN))
+      }
+    }
+  }
 
   /**
     * this is the utilized repository, doing all the heavy lifting
