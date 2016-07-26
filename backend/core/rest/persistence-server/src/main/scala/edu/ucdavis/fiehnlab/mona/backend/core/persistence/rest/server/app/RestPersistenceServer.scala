8f33078e4fcfc12e0fac0ff1e94c2d9db1bf2f99
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-07-26 16:18:22 -0700

    fixed spring dependency issue in persistence service
    Former-commit-id: 2187b911aef4d56c0cf0e2dc53ebba9e16082733

--- backend/core/rest/persistence-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/app/RestPersistenceServer.scala
+++ backend/core/rest/persistence-server/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/app/RestPersistenceServer.scala
@@ -1,8 +1,10 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.app
 
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.config.JWTAuthenticationConfig
+import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.service.MongoLoginService
 import edu.ucdavis.fiehnlab.mona.backend.core.auth.jwt.types.TokenSecret
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.service.LoginService
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.SwaggerConfig
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.server.config.RestServerConfig
 import org.springframework.beans.factory.annotation.Value
@@ -20,9 +22,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @EnableDiscoveryClient
 @Import(Array(classOf[RestServerConfig],classOf[JWTAuthenticationConfig],classOf[SwaggerConfig]))
 @EnableSwagger2
-class RestPersistenceServer
+class RestPersistenceServer {
 
-object RestPersistenceServer extends App{
+  @Bean
+  def loginService: LoginService = new MongoLoginService
+}
+
+object RestPersistenceServer extends App {
   System.setProperty("spring.config.name", "persistence-service")
   new SpringApplication(classOf[RestPersistenceServer]).run()
 }
