a9acf2e1c1568fd8a11f469de95083b89fbf9573
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-08-26 13:32:23 -0700

    convert from InChI in compute compound properties curation rule if no mol data is specified
    Former-commit-id: c6acd2038070309cc9daf591a064360e334777c2

--- backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/compound/CalculateCompoundProperties.scala
+++ backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/compound/CalculateCompoundProperties.scala
@@ -1,18 +1,19 @@
 package edu.ucdavis.fiehnlab.mona.backend.curation.processor.compound
 
-import java.io.StringReader
+import java.io.{StringReader, StringWriter}
 
 import com.typesafe.scalalogging.LazyLogging
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.{Compound, MetaData, Spectrum}
 import edu.ucdavis.fiehnlab.mona.backend.core.workflow.annotations.Step
 import edu.ucdavis.fiehnlab.mona.backend.curation.processor.RemoveComputedData
-import edu.ucdavis.fiehnlab.mona.backend.curation.processor.compound.cts.{FetchCTSCompoundData}
+import edu.ucdavis.fiehnlab.mona.backend.curation.processor.compound.cts.FetchCTSCompoundData
 import edu.ucdavis.fiehnlab.mona.backend.curation.util.CommonMetaData
-import org.openscience.cdk.inchi.{InChIGenerator, InChIGeneratorFactory}
+import net.sf.jniinchi.INCHI_RET
+import org.openscience.cdk.inchi.{InChIGenerator, InChIGeneratorFactory, InChIToStructure}
 import org.openscience.cdk.smiles.SmilesGenerator
 import org.openscience.cdk.{AtomContainer, DefaultChemObjectBuilder}
 import org.openscience.cdk.interfaces.{IAtomContainer, IChemObject, IMolecularFormula}
-import org.openscience.cdk.io.MDLV2000Reader
+import org.openscience.cdk.io.{MDLV2000Reader, MDLV2000Writer}
 import org.openscience.cdk.tools.CDKHydrogenAdder
 import org.openscience.cdk.tools.manipulator.{AtomContainerManipulator, MolecularFormulaManipulator}
 import org.springframework.batch.item.ItemProcessor
@@ -31,6 +32,23 @@ class CalculateCompoundProperties extends ItemProcessor[Spectrum, Spectrum] with
     spectrum.copy(compound = updatedCompound)
   }
 
+  protected def inchiToMol(inchi: String): String = {
+    val inchiGeneratorFactory: InChIGeneratorFactory = InChIGeneratorFactory.getInstance()
+    val inChIToStructure: InChIToStructure = inchiGeneratorFactory.getInChIToStructure(inchi, DefaultChemObjectBuilder.getInstance())
+
+    if (inChIToStructure.getReturnStatus != INCHI_RET.OKAY && inChIToStructure.getReturnStatus != INCHI_RET.WARNING) {
+      null
+    } else {
+      val stringWriter: StringWriter = new StringWriter()
+      val mdlWriter: MDLV2000Writer = new MDLV2000Writer(stringWriter)
+
+      mdlWriter.writeMolecule(inChIToStructure.getAtomContainer)
+      mdlWriter.close()
+
+      stringWriter.toString
+    }
+  }
+
   def calculateCompoundProperties(compound: Compound): Compound = {
     // Updated metadata to add to this compound
     val metaData: ArrayBuffer[MetaData] = new ArrayBuffer[MetaData]()
@@ -38,47 +56,60 @@ class CalculateCompoundProperties extends ItemProcessor[Spectrum, Spectrum] with
     compound.metaData.foreach(x => metaData.append(x))
 
 
-    // Read MOL data
-    val reader = new MDLV2000Reader(new StringReader(compound.molFile))
-    val molecule: IAtomContainer = reader.read(new AtomContainer())
-
-    logger.debug(s"received mol: \n ${compound.molFile}")
-
-    // Update molecule
-    AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(molecule)
-    CDKHydrogenAdder.getInstance(DefaultChemObjectBuilder.getInstance()).addImplicitHydrogens(molecule)
-    AtomContainerManipulator.convertImplicitToExplicitHydrogens(molecule)
+    val molFile: String =
+      if (compound.molFile != null) compound.molFile
+      else if (compound.inchi != null) inchiToMol(compound.inchi)
+      else null
 
 
-    // Get molecular properties
-    val molecularFormula: IMolecularFormula = MolecularFormulaManipulator.getMolecularFormula(molecule)
+    if (molFile == null) {
+      compound
+    }
 
-    metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.MOLECULAR_FORMULA,
-      null, null, null, MolecularFormulaManipulator.getString(molecularFormula)))
+    else {
+      // Read MOL data
+      val reader = new MDLV2000Reader(new StringReader(molFile))
+      val molecule: IAtomContainer = reader.read(new AtomContainer())
 
-    metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.TOTAL_EXACT_MASS,
-      null, null, null, MolecularFormulaManipulator.getTotalExactMass(molecularFormula)))
+      logger.debug(s"received mol:\n $molFile")
 
-
-    // Calculate InChI
-    val inchiGenerator: InChIGenerator = InChIGeneratorFactory.getInstance().getInChIGenerator(molecule)
-
-    metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.INCHI_CODE,
-      null, null, null, inchiGenerator.getInchi))
-
-    logger.debug(s"return status is: ${inchiGenerator.getReturnStatus}")
-    metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.INCHI_KEY,
-      null, null, null, inchiGenerator.getInchiKey))
+      // Update molecule
+      AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(molecule)
+      CDKHydrogenAdder.getInstance(DefaultChemObjectBuilder.getInstance()).addImplicitHydrogens(molecule)
+      AtomContainerManipulator.convertImplicitToExplicitHydrogens(molecule)
 
 
-    // Calculate SMILES
-    metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.SMILES,
-      null, null, null, SmilesGenerator.unique().create(molecule)))
+      // Get molecular properties
+      val molecularFormula: IMolecularFormula = MolecularFormulaManipulator.getMolecularFormula(molecule)
+
+      metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.MOLECULAR_FORMULA,
+        null, null, null, MolecularFormulaManipulator.getString(molecularFormula)))
+
+      metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.TOTAL_EXACT_MASS,
+        null, null, null, MolecularFormulaManipulator.getTotalExactMass(molecularFormula)))
 
 
-    // Return compound with update metadata
-    compound.copy(
-      metaData = metaData.toArray
-    )
+      // Calculate InChI
+      val inchiGenerator: InChIGenerator = InChIGeneratorFactory.getInstance().getInChIGenerator(molecule)
+
+      metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.INCHI_CODE,
+        null, null, null, inchiGenerator.getInchi))
+
+      logger.debug(s"return status is: ${inchiGenerator.getReturnStatus}")
+      metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.INCHI_KEY,
+        null, null, null, inchiGenerator.getInchiKey))
+
+
+      // Calculate SMILES
+      metaData.append(MetaData("computed", computed = true, hidden = false, CommonMetaData.SMILES,
+        null, null, null, SmilesGenerator.unique().create(molecule)))
+
+
+      // Return compound with update metadata
+      compound.copy(
+        molFile = molFile,
+        metaData = metaData.toArray
+      )
+    }
   }
 }
