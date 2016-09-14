74b84e2a6cba477964e8059cef8634a6a0ed5c46
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-09-14 10:48:43 -0700

    added abstract downloader and implementations for different output formats
    Former-commit-id: 7c9931cb967fa84761d183da73468b403f764cbe

---
+++ backend/services/downloader/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/downloader/writer/JSONDownloader.scala
@@ -0,0 +1,47 @@
+package edu.ucdavis.fiehnlab.mona.backend.services.downloader.writer
+
+import java.io.BufferedWriter
+import java.nio.file.Path
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.MonaMapper
+import org.springframework.stereotype.Component
+
+/**
+  * Created by sajjan on 9/13/16.
+  */
+@Component
+class JSONDownloader extends AbstractDownloader {
+
+  val objectMapper: ObjectMapper = MonaMapper.create
+
+
+  /**
+    * File format prefix
+    *
+    * @return
+    */
+  override def getFilePrefix: String = "[\n"
+
+  /**
+    * File format suffix
+    *
+    * @return
+    */
+  override def getFileSuffix: String = "\n]"
+
+  /**
+    * File format separator
+    *
+    * @return
+    */
+  override def getFileSeparator: String = ",\n"
+
+  /**
+    *
+    */
+  override def writeSpectrum(spectrum: Spectrum, bufferedWriter: BufferedWriter) = {
+    bufferedWriter.write(objectMapper.writeValueAsString(spectrum))
+  }
+}
