69336ea2b3c350faee7afd93c256f2dbf38aa7b7
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-09-22 10:09:19 -0700

    update news feed in upload status page to show a list of recently uploaded spectra from user
    Former-commit-id: 9263361e101b50bf1711b326036f8cb53b49dd25

--- backend/app/server/proxy/src/main/angularjs/scripts/controllers/upload/spectraUploadStatusController.js
+++ backend/app/server/proxy/src/main/angularjs/scripts/controllers/upload/spectraUploadStatusController.js
@@ -4,24 +4,42 @@
 
 (function() {
     'use strict';
-    SpectraUploadController.$inject = ['$scope', '$rootScope', 'AuthenticationService', 'UploadLibraryService'];
+    SpectraUploadController.$inject = ['$scope', '$rootScope', 'AuthenticationService', 'UploadLibraryService', '$log'];
     angular.module('moaClientApp')
         .controller('SpectraUploadController', SpectraUploadController);
 
     /* @ngInject */
-    function SpectraUploadController($scope, $rootScope, AuthenticationService, UploadLibraryService) {
+    function SpectraUploadController($scope, $rootScope, AuthenticationService, UploadLibraryService, $log) {
+
+        $scope.errors = [];
+        $scope.spectra = [];
+
         /**
          * Check whether the user is uploading, and open the upload dialog accordingly
          */
         (function() {
             $scope.spectraUploaded = UploadLibraryService.uploadedSpectraCount > 0;
 
-            if (AuthenticationService.isLoggedIn() && !$scope.spectraUploaded) {
-                // Not using the old upload wizard anymore
-                //$scope.uploadSpectraDialog()
-            } else if (!AuthenticationService.isLoggedIn() && !AuthenticationService.isLoggingIn()) {
-                $rootScope.$broadcast('auth:login');
-            }
+
+            // if (AuthenticationService.isLoggedIn() && !$scope.spectraUploaded) {
+            //     // Not using the old upload wizard anymore
+            //     //$scope.uploadSpectraDialog()
+            // } else if (!AuthenticationService.isLoggedIn() && !AuthenticationService.isLoggingIn()) {
+            //     $rootScope.$broadcast('auth:login');
+            // }
         })();
+
+
+        $scope.$on('spectra:uploadsuccess', function(event, data) {
+            $log.info("SUCCESS!");
+            $log.info(data);
+
+            $scope.spectra.push(data.data)
+        });
+
+        $scope.$on('spectra:uploaderror', function(event, error) {
+            $log.info("ERROR!");
+            $log.info(error);
+        });
     }
 })();
