d832ca69fd5a441c9d1b2763d59830872f7c1d2c
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-09-23 11:34:58 -0700

    updated chromatography identification curation rule to identify CE/MS, improve logging and updated tests
    Former-commit-id: ce018c9509a02e7fe882ef1a85344c38fd465615

--- backend/core/curation/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/instrument/IdentifyChromotographyTest.scala
+++ backend/core/curation/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/instrument/IdentifyChromotographyTest.scala
@@ -18,20 +18,34 @@ class IdentifyChromotographyTest extends WordSpec {
 
     val processor = new IdentifyChromatography
 
-    val input = new InputStreamReader(getClass.getResourceAsStream("/monaRecord.json"))
-
-    val spectrumGiven: Spectrum = reader.read(input)
-
-    "given a spectra" must {
-
-      val processedSpectrum = processor.process(spectrumGiven)
+    "given an LC/MS spectrum" must {
+      val lcmsRecord = new InputStreamReader(getClass.getResourceAsStream("/monaRecord.json"))
+      val spectrum: Spectrum = reader.read(lcmsRecord)
+      val processedSpectrum = processor.process(spectrum)
 
       "identify spectrum as LC/MS" in {
         assert(processedSpectrum.tags.exists(_.text == CommonTags.LCMS_SPECTRUM))
       }
 
-      "not identify spectrum as GC/MS" in {
+      "not identify spectrum as GC/MS or CE/MS" in {
         assert(processedSpectrum.tags.forall(_.text != CommonTags.GCMS_SPECTRUM))
+        assert(processedSpectrum.tags.forall(_.text != CommonTags.CEMS_SPECTRUM))
+      }
+    }
+
+
+    "given an GC/MS spectrum" must {
+      val gcmsRecord = new InputStreamReader(getClass.getResourceAsStream("/gcmsRecord.json"))
+      val spectrum: Spectrum = reader.read(gcmsRecord)
+      val processedSpectrum = processor.process(spectrum)
+
+      "identify spectrum as GC/MS" in {
+        assert(processedSpectrum.tags.exists(_.text == CommonTags.GCMS_SPECTRUM))
+      }
+
+      "not identify spectrum as LC/MS or CE/MS" in {
+        assert(processedSpectrum.tags.forall(_.text != CommonTags.LCMS_SPECTRUM))
+        assert(processedSpectrum.tags.forall(_.text != CommonTags.CEMS_SPECTRUM))
       }
     }
   }
