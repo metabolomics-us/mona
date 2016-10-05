323a5c20fda6490f74a36242dbf4d4dde8b4a54c
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-10-05 10:41:11 -0700

    enable discovery of repository service
    Former-commit-id: 802bca41972b8073e53a2b6a15ab00f462d72bda

--- backend/services/repository/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/WebRepository.scala
+++ backend/services/repository/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/services/repository/WebRepository.scala
@@ -20,6 +20,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.embedded._
 import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.{Bean, Configuration, DependsOn, Import}
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.WebSecurity
@@ -30,6 +31,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
   * Created by wohlg_000 on 5/18/2016.
   */
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import(Array(classOf[MonaEventBusConfiguration], classOf[MonaNotificationBusConfiguration]))
 class WebRepository extends WebSecurityConfigurerAdapter with LazyLogging {
 
