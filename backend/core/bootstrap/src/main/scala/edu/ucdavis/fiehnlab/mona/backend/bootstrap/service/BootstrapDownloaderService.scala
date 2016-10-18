05d9a77b0033ffda3e2e41605584c9affac7a1a8
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-10-17 17:06:52 -0700

    updated to predefined query options
    Former-commit-id: 2b1491e43417de1c444d94232617d2d60b67482a

--- backend/core/bootstrap/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/bootstrap/service/BootstrapDownloaderService.scala
+++ backend/core/bootstrap/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/bootstrap/service/BootstrapDownloaderService.scala
@@ -37,8 +37,6 @@ class BootstrapDownloaderService extends LazyLogging {
     * @return
     */
   def createPredefinedQueries() = {
-    // Create export for all spectra
-    saveQuery("All Spectra", "All Spectra", "")
 
     // Create library exports
     createLibraryQueries()
@@ -53,14 +51,14 @@ class BootstrapDownloaderService extends LazyLogging {
     * @return
     */
   def createLibraryQueries() = {
-    saveQuery("Libraries - MassBank", "MassBank Spectral Database", "tags.text==massbank")
-    saveQuery("Libraries - ReSpect", "RIKEN MS^n Spectral Database for Phytochemicals", "tags.text==respect")
-    saveQuery("Libraries - HMDB", "Human Metabolome Database", "tags.text==hmdb")
-    saveQuery("Libraries - GNPS", "Global Natural Product Social Molecular Networking Library", "tags.text==gnps")
-    saveQuery("Libraries - LipidBlast", "LipidBlast In-Silico MS/MS Database for Lipid Identification", "tags.text==lipidblast")
-    saveQuery("Libraries - FAHFA", "Fatty Acid ester of Hydroxyl Fatty Acid In-Silico Library", "tags.text==fahfa")
-    saveQuery("Libraries - iTree", "iTree Mass Spectral Tree Library", "tags.text==itree")
-    saveQuery("Libraries - RTX5 Fiehnlib", "RTX5 Fiehn Lab Metabolic Profiling Library", "tags.text==fiehnlib")
+    saveQuery("Libraries - MassBank", "MassBank Spectral Database", "tags.text==MassBank")
+    saveQuery("Libraries - ReSpect", "RIKEN MS^n Spectral Database for Phytochemicals", "tags.text==ReSpect")
+    saveQuery("Libraries - HMDB", "Human Metabolome Database", "tags.text==HMDB")
+    saveQuery("Libraries - GNPS", "Global Natural Product Social Molecular Networking Library", "tags.text==GNPS")
+    saveQuery("Libraries - LipidBlast", "LipidBlast In-Silico MS/MS Database for Lipid Identification", "tags.text==LipidBlast")
+    saveQuery("Libraries - FAHFA", "Fatty Acid ester of Hydroxyl Fatty Acid In-Silico Library", "tags.text==FAHFA")
+    saveQuery("Libraries - iTree", "iTree Mass Spectral Tree Library", "tags.text==iTree")
+    saveQuery("Libraries - RTX5 Fiehnlib", "RTX5 Fiehn Lab Metabolic Profiling Library", "tags.text==FiehnLib")
   }
 
   /**
@@ -69,7 +67,12 @@ class BootstrapDownloaderService extends LazyLogging {
     * @return
     */
   def createGeneralQueries() = {
-//    saveQuery("GC/MS", "GC/MS spectra", "tags.text==\"GC/MS\"")
-//    saveQuery("LC/MS", "LC/MS spectra", "tags.text==\"LC/MS\"")
+    // Create export for all spectra
+    saveQuery("All Spectra", "All Spectra", "")
+//    saveQuery("All Spectra - In-Silico Spectra", "In-Silico Spectra", "tags.text=='in-silico'")
+//    saveQuery("All Spectra - Experimental Spectra", "Experimental Spectra", "")
+
+    saveQuery("GC/MS", "GC/MS spectra", "tags.text==\"GC-MS\"")
+    saveQuery("LC/MS", "LC/MS spectra", "tags.text==\"LC-MS\"")
   }
 }
