ce73f2b4f4cc9b1363dcc9f603410b3d785b25e7
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-11-07 11:01:00 -0800

    fixed SPLASH query directive
    Former-commit-id: 2d115d6e44af52ffbedc93c5a27b2f657d2c2a5d

---
+++ backend/app/server/proxy/src/main/angularjs/scripts/directives/metadata/splashQueryDirective.js
@@ -0,0 +1,43 @@
+/**
+ * Creates or updates a query based on SPLASH
+ */
+
+(function() {
+    'use strict';
+
+    splashQueryController.$inject = ['$scope', 'SpectraQueryBuilderService', '$location'];
+    angular.module('moaClientApp')
+        .directive('splashQuery', splashQuery);
+
+    function splashQuery() {
+        return {
+            restrict: 'A',
+            templateUrl: '/views/templates/query/splashQuery.html',
+            replace: true,
+            transclude: true,
+            scope: {
+                value: '=value'
+            },
+            controller: splashQueryController
+        };
+    }
+
+    /* @ngInject */
+    function splashQueryController($scope, SpectraQueryBuilderService, $location) {
+        /**
+         * Create a new query based on the selected SPLASH
+         */
+        $scope.newQuery = function() {
+            SpectraQueryBuilderService.prepareQuery();
+            $scope.addToQuery();
+        };
+
+        /**
+         * Add selected SPLASH to the current query
+         */
+        $scope.addToQuery = function() {
+            SpectraQueryBuilderService.addSplashToQuery($scope.value.splash);
+            SpectraQueryBuilderService.executeQuery();
+        };
+    }
+})();
