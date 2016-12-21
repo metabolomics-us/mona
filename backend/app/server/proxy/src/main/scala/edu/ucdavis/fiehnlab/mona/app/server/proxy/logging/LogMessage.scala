d5a21f19ebe2aecdc6b1be7538906dac793c3ae2
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2016-12-20 16:08:45 -0800

    improvements to proxy logging and added geolocation
    Former-commit-id: db803ebe70cc9b073e6481bec4b7d7cb9ec03afc

--- backend/app/server/proxy/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/proxy/logging/LogMessage.scala
+++ backend/app/server/proxy/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/proxy/logging/LogMessage.scala
@@ -1,14 +1,30 @@
 package edu.ucdavis.fiehnlab.mona.app.server.proxy.logging
 
+import java.util.Date
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+import scala.annotation.meta.field
+
 /**
   * Created by sajjan on 12/15/16.
   */
+@Document(collection = "LOGS")
 case class LogMessage(
+                       @(Id@field)
+                       id: String,
                        httpStatus: Int,
                        httpMethod: String,
                        path: String,
                        queryString: String,
                        postData: String,
+
                        clientIp: String,
-                       duration: Long
+                       clientCountry: String,
+                       clientRegion: String,
+                       clientCity: String,
+
+                       duration: Long,
+                       date: Date
                      )
