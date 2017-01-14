1742e398b7f2dbb3026e758b650649e87f9f3d1d
Gert Wohlgemuth <Gert Wohlgemuth>
2017-01-13 18:50:33 -0800

    working on msp writer and webhook service
    Former-commit-id: 08e2c5c8c0cfa86ffcbc344847cb7b57f854c39c

--- backend/services/webhooks-server/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/webhooks/controller/WebhookControllerTest.scala
+++ backend/services/webhooks-server/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/rest/server/webhooks/controller/WebhookControllerTest.scala
@@ -102,66 +102,17 @@ class WebhookControllerTest extends AbstractGenericRESTControllerTest[WebHook]("
         given().body(getValue).when().post(s"/webhooks/pull").then().statusCode(401)
       }
 
-      "be able to download a specific query from the remote, with a small amount of results" in {
-        spectrumPersistenceService.deleteAll()
-        eventually(timeout(50 seconds), interval(1000 millis)){
-          logger.info(s"local db is now: ${spectrumPersistenceService.count()} spectra")
-          assert(spectrumPersistenceService.count() == 0)
-        }
-
-        authenticate().post("""/webhooks/pull?query=metaData=q='name=="column" and value=="Acclaim RSLC C18 2.2um, 2.1x100mm, Thermo"' and metaData=q='name=="collision energy" and value=="Ramp 21.9-32.9 eV"'""").then().statusCode(200)
-
-        eventually(timeout(50 seconds), interval(1000 millis)){
-          logger.info(s"local db is now: ${spectrumPersistenceService.count()} spectra")
-          assert(spectrumPersistenceService.count() >= 3)
-        }
-
-      }
-
-
-      "be able to download a specific query from the remote, with a not so small amount of results" in {
-        spectrumPersistenceService.deleteAll()
-        eventually(timeout(50 seconds), interval(1000 millis)){
-          logger.info(s"local db is now: ${spectrumPersistenceService.count()} spectra")
-          assert(spectrumPersistenceService.count() == 0)
-        }
-
-        authenticate().post("""/webhooks/pull?query=metaData=q='name=="collision energy" and value=="50 eV"' and metaData=q='name=="ionization mode" and value=="positive"' and metaData=q='name=="column temperature" and value=="RT"'""").then().statusCode(200)
-
-        eventually(timeout(50 seconds), interval(1000 millis)){
-          logger.info(s"local db is now: ${spectrumPersistenceService.count()} spectra")
-          assert(spectrumPersistenceService.count() >= 84)
-        }
-
-      }
-
-
-      "be able to download a specific query from the remote, with a medium amount of results" in {
-        spectrumPersistenceService.deleteAll()
-        eventually(timeout(50 seconds), interval(1000 millis)){
-          logger.info(s"local db is now: ${spectrumPersistenceService.count()} spectra")
-          assert(spectrumPersistenceService.count() == 0)
-        }
-
-        authenticate().post("""/webhooks/pull?query=metaData=q='name=="collision energy" and value=="50 eV"'""").then().statusCode(200)
-
-        eventually(timeout(50 seconds), interval(1000 millis)){
-          logger.info(s"local db is now: ${spectrumPersistenceService.count()} spectra")
-          assert(spectrumPersistenceService.count() >= 496)
-        }
-
-      }
       "be able to download a specific query from the remote, with a large amount of results" in {
         spectrumPersistenceService.deleteAll()
         eventually(timeout(50 seconds)) {
           assert(spectrumPersistenceService.count() == 0)
         }
 
-        authenticate().post("""/webhooks/pull?query=metaData=q='name=="flow gradient" and value=="99/1 at 0-1 min, 61/39 at 3 min, 0.1/99.9 at 14-16 min, 99/1 at 16.1-20 min"'""").then().statusCode(200)
+        authenticate().post("""/webhooks/pull?query=metaData=q='name=="instrument type" and value=="Quattro_QQQ"'""").then().statusCode(200)
 
         eventually(timeout(50 seconds), interval(1000 millis)){
           logger.info(s"local db is now: ${spectrumPersistenceService.count()} spectra")
-          assert(spectrumPersistenceService.count() >= 1492)
+          assert(spectrumPersistenceService.count() >= 2269)
         }
 
       }
@@ -178,7 +129,7 @@ class WebhookControllerTest extends AbstractGenericRESTControllerTest[WebHook]("
         //get count of spectra in db
 
         //add mona spectrum
-        given().body(getValue).when().get(s"/webhooks/sync/AU100601/add").then().statusCode(200)
+        given().body(getValue).when().get(s"/webhooks/sync/HMDB00001_1/add").then().statusCode(200)
 
         //ensure the new spectra is now added
         eventually(timeout(5 seconds), interval(500 millis)){
@@ -199,7 +150,7 @@ class WebhookControllerTest extends AbstractGenericRESTControllerTest[WebHook]("
         //get count of spectra in db
 
         //add mona spectrum
-        given().body(getValue).when().get(s"/webhooks/sync/AU100601/add").then().statusCode(200)
+        given().body(getValue).when().get(s"/webhooks/sync/HMDB00001_1/add").then().statusCode(200)
 
         //wait a bit
 
@@ -209,7 +160,7 @@ class WebhookControllerTest extends AbstractGenericRESTControllerTest[WebHook]("
         }
 
         //add mona spectrum
-        given().body(getValue).when().get(s"/webhooks/sync/AU100601/update").then().statusCode(200)
+        given().body(getValue).when().get(s"/webhooks/sync/HMDB00001_1/update").then().statusCode(200)
 
 
         //ensure the new spectra is now update
@@ -221,7 +172,7 @@ class WebhookControllerTest extends AbstractGenericRESTControllerTest[WebHook]("
       }
 
       "throw an exception for wrongly specified events" in {
-        given().body(getValue).when().get(s"/webhooks/sync/AU100601/thisIsNoEvent").then().statusCode(400)
+        given().body(getValue).when().get(s"/webhooks/sync/HMDB00001_1/thisIsNoEvent").then().statusCode(400)
       }
     }
   }
