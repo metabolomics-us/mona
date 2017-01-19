5762149bff1287dc1942689aa79ba2752b20f21c
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-01-19 15:49:42 -0800

    cleanup repository and remove legacy mona resources
    Former-commit-id: aed51b6983b098c31c2d22eae7ee573a055e741a

--- mona-docker/all-in-one/stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# MoNA docker container name
-MONA_IMAGE_NAME="mona/all-in-one:latest"
-
-# Stop the docker container
-docker stop $(docker ps -a | grep $MONA_IMAGE_NAME | awk '{ print $1 }')
--- test-data/submit.local.single.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for i in `seq $2`; do curl -XPOST -H 'Content-Type:application/json' -H 'Accept:application/json' --data-binary @$1 http://localhost:8080/rest/spectra/single/save; done
