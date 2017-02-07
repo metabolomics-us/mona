ccdf11e4090111ac587da1a6eb6a9a5ce66dac82
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-02-06 16:16:11 -0800

    updated bootstrap client to synchronize databases on startup
    Former-commit-id: 8e6d19a20b604be8f8173f5d415d4cd498906973

--- backend/core/bootstrap/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/bootstrap/service/BootstrapPersistenceService.scala
+++ backend/core/bootstrap/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/bootstrap/service/BootstrapPersistenceService.scala
@@ -1,14 +1,24 @@
 package edu.ucdavis.fiehnlab.mona.backend.bootstrap.service
 
+import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.mona.backend.core.persistence.service.persistence.SpectrumPersistenceService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 /**
   * Created by sajjan on 7/14/16.
   */
 @Service
-class BootstrapPersistenceService {
+class BootstrapPersistenceService extends LazyLogging {
+
+  @Autowired
+  val spectrumPersistenceService: SpectrumPersistenceService = null
 
   def synchronizeDatabases(): Unit = {
+    logger.info("Starting synchronization between MongoDB and ElasticSearch...")
+    val startTime: Long = System.currentTimeMillis()
 
+    spectrumPersistenceService.forceSynchronization()
+    logger.info(s"Synchronization finished in ${(System.currentTimeMillis() - startTime) / 1000} seconds")
   }
 }
