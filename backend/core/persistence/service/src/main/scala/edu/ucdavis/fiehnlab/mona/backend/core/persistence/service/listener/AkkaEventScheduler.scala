b98878387da6d48981680d54a032b0a253161cff
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-02-07 16:15:35 -0800

    reorder docker compose file and minor changes
    Former-commit-id: 84aa896f32f712f505d83f3c45511f0c9ec0c1e7

--- backend/core/persistence/service/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/service/listener/AkkaEventScheduler.scala
+++ backend/core/persistence/service/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/persistence/service/listener/AkkaEventScheduler.scala
@@ -7,7 +7,7 @@ import edu.ucdavis.fiehnlab.mona.backend.core.domain.event.{Event, EventSchedule
 import scala.collection.JavaConverters._
 
 /**
-  * akka based event scheduler to send events using an actor and so allows for higher throughput
+  * Akka based event scheduler to send events using an actor and so allows for higher throughput
   * of events compared to the default implementation
   *
   * @tparam T
@@ -17,7 +17,7 @@ class AkkaEventScheduler[T] extends EventScheduler[T] {
   val system = ActorSystem("MonaEventScheduler")
 
   /**
-    * schedules the processing of the given event to be processed down stream
+    * Schedules the processing of the given event to be processed down stream
     *
     * @param event
     */
@@ -27,23 +27,22 @@ class AkkaEventScheduler[T] extends EventScheduler[T] {
   }
 }
 
-
-
 /**
-  * the actual actor, which receives a copy of all our
+  * The actual actor which receives a copy of all our data
   *
   * @tparam T
   */
 class SchedulingActor[T] extends Actor with LazyLogging {
 
   /**
-    * when we receive our data. Rather uggly and would be nicer to inject this somehow with spring instead of doing this
+    * When we receive our data. Rather ugly and would be nicer to inject this somehow with spring instead of doing this
     *
     * @return
     */
   override def receive: Receive = {
     case x: (java.util.List[PersistenceEventListener[T]], Event[T]) =>
       logger.debug("\nnotify listeners")
+
       x._1.asScala.sortBy(_.priority).reverse.foreach { listener =>
         logger.debug(s"Calling ${listener.getClass.getSimpleName} with event ${x._2.eventType}")
         listener.handleEvent(x._2)
