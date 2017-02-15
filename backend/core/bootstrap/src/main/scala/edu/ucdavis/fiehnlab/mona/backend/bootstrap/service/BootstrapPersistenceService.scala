840004727a1604a048bbebb367ed0a3828487744
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-02-14 16:53:55 -0800

    improved bootstrap synchronization service
    Former-commit-id: 924b8d99fe7c1fafe798f19f112a4b2267a1e98f

--- backend/core/bootstrap/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/bootstrap/service/BootstrapPersistenceService.scala
+++ backend/core/bootstrap/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/bootstrap/service/BootstrapPersistenceService.scala
@@ -1,6 +1,8 @@
 package edu.ucdavis.fiehnlab.mona.backend.bootstrap.service
 
+import java.util
 import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.service.persistence.SpectrumPersistenceService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -18,7 +20,22 @@ class BootstrapPersistenceService extends LazyLogging {
     logger.info("Starting synchronization between MongoDB and ElasticSearch...")
     val startTime: Long = System.currentTimeMillis()
 
+    val it: util.Iterator[Spectrum] = spectrumPersistenceService.findAll().iterator()
+    var counter: Int = 0
+
+    while(it.hasNext) {
+      val spectrum: Spectrum = it.next()
+      spectrumPersistenceService.fireSyncEvent(spectrum)
+      counter += 1
+
+      if (counter % 10000 == 0) {
+        logger.info(s"\tSynchronized spectrum #$counter with id ${spectrum.id}")
+      } else {
+        logger.trace(s"\tSynchronized spectrum #$counter with id ${spectrum.id}")
+      }
+    }
+
     spectrumPersistenceService.forceSynchronization()
-    logger.info(s"Synchronization finished in ${(System.currentTimeMillis() - startTime) / 1000} seconds")
+    logger.info(s"Synchronization of $counter spectra finished in ${(System.currentTimeMillis() - startTime) / 1000} seconds")
   }
 }
