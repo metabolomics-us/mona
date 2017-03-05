9218631aabd3fcb7dfea051964e8cd46c9f0143b
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-03-05 10:25:21 -0800

    added authentication and webhook registration to webhook repository
    Former-commit-id: ce3be02445a802fb713f57b66ae9a6849fd71d0c

--- backend/app/clients/repository/src/main/java/edu/ucdavis/fiehnlab/mona/app/client/repository/config/RepositoryConfig.java
+++ backend/app/clients/repository/src/main/java/edu/ucdavis/fiehnlab/mona/app/client/repository/config/RepositoryConfig.java
@@ -2,6 +2,7 @@ package edu.ucdavis.fiehnlab.mona.app.client.repository.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by matthewmueller on 3/3/17.
