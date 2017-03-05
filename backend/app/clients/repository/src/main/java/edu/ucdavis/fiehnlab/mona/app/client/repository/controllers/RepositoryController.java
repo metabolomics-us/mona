8c8cb0d58af15c7cd63a09761096d93a0dfad595
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-03-05 10:45:02 -0800

    added downloader method to webhooks repository that utilizes the mona rest client
    Former-commit-id: 0be1d35c473c09a50a761fb8552711991271427d

--- backend/app/clients/repository/src/main/java/edu/ucdavis/fiehnlab/mona/app/client/repository/controllers/RepositoryController.java
+++ backend/app/clients/repository/src/main/java/edu/ucdavis/fiehnlab/mona/app/client/repository/controllers/RepositoryController.java
@@ -1,5 +1,7 @@
 package edu.ucdavis.fiehnlab.mona.app.client.repository.controllers;
 
+import edu.ucdavis.fiehnlab.mona.app.client.repository.service.RepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,35 +16,40 @@ import java.util.Arrays;
 @RestController
 public class RepositoryController {
 
+    @Autowired
+    RepositoryService repositoryService;
+
     @RequestMapping("/sync")
     public String hook(@RequestParam(value = "id") String id,
                        @RequestParam(value = "type") String event) throws IOException {
+//
+//        ProcessBuilder pb = new ProcessBuilder(
+//                "curl",
+//                "-s",
+//                "http://mona.fiehnlab.ucdavis.edu/rest/spectra/"+id);
+//
+//        pb.redirectErrorStream(true);
+//        Process p = pb.start();
+//        InputStream is = p.getInputStream();
+//
+//        FileOutputStream outputStream = new FileOutputStream(
+//                id+".txt");
+//
+//        String line;
+//        BufferedInputStream bis = new BufferedInputStream(is);
+//        byte[] bytes = new byte[100];
+//        int numberByteReaded;
+//        while ((numberByteReaded = bis.read(bytes, 0, 100)) != -1) {
+//
+//            outputStream.write(bytes, 0, numberByteReaded);
+//            Arrays.fill(bytes, (byte) 0);
+//
+//        }
+//
+//        outputStream.flush();
+//        outputStream.close();
 
-        ProcessBuilder pb = new ProcessBuilder(
-                "curl",
-                "-s",
-                "http://mona.fiehnlab.ucdavis.edu/rest/spectra/"+id);
-
-        pb.redirectErrorStream(true);
-        Process p = pb.start();
-        InputStream is = p.getInputStream();
-
-        FileOutputStream outputStream = new FileOutputStream(
-                id+".txt");
-
-        String line;
-        BufferedInputStream bis = new BufferedInputStream(is);
-        byte[] bytes = new byte[100];
-        int numberByteReaded;
-        while ((numberByteReaded = bis.read(bytes, 0, 100)) != -1) {
-
-            outputStream.write(bytes, 0, numberByteReaded);
-            Arrays.fill(bytes, (byte) 0);
-
-        }
-
-        outputStream.flush();
-        outputStream.close();
+        repositoryService.handleWebHook(id, event);
 
         return "Spectrum "+id+" : "+event;
     }
