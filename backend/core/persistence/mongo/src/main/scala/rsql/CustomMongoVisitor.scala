a6c7007070fd2fb1f8bd8e40b863048bdc25e799
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-03-21 10:20:49 -0700

    add case-insensitve regex to mongo like query
    Former-commit-id: 1a9aee7eea04cca1a908ddb565f184b12c2271ef

--- backend/core/persistence/mongo/src/main/scala/rsql/CustomMongoVisitor.scala
+++ backend/core/persistence/mongo/src/main/scala/rsql/CustomMongoVisitor.scala
@@ -1,9 +1,12 @@
 package rsql
 
+import java.util.regex.Pattern
+
 import com.github.rutledgepaulv.qbuilders.nodes.ComparisonNode
 import com.github.rutledgepaulv.qbuilders.operators.ComparisonOperator
 import com.github.rutledgepaulv.qbuilders.visitors.MongoVisitor
 import com.typesafe.scalalogging.LazyLogging
+import edu.ucdavis.fiehnlab.rqe.like.LikeStringFieldImpl
 import edu.ucdavis.fiehnlab.rqe.regex.RegexStringFieldImpl
 import org.springframework.data.mongodb.core.query.Criteria
 
@@ -14,9 +17,10 @@ class CustomMongoVisitor extends MongoVisitor with LazyLogging {
   override protected def visit(node: ComparisonNode): Criteria = {
     val operator: ComparisonOperator = node.getOperator
 
-    // Handle custom regex query, or else pass the node to the default visitor
-    if (operator.equals(RegexStringFieldImpl.REGEX)) {
-      Criteria.where(node.getField.asKey).regex(node.getValues.iterator().next().toString)
+    // Handle custom regex and like queries, or else pass the node to the default visitor
+    if (operator.equals(RegexStringFieldImpl.REGEX) || operator.equals(LikeStringFieldImpl.LIKE)) {
+      val pattern: Pattern = Pattern.compile(node.getValues.iterator().next().toString, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE)
+      Criteria.where(node.getField.asKey).regex(pattern)
     } else {
       super.visit(node)
     }
