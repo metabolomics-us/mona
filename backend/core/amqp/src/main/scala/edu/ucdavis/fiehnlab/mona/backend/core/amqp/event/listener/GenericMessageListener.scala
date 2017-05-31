6e93caab849a4317f606dcab13dca7cab6dc8c86
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-05-31 10:27:12 -0700

    added additional curation scheduler tests
    Former-commit-id: 5b5aec613fab85acd859dc5fc6e6b91dd04ec0fc

--- backend/core/amqp/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/amqp/event/listener/GenericMessageListener.scala
+++ backend/core/amqp/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/core/amqp/event/listener/GenericMessageListener.scala
@@ -1,7 +1,6 @@
 package edu.ucdavis.fiehnlab.mona.backend.core.amqp.event.listener
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import edu.ucdavis.fiehnlab.mona.backend.core.domain.event.Event
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.MonaMapper
 import org.springframework.amqp.core.{Message, MessageListener}
 
@@ -10,7 +9,7 @@ import scala.reflect._
 /**
   * Created by wohlg on 4/18/2016.
   */
-abstract class GenericMessageListener[T:ClassTag] extends MessageListener{
+abstract class GenericMessageListener[T: ClassTag] extends MessageListener {
 
   val objectMapper:ObjectMapper = MonaMapper.create
 
@@ -19,7 +18,7 @@ abstract class GenericMessageListener[T:ClassTag] extends MessageListener{
     * @param message
     */
   override final def onMessage(message: Message): Unit = {
-    val content:T = objectMapper.readValue(message.getBody,classTag[T].runtimeClass).asInstanceOf[T]
+    val content: T = objectMapper.readValue(message.getBody,classTag[T].runtimeClass).asInstanceOf[T]
 
     handleMessage(content)
   }
@@ -28,5 +27,5 @@ abstract class GenericMessageListener[T:ClassTag] extends MessageListener{
     * works on the actual message
     * @param content
     */
-  def handleMessage(content:T): Unit
+  def handleMessage(content: T): Unit
 }
