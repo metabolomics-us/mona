037eaec2c36a23a455255e47d546e11386e80ea3
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-07-07 15:01:28 -0700

    added peak index and tests
    Former-commit-id: 82b6526dd76a4d7649d97d9017c873c2e1c5b3a1

--- backend/services/similarity/src/main/scala/edu/ucdavis/fiehnlab/mona/core/similarity/service/SimilarityStartupService.scala
+++ backend/services/similarity/src/main/scala/edu/ucdavis/fiehnlab/mona/core/similarity/service/SimilarityStartupService.scala
@@ -1,11 +1,12 @@
 package edu.ucdavis.fiehnlab.mona.core.similarity.service
 
 import com.typesafe.scalalogging.LazyLogging
-import edu.ucdavis.fiehnlab.mona.backend.core.domain.{MetaData, Spectrum}
 import edu.ucdavis.fiehnlab.mona.backend.core.domain.util.DynamicIterable
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.{MetaData, Spectrum}
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.mongo.repository.ISpectrumMongoRepositoryCustom
 import edu.ucdavis.fiehnlab.mona.backend.curation.util.CommonMetaData
-import edu.ucdavis.fiehnlab.mona.core.similarity.types.SimpleSpectrum
+import edu.ucdavis.fiehnlab.mona.core.similarity.types.IndexType.IndexType
+import edu.ucdavis.fiehnlab.mona.core.similarity.types.{IndexType, SimpleSpectrum}
 import edu.ucdavis.fiehnlab.mona.core.similarity.util.IndexUtils
 import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.boot.context.event.ApplicationReadyEvent
@@ -36,6 +37,21 @@ class SimilarityStartupService extends ApplicationListener[ApplicationReadyEvent
   }
 
 
+  private def addToIndex(spectrum: Spectrum, precursorMZ: Option[MetaData], indexName: String, indexType: IndexType): Int = {
+    val precursorString: String = precursorMZ.get.value.toString
+
+    if (precursorMZ.isDefined) {
+      try {
+        indexUtils.addToIndex(new SimpleSpectrum(spectrum.id, spectrum.spectrum, precursorString.toDouble))
+      } catch {
+        case _: Throwable => indexUtils.addToIndex(new SimpleSpectrum(spectrum.id, spectrum.spectrum))
+      }
+    } else {
+      indexUtils.addToIndex(new SimpleSpectrum(spectrum.id, spectrum.spectrum))
+    }
+  }
+
+
   def populateIndices(): Unit = {
     logger.info("Populating indices...")
 
@@ -52,25 +68,15 @@ class SimilarityStartupService extends ApplicationListener[ApplicationReadyEvent
       val precursorMZ: Option[MetaData] = spectrum.metaData.find(_.name == CommonMetaData.PRECURSOR_MASS)
 
       // Add precursor information if available
-      val indexSize: Int =
-        if (precursorMZ.isDefined) {
-          val precursorString: String = precursorMZ.get.value.toString
-
-          try {
-            indexUtils.addToIndex(new SimpleSpectrum(spectrum.id, spectrum.spectrum, precursorString.toDouble))
-          } catch {
-            case _: Throwable => indexUtils.addToIndex(new SimpleSpectrum(spectrum.id, spectrum.spectrum))
-          }
-        } else {
-          indexUtils.addToIndex(new SimpleSpectrum(spectrum.id, spectrum.spectrum))
-        }
+      val mainIndexSize: Int = addToIndex(spectrum, precursorMZ, "default", IndexType.SIMILAR_HISTOGRAM)
+      val peakIndexSize: Int =  addToIndex(spectrum, precursorMZ, "default", IndexType.PEAK)
 
       counter += 1
 
       if (counter % 10000 == 0) {
-        logger.info(s"\tIndexed spectrum #$counter with id ${spectrum.id}, index size = $indexSize")
+        logger.info(s"\tIndexed spectrum #$counter with id ${spectrum.id}, main index size = $mainIndexSize, peak index size = $peakIndexSize")
       } else {
-        logger.debug(s"\tIndexed spectrum #$counter with id ${spectrum.id}, index size = $indexSize")
+        logger.trace(s"\tIndexed spectrum #$counter with id ${spectrum.id}, main index size = $mainIndexSize, peak index size = $peakIndexSize")
       }
     }
 
