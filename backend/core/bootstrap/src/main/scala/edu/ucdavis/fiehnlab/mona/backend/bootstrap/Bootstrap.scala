3073c80ad87f889100c9f754e1de29b6c13a0938
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-07-31 12:08:24 -0700

    fixed hanging boostrap service after tasks completed
    Former-commit-id: 6f0c9ab5e468a6ff64e22afa4cc0d732ec9c9cbc

--- backend/core/bootstrap/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/bootstrap/Bootstrap.scala
+++ backend/core/bootstrap/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/bootstrap/Bootstrap.scala
@@ -5,18 +5,21 @@ import edu.ucdavis.fiehnlab.mona.backend.bootstrap.service.{BootstrapDownloaderS
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.rest.SwaggerConfig
 import edu.ucdavis.fiehnlab.mona.backend.core.persistence.service.config.PersistenceServiceConfig
 import edu.ucdavis.fiehnlab.mona.backend.services.downloader.core.config.DownloadConfig
+import edu.ucdavis.fiehnlab.mona.backend.services.downloader.core.repository.{PredefinedQueryMongoRepository, QueryExportMongoRepository}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.{ApplicationArguments, ApplicationRunner, SpringApplication}
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Import
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 /**
   * Created by sajjan on 6/16/16.
   */
 @SpringBootApplication
-@Import(Array(classOf[PersistenceServiceConfig], classOf[DownloadConfig]))
+@Import(Array(classOf[PersistenceServiceConfig]))
+@EnableMongoRepositories(basePackageClasses = Array(classOf[QueryExportMongoRepository], classOf[PredefinedQueryMongoRepository]))
 class Bootstrap extends ApplicationRunner with LazyLogging {
 
   @Autowired
@@ -27,7 +30,6 @@ class Bootstrap extends ApplicationRunner with LazyLogging {
 
 
   override def run(applicationArguments: ApplicationArguments): Unit = {
-
     // Configure downloader
     downloaderService.createPredefinedQueries()
 
@@ -40,5 +42,5 @@ object Bootstrap extends App {
   val app = new SpringApplication(classOf[Bootstrap])
   app.setWebEnvironment(false)
 
-  val context = app.run(args: _*)
+  System.exit(SpringApplication.exit(app.run(args: _*)))
 }
