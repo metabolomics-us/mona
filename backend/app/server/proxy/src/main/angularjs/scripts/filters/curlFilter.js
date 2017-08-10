20207de0bfd02bd148732242e035313902b17ec7
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-08-10 14:23:35 -0700

    minor fix to curl filter in spectrum browser
    Former-commit-id: 5f1687e06dd3ac22371f980927c636715df2eed0

--- backend/app/server/proxy/src/main/angularjs/scripts/filters/curlFilter.js
+++ backend/app/server/proxy/src/main/angularjs/scripts/filters/curlFilter.js
@@ -2,39 +2,40 @@
  * Created by wohlgemuth on 11/6/14.
  */
 
-
 (function() {
     'use strict';
 
     /**
-     * Generates a cURL link for us to download a given RSQL query
+     * Generates a cURL link for us to download a given RSQL and text query
      */
     angular.module('moaClientApp')
-      .filter('curl', ['REST_BACKEND_SERVER', function(REST_BACKEND_SERVER) {
-          return function(input) {
-              // Use location.host if the server url is empty for deployment mode
-              var host = REST_BACKEND_SERVER == '' ? location.origin : REST_BACKEND_SERVER;
+        .filter('curl', ['REST_BACKEND_SERVER', function(REST_BACKEND_SERVER) {
+            return function(input) {
+                // Use location.host if the server url is empty for deployment mode
+                var host = REST_BACKEND_SERVER == '' ? location.origin : REST_BACKEND_SERVER;
 
-              var query = input && typeof(input.query) == 'string' ? input.query.replace(/"/g, '\\"') : '';
-              var text = input && typeof(input.text) == 'string' ? input.text : '';
+                var query = input && typeof(input.query) == 'string' ? input.query.replace(/"/g, '\\"') : '';
+                var text = input && typeof(input.text) == 'string' ? input.text : '';
 
-              if (query !== '' || text !== '') {
-                  var cmd = 'curl "' + host + '/rest/spectra/search?';
+                if (query !== '' || text !== '') {
+                    var cmd = 'curl "' + host + '/rest/spectra/search?';
 
-                  if (query !== '') {
-                      cmd += 'query='+ query;
+                    if (query !== '') {
+                        cmd += 'query='+ query;
 
-                      if (text !== '') {
-                          cmd += '&text='+ text;
-                      }
-                  } else {
-                      cmd += 'text='+ text;
-                  }
+                        if (text !== '') {
+                            cmd += '&text='+ text;
+                        }
+                    } else {
+                        cmd += 'text='+ text;
+                    }
 
-                  return cmd;
-              } else {
-                  return 'curl "' + host + '/rest/spectra"';
-              }
-          };
-      }])
+                    cmd += '"';
+
+                    return cmd;
+                } else {
+                    return 'curl "' + host + '/rest/spectra"';
+                }
+            };
+        }])
 })();
