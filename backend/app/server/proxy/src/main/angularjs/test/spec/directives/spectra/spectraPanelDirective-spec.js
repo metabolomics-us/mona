430440f363811425671dee1f47f1cdc0de4d4f7e
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-09-14 09:58:47 -0700

    working on angular tests
    Former-commit-id: cc4696afb2f2ddda431c2c2e72abef183a242382

--- backend/app/server/proxy/src/main/angularjs/test/spec/directives/spectra/spectraPanelDirective-spec.js
+++ backend/app/server/proxy/src/main/angularjs/test/spec/directives/spectra/spectraPanelDirective-spec.js
@@ -1,26 +1,44 @@
-'use strict'
+'use strict';
 
 describe('Directive: Spectra Panel Directive', function() {
-    beforeEach(module('moaClientApp'));
+    beforeEach(angular.mock.module('moaClientApp'));
     beforeEach(module('templates'));
 
     var scope, element, controller;
+    var $compile, $scope;
 
-    beforeEach(inject(function($rootScope, $compile) {
-        // create our html element
-        element = angular.element('<display-spectra-panel></display-spectra-panel>');
-        scope = $rootScope.$new();
-        scope.spectrum = {id: 'test'};
-        controller = element.controller('displaySpectraPanel');
-
-        //compile element and scope
-        $compile(element)(scope);
-        scope.$digest();
-
+    beforeEach(inject(function(_$compile_, _$rootScope_) {
+        $compile = _$compile_;
+        $scope = _$rootScope_.$new();
     }));
 
-    it('should load the template', function() {
-        var content = element.text();
-        console.log(controller);
-    });
+    it('should load the template', inject(function() {
+        $scope.spectrum = {id: 'test'};
+        var template = $compile(angular.element('<div display-spectra-panel spectrum="spectrum"></div>'))($scope);
+
+        $scope.$digest();
+
+        var templateAsHtml = template.html();
+        console.log(templateAsHtml);
+        console.log($scope);
+    }));
+
+    // beforeEach(inject(function($rootScope, $compile) {
+    //     // create our html element
+    //     element = angular.element('<display-spectra-panel></display-spectra-panel>');
+    //     scope = $rootScope.$new();
+    //     scope.spectrum = {id: 'test'};
+    //     controller = element.controller('displaySpectraPanel');
+    //
+    //     //compile element and scope
+    //     $compile(element)(scope);
+    //     scope.$digest();
+    //
+    // }));
+    //
+    // it('should load the template', function() {
+    //     var content = element.text();
+    //     console.log(content);
+    //     console.log(controller);
+    // });
 });
