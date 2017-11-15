653461712230ab79aa8e1f87ea1234722fd0447d
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-11-15 10:36:48 -0800

    minor changes to curation rules
    Former-commit-id: 226a6f2680553d0c6b15776dca0d1072d58422d6

--- backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/validation/ColumnValidation.scala
+++ backend/core/curation/src/main/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/validation/ColumnValidation.scala
@@ -1,10 +1,12 @@
 package edu.ucdavis.fiehnlab.mona.backend.curation.processor.validation
 
 import com.typesafe.scalalogging.LazyLogging
-import edu.ucdavis.fiehnlab.mona.backend.core.domain.{MetaData, Spectrum, Tags}
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.{MetaData, Spectrum}
 import edu.ucdavis.fiehnlab.mona.backend.core.workflow.annotations.Step
 import edu.ucdavis.fiehnlab.mona.backend.curation.util.CommonMetaData
 import org.springframework.batch.item.ItemProcessor
+
+import scala.util.matching.Regex
 
 /**
   * Created by sajjan on 4/16/16.
@@ -12,8 +14,8 @@ import org.springframework.batch.item.ItemProcessor
 @Step(description = "this step will validate the provided column information")
 class ColumnValidation extends ItemProcessor[Spectrum, Spectrum] with LazyLogging {
 
-  val DIAMETER_AND_LENGTH_REGEX = "(\\d+\\.?\\d*(?: [cmu]?m)?)(?:(?:\\s?[xX]\\s?)|(?: by ))(\\d+\\.?\\d*\\s?[cmu]?m)".r
-  val LENGTH_REGEX = "(\\d+\\.?\\d*\\s?[cmu]?m)".r
+  val DIAMETER_AND_LENGTH_REGEX: Regex = "(\\d+\\.?\\d*(?: [cmu]?m)?)(?:(?:\\s?[xX]\\s?)|(?: by ))(\\d+\\.?\\d*\\s?[cmu]?m)".r
+  val LENGTH_REGEX: Regex = "(\\d+\\.?\\d*\\s?[cmu]?m)".r
 
   /**
     * processes the given spectrum
@@ -31,7 +33,7 @@ class ColumnValidation extends ItemProcessor[Spectrum, Spectrum] with LazyLoggin
         val diameterAndLengthDefined: Boolean = DIAMETER_AND_LENGTH_REGEX.findFirstIn(x.value.toString).isDefined
         val onlyLengthDefined: Boolean = LENGTH_REGEX.findFirstIn(x.value.toString).isDefined
 
-        if(!diameterAndLengthDefined && !onlyLengthDefined) {
+        if (!diameterAndLengthDefined && !onlyLengthDefined) {
           logger.warn(s"Length and Diameter not specified for spectrum ${spectrum.id}")
         }
       }
