32e2e202795d904e1ca07a3f8cb8c414de6389a5
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-12-05 09:19:12 -0800

    fixed spring cloud version and updated discovery and config servers
    Former-commit-id: daaf1ad623978a4d5f05c86e50e38d8a73c3e25e

--- backend/app/server/configuration/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/config/ConfigServer.scala
+++ backend/app/server/configuration/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/config/ConfigServer.scala
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
-import org.springframework.cloud.context.config.annotation.RefreshScope
 
 /**
   * Created by wohlgemuth on 3/28/16.
@@ -12,10 +11,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
-@RefreshScope
-class ConfigServer {
-
-}
+class ConfigServer
 
 object ConfigServer extends App {
   new SpringApplication(classOf[ConfigServer]).run()
