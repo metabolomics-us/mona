cc3733cc0a772d791f9f45d7d3b3bfed8090615b
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2017-12-12 11:12:52 -0800

    merge discovery+configuration servers and move to core package
    Former-commit-id: ff6ad819c68d1506e09302bda8fa15810b98261f

--- backend/core/discovery/src/main/scala/edu/ucdavis/fiehnlab/mona/discovery/DiscoveryServer.scala
+++ backend/core/discovery/src/main/scala/edu/ucdavis/fiehnlab/mona/discovery/DiscoveryServer.scala
@@ -1,13 +1,15 @@
-package edu.ucdavis.fiehnlab.mona.app.server.discovery
+package edu.ucdavis.fiehnlab.mona.discovery
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 /**
   * Created by wohlgemuth on 3/28/16.
   */
 @EnableEurekaServer
+@EnableConfigServer
 @SpringBootApplication
 class DiscoveryServer
 
