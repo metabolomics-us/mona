2688c9ce5339c0d294f91bd4c3802bfddb649c95
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-03-19 13:32:42 -0700

    added CRLF support for NIST compatible exports
    Former-commit-id: c27d7fc88cc013f54dd68f7d2dbdc21ad57cf977

--- backend/core/domain/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/domain/io/sdf/SDFWriterTest.scala
+++ backend/core/domain/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/domain/io/sdf/SDFWriterTest.scala
@@ -11,136 +11,148 @@ import org.scalatest.WordSpec
   */
 class SDFWriterTest extends WordSpec {
 
-  "we should be able to create an instance of the writer" when {
-    val writer: SDFWriter = new SDFWriter
+  "a writer" should {
+    "export monaRecord.json" must {
+      val input: InputStreamReader = new InputStreamReader(getClass.getResourceAsStream("/monaRecord.json"))
+      val spectrum: Spectrum = JSONDomainReader.create[Spectrum].read(input)
 
-    "a writer" should {
-      "export monaRecord.json" must {
-        val reader: JSONDomainReader[Spectrum] = JSONDomainReader.create[Spectrum]
-        val input: InputStreamReader = new InputStreamReader(getClass.getResourceAsStream("/monaRecord.json"))
-        val spectrum: Spectrum = reader.read(input)
+      val writer: SDFWriter = new SDFWriter
+      val out: StringWriter = new StringWriter()
+      writer.write(spectrum, out)
 
-        val out: StringWriter = new StringWriter()
-        writer.write(spectrum, out)
-
-        "Name" in {
-          assert(out.toString.contains(">  <NAME>\nCY8"))
-        }
-
-        "synonyms" in {
-          assert(out.toString.contains(">  <SYNONYMS>\nCyclopamine\n[3H]-Cyclopamine\n11-deoxojervine\n(3beta,22S,23R)-17,23-Epoxyveratraman-3-ol"))
-        }
-
-        "precursor type" in {
-          assert(out.toString.contains(">  <PRECURSOR TYPE>\n[M+H]+"))
-        }
-
-        "spectrum type/ms level" in {
-          assert(out.toString.contains(">  <SPECTRUM TYPE>\nMS2"))
-        }
-
-        "precursor m/z" in {
-          assert(out.toString.contains(">  <PRECURSOR M/Z>\n412.3"))
-        }
-
-        "instrument type" in {
-          assert(out.toString.contains(">  <INSTRUMENT TYPE>\nLC-ESI-QTOF"))
-        }
-
-        "instrument" in {
-          assert(out.toString.contains(">  <INSTRUMENT>\nBruker maXis ESI-QTOF"))
-        }
-
-        "InChIKey" in {
-          assert(out.toString.contains(">  <INCHIKEY>\nQASFUMOKHFSJGL-LAFRSMQTSA-N"))
-        }
-
-        "molecular weight" in {
-          assert(out.toString.contains(">  <MW>\n411"))
-        }
-
-        "DB#" in {
-          assert(out.toString.contains(">  <ID>\n252"))
-        }
-
-        "exact mass" in {
-          assert(out.toString.contains(">  <EXACT MASS>\n411.313"))
-        }
-
-        "correct number of peaks" in {
-          assert(out.toString.contains(">  <NUM PEAKS>\n100"))
-        }
+      "Name" in {
+        assert(out.toString.contains(">  <NAME>\r\nCY8"))
       }
 
-      "export first curatedRecords.json" must {
-        val reader: JSONDomainReader[Array[Spectrum]] = JSONDomainReader.create[Array[Spectrum]]
-        val input: InputStreamReader = new InputStreamReader(getClass.getResourceAsStream("/curatedRecords.json"))
-        val spectrum: Spectrum = reader.read(input).head
+      "synonyms" in {
+        assert(out.toString.contains(">  <SYNONYMS>\r\nCyclopamine\r\n[3H]-Cyclopamine\r\n11-deoxojervine\r\n(3beta,22S,23R)-17,23-Epoxyveratraman-3-ol"))
+      }
 
-        val out: StringWriter = new StringWriter()
-        writer.write(spectrum, out)
+      "precursor type" in {
+        assert(out.toString.contains(">  <PRECURSOR TYPE>\r\n[M+H]+"))
+      }
 
-        println(out.toString)
+      "spectrum type/ms level" in {
+        assert(out.toString.contains(">  <SPECTRUM TYPE>\r\nMS2"))
+      }
 
-        "Name" in {
-          assert(out.toString.contains(">  <NAME>\nSulfaclozine"))
-        }
+      "precursor m/z" in {
+        assert(out.toString.contains(">  <PRECURSOR M/Z>\r\n412.3"))
+      }
 
-        "synonyms" in {
-          assert(out.toString.contains(">  <SYNONYMS>\n4-amino-N-(6-chloropyrazin-2-yl)benzenesulfonamide"))
-        }
+      "instrument type" in {
+        assert(out.toString.contains(">  <INSTRUMENT TYPE>\r\nLC-ESI-QTOF"))
+      }
 
-        "precursor type" in {
-          assert(out.toString.contains(">  <PRECURSOR TYPE>\n[M+H]+"))
-        }
+      "instrument" in {
+        assert(out.toString.contains(">  <INSTRUMENT>\r\nBruker maXis ESI-QTOF"))
+      }
 
-        "spectrum type/ms level" in {
-          assert(out.toString.contains(">  <SPECTRUM TYPE>\nMS2"))
-        }
+      "InChIKey" in {
+        assert(out.toString.contains(">  <INCHIKEY>\r\nQASFUMOKHFSJGL-LAFRSMQTSA-N"))
+      }
 
-        "precursor m/z" in {
-          assert(out.toString.contains(">  <PRECURSOR M/Z>\n285.0208"))
-        }
+      "molecular weight" in {
+        assert(out.toString.contains(">  <MW>\r\n411"))
+      }
 
-        "instrument type" in {
-          assert(out.toString.contains(">  <INSTRUMENT TYPE>\nLC-ESI-QTOF"))
-        }
+      "DB#" in {
+        assert(out.toString.contains(">  <ID>\r\n252"))
+      }
 
-        "instrument" in {
-          assert(out.toString.contains(">  <INSTRUMENT>\nBruker maXis Impact"))
-        }
+      "exact mass" in {
+        assert(out.toString.contains(">  <EXACT MASS>\r\n411.313"))
+      }
 
-        "ionization mode" in {
-          assert(out.toString.contains(">  <ION MODE>\nP"))
-        }
+      "correct number of peaks" in {
+        assert(out.toString.contains(">  <NUM PEAKS>\r\n100"))
+      }
+    }
 
-        "collision energy" in {
-          assert(out.toString.contains(">  <COLLISION ENERGY>\nRamp 21.1-31.6 eV"))
-        }
+    "export first curatedRecords.json" must {
+      val reader: JSONDomainReader[Array[Spectrum]] = JSONDomainReader.create[Array[Spectrum]]
+      val input: InputStreamReader = new InputStreamReader(getClass.getResourceAsStream("/curatedRecords.json"))
+      val spectrum: Spectrum = reader.read(input).head
 
-        "InChIKey" in {
-          assert(out.toString.contains(">  <INCHIKEY>\nQKLPUVXBJHRFQZ-UHFFFAOYSA-N"))
-        }
+      val writer: SDFWriter = new SDFWriter
+      val out: StringWriter = new StringWriter()
+      writer.write(spectrum, out)
 
-        "molecular formula" in {
-          assert(out.toString.contains(">  <FORMULA>\nC10H9ClN4O2S"))
-        }
+      println(out.toString)
 
-        "molecular weight" in {
-          assert(out.toString.contains(">  <MW>\n284"))
-        }
+      "Name" in {
+        assert(out.toString.contains(">  <NAME>\r\nSulfaclozine"))
+      }
 
-        "DB#" in {
-          assert(out.toString.contains(">  <ID>\nAU100601"))
-        }
+      "synonyms" in {
+        assert(out.toString.contains(">  <SYNONYMS>\r\n4-amino-N-(6-chloropyrazin-2-yl)benzenesulfonamide"))
+      }
 
-        "exact mass" in {
-          assert(out.toString.contains(">  <EXACT MASS>\n284.013474208"))
-        }
+      "precursor type" in {
+        assert(out.toString.contains(">  <PRECURSOR TYPE>\r\n[M+H]+"))
+      }
 
-        "correct number of peaks" in {
-          assert(out.toString.contains(">  <NUM PEAKS>\n27"))
-        }
+      "spectrum type/ms level" in {
+        assert(out.toString.contains(">  <SPECTRUM TYPE>\r\nMS2"))
+      }
+
+      "precursor m/z" in {
+        assert(out.toString.contains(">  <PRECURSOR M/Z>\r\n285.0208"))
+      }
+
+      "instrument type" in {
+        assert(out.toString.contains(">  <INSTRUMENT TYPE>\r\nLC-ESI-QTOF"))
+      }
+
+      "instrument" in {
+        assert(out.toString.contains(">  <INSTRUMENT>\r\nBruker maXis Impact"))
+      }
+
+      "ionization mode" in {
+        assert(out.toString.contains(">  <ION MODE>\r\nP"))
+      }
+
+      "collision energy" in {
+        assert(out.toString.contains(">  <COLLISION ENERGY>\r\nRamp 21.1-31.6 eV"))
+      }
+
+      "InChIKey" in {
+        assert(out.toString.contains(">  <INCHIKEY>\r\nQKLPUVXBJHRFQZ-UHFFFAOYSA-N"))
+      }
+
+      "molecular formula" in {
+        assert(out.toString.contains(">  <FORMULA>\r\nC10H9ClN4O2S"))
+      }
+
+      "molecular weight" in {
+        assert(out.toString.contains(">  <MW>\r\n284"))
+      }
+
+      "DB#" in {
+        assert(out.toString.contains(">  <ID>\r\nAU100601"))
+      }
+
+      "exact mass" in {
+        assert(out.toString.contains(">  <EXACT MASS>\r\n284.013474208"))
+      }
+
+      "correct number of peaks" in {
+        assert(out.toString.contains(">  <NUM PEAKS>\r\n27"))
+      }
+    }
+
+    "export curatedRecords.json" must {
+      val input: InputStreamReader = new InputStreamReader(getClass.getResourceAsStream("/curatedRecords.json"))
+      val spectra: Array[Spectrum] = JSONDomainReader.create[Array[Spectrum]].read(input)
+
+      val writer: SDFWriter = new SDFWriter
+      val out: StringWriter = new StringWriter()
+      spectra.foreach(x => writer.write(x, out))
+
+      println(out.toString)
+
+      "export the correct number of records" in {
+        assert(out.toString.split("\\$\\$\\$\\$").length == 51)
       }
     }
   }
