efa08d151af74ca30a1e55b2c6e70df6bccb9b25
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-03-22 11:40:10 -0700

    improve SMILES handling and support aromatic forms
    Former-commit-id: 44c9a73334f923d1968f5e21b9f471dfb00a9d90

--- backend/core/curation/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/compound/CompoundConversionTest.scala
+++ backend/core/curation/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/curation/processor/compound/CompoundConversionTest.scala
@@ -11,21 +11,22 @@ class CompoundConversionTest extends WordSpec {
   "CalculateConversionTest" should {
     val compoundConversion: CompoundConversion = new CompoundConversion
 
-    "generate the correct canonical SMILES for CC(=O)N(CCCC1C(=O)NC(C(=O)NC(C(=O)NCC(=O)NCC(=O)NCC(=O)N1)CCCN(C(=O)C)[O-])CCCN(C(=O)C)[O-])[O-].[Fe+3][Na+]" in {
-      val SMILES: String = "CC(=O)N(CCCC1C(=O)NC(C(=O)NC(C(=O)NCC(=O)NCC(=O)NCC(=O)N1)CCCN(C(=O)C)[O-])CCCN(C(=O)C)[O-])[O-].[Fe+3][Na+]"
-      val molecule: IAtomContainer = compoundConversion.parseMolDefinition(compoundConversion.smilesToMolDefinition(SMILES))
+    val smiles_tests: Map[String, String] = Map(
+      "CC(=O)N(CCCC1C(=O)NC(C(=O)NC(C(=O)NCC(=O)NCC(=O)NCC(=O)N1)CCCN(C(=O)C)[O-])CCCN(C(=O)C)[O-])[O-].[Fe+3][Na+]" -> "O=C1NCC(=O)NC(C(=O)NC(C(=O)NC(C(=O)NCC(=O)NC1)CCCN([O-])C(=O)C)CCCN([O-])C(=O)C)CCCN([O-])C(=O)C.[Na+][Fe+3]",
+      "CC1C(C(C(C(O1)OCC2C(C(C(C(O2)OC3=CC(=C4C(=C3)OC(=CC4=O)C5=CC=C(C=C5)OC)O)O)O)O)O)O)O" -> "O=C1C=C(OC2=CC(OC3OC(COC4OC(C)C(O)C(O)C4O)C(O)C(O)C3O)=CC(O)=C12)C=5C=CC(OC)=CC5",
 
-      assert(compoundConversion.moleculeToSMILES(molecule) == SMILES)
+      // Aromatics
+      "CCCCNC(=O)NS(=O)(=O)c1ccc(C)cc1" -> "O=C(NCCCC)NS(=O)(=O)c1ccc(cc1)C",
+      "CC1CCN(CCCC(=O)c2ccc(F)cc2)CC1" -> "O=C(c1ccc(F)cc1)CCCN2CCC(C)CC2"
+    )
+
+    smiles_tests.foreach { case (key, value) =>
+
+      s"generate the correct canonical SMILES for $key" in {
+        val molecule: IAtomContainer = compoundConversion.parseMolDefinition(compoundConversion.smilesToMolDefinition(key))
+
+        assert(compoundConversion.moleculeToSMILES(molecule) == value)
+      }
     }
-
-    "generate the correct canonical SMILES for CC1C(C(C(C(O1)OCC2C(C(C(C(O2)OC3=CC(=C4C(=C3)OC(=CC4=O)C5=CC=C(C=C5)OC)O)O)O)O)O)O)O" in {
-      val SMILES: String = "CC1C(C(C(C(O1)OCC2C(C(C(C(O2)OC3=CC(=C4C(=C3)OC(=CC4=O)C5=CC=C(C=C5)OC)O)O)O)O)O)O)O"
-      val molecule: IAtomContainer = compoundConversion.parseMolDefinition(compoundConversion.smilesToMolDefinition(SMILES))
-
-
-
-      assert(compoundConversion.moleculeToSMILES(molecule) == SMILES)
-    }
-
   }
 }
