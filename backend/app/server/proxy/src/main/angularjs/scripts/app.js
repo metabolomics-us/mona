ac51453f852b3a38d343c4201a64f335fc22daa0
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-05-02 16:38:12 -0700

    initial refactoring into es6 with webpack build
    Former-commit-id: b108d6affd6818bdba23eced2786d5d9f7d52791

--- backend/app/server/proxy/src/main/angularjs/scripts/app.js
+++ backend/app/server/proxy/src/main/angularjs/scripts/app.js
@@ -1,93 +1,104 @@
-/***************************************
- * Our main module, and configs. Read the README.md for best practices
- * before making any changes to moaClientApp
- * *************************************/
+'use strict';
 
-(function() {
-    'use strict';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-    angular.module('moaClientApp', [
-        'ngRoute',
-        'ngResource',
-        'ngCookies',
-        'ngAnimate',
-        'ui.bootstrap',
-        'angular.filter',
-        'dialogs.main',
-        'ngTagsInput',
-        'wohlgemuth.msp.parser',
-        'wohlgemuth.mgf.parser',
-        'wohlgemuth.massbank.parser',
-        'wohlgemuth.cts',
-        'angularFileUpload',
-        'angularMasspecPlotter',
-        'toaster',
-        'nvd3',
-        'angular-google-analytics'
-    ])
+import angular from 'angular';
+import ngRoute from 'angular-route';
+import ngResource from 'angular-resource';
+import ngCookies from 'angular-cookies';
+import ngAnimate from 'angular-animate';
+import uiBootstrap from 'angular-ui-bootstrap'
 
-        /**
-         * Global $http error handling
-         * usage: in app.config
-         */
-        /* @ngInject */
-        .config(['$provide', '$httpProvider', function($provide, $httpProvider) {
-            $provide.factory('httpInterceptor', ['$q', '$location', '$rootScope', function($q, $location, $rootScope) {
-                $rootScope.httpError = [];
-                return {
-                    requestError: function(rejection) {
-                        return $q.reject(rejection);
-                    },
-                    response: function(response) {
-                        return response || $q.when(response);
-                    },
-                    responseError: function(rejection) {
-                        $rootScope.httpError.push(rejection);
-                        //$location.path('/'); enable this if we want to redirect users to index.html on error
-                        return $q.reject(rejection);
-                    }
-                };
-            }]);
+//var angular = require('angular');
+//import angularRoute from 'angular-route/angular-route';
 
-            $httpProvider.defaults.useXDomain = true;
-            delete $httpProvider.defaults.headers.common['X-Requested-With'];
-            $httpProvider.interceptors.push('httpInterceptor');
-        }])
+import {AuthenticationController} from './controllers/authentication/authenticationController';
 
-        /**
-         * Configure Google Analytics
-         */
-        .config(['AnalyticsProvider', function(AnalyticsProvider) {
-            AnalyticsProvider
-                .setAccount('UA-87692241-2')
-                .trackPages(true)
-                .ignoreFirstPageLoad(true);
-        }])
-        .run(['Analytics', function(Analytics) {}])
+angular.module('moaClientApp', [
+  ngRoute,
+  ngResource,
+  ngCookies,
+  ngAnimate,
+  uiBootstrap
 
-        /**
-         * App name
-         */
-        /* @ngInject */
-        .run(['$rootScope', function($rootScope) {
-            $rootScope.APP_NAME = 'MassBank of North America';
-            $rootScope.APP_NAME_ABBR = 'MoNA';
-            $rootScope.APP_VERSION = 'v1.0';
-        }])
+//        'angular.filter',
+//        'dialogs.main',
+//        'ngTagsInput',
+//        'wohlgemuth.msp.parser',
+//        'wohlgemuth.mgf.parser',
+//        'wohlgemuth.massbank.parser',
+//        'wohlgemuth.cts',
+//        'angularFileUpload',
+//        'angularMasspecPlotter',
+//        'toaster',
+//        'nvd3',
+//        'angular-google-analytics'
+])
 
-        /**
-         * Prompt user before leaving the page if spectra are being uploaded.
-         * Uses $injector to bypass timeout error when testing with protractor.
-         */
-        /* @ngInject */
-        .run(['$window', '$injector', function($window, $injector) {
-            $window.onbeforeunload = function(e) {
-                var service = $injector.get('UploadLibraryService');
+  /**
+   * Global $http error handling
+   * usage: in app.config
+   */
+  /* @ngInject */
+  .config(['$provide', '$httpProvider', function($provide, $httpProvider) {
+    $provide.factory('httpInterceptor', ['$q', '$location', '$rootScope', function($q, $location, $rootScope) {
+      $rootScope.httpError = [];
 
-                if (service.isUploading()) {
-                    var progress = parseInt(((service.completedSpectraCount / service.uploadedSpectraCount) * 100), 10);
-                    return 'MoNA is ' + progress + '% done with processing and uploading spectra.';
-                }
-            };
-        }]);
-})();
+      return {
+        requestError: function(rejection) {
+          return $q.reject(rejection);
+        },
+        response: function(response) {
+          return response || $q.when(response);
+        },
+        responseError: function(rejection) {
+          $rootScope.httpError.push(rejection);
+          //$location.path('/'); enable this if we want to redirect users to index.html on error
+          return $q.reject(rejection);
+        }
+      };
+    }]);
+
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $httpProvider.interceptors.push('httpInterceptor');
+  }])
+
+  /**
+   * Configure Google Analytics
+   */
+//        .config(['AnalyticsProvider', function(AnalyticsProvider) {
+//            AnalyticsProvider
+//                .setAccount('UA-87692241-2')
+//                .trackPages(true)
+//                .ignoreFirstPageLoad(true);
+//        }])
+//        .run(['Analytics', function(Analytics) {}])
+
+  /**
+   * App name
+   */
+  /* @ngInject */
+  .run(['$rootScope', function($rootScope) {
+    $rootScope.APP_NAME = 'MassBank of North America';
+    $rootScope.APP_NAME_ABBR = 'MoNA';
+    $rootScope.APP_VERSION = 'v1.0';
+  }])
+
+  /**
+   * Prompt user before leaving the page if spectra are being uploaded.
+   * Uses $injector to bypass timeout error when testing with protractor.
+   */
+  /* @ngInject */
+  .run(['$window', '$injector', function($window, $injector) {
+    $window.onbeforeunload = function(e) {
+      var service = $injector.get('UploadLibraryService');
+
+      if (service.isUploading()) {
+        var progress = parseInt(((service.completedSpectraCount / service.uploadedSpectraCount) * 100), 10);
+        return 'MoNA is ' + progress + '% done with processing and uploading spectra.';
+      }
+    };
+  }])
+
+  .controller('AuthenticationController', AuthenticationController);
--- backend/app/server/proxy/src/main/angularjs/scripts/controllers/authentication/authenticationController.js
+++ backend/app/server/proxy/src/main/angularjs/scripts/controllers/authentication/authenticationController.js
@@ -4,113 +4,112 @@
 
 // TODO: waiting for implementation of return user data for admin from authentication Service
 
-(function() {
-    'use strict';
-    AuthenticationController.$inject = ['$scope', '$rootScope', '$uibModal', 'AuthenticationService'];
-    angular.module('moaClientApp')
-      .controller('AuthenticationController', AuthenticationController);
+'use strict';
 
 
-    /* @ngInject */
-    function AuthenticationController($scope, $rootScope, $uibModal, AuthenticationService) {
-        var ADMIN_ROLE_NAME = 'ROLE_ADMIN';
-        var self = this;
+//AuthenticationController.$inject = ['$scope', '$rootScope', '$uibModal', 'AuthenticationService'];
+//export function AuthenticationController($scope, $rootScope, $uibModal, AuthenticationService) {
+AuthenticationController.$inject = ['$scope', '$rootScope'];
 
-        self.currentUser = null;
-        self.welcomeMessage = 'Login/Register';
+/* @ngInject */
+export function AuthenticationController($scope, $rootScope) {
+  var ADMIN_ROLE_NAME = 'ROLE_ADMIN';
+
+  this.currentUser = null;
+  this.welcomeMessage = 'Login/Register';
+//
+//  /**
+//   * Returns whether or not the user is logged in
+//   * @returns {*}
+//   */
+//  self.isLoggedIn = function() {
+//    return AuthenticationService.isLoggedIn();
+//  };
+//
+//  self.isAdmin = function() {
+//    if (AuthenticationService.isLoggedIn() && angular.isDefined($rootScope.currentUser.roles)) {
+//      for (var i = 0; i < $rootScope.currentUser.roles.length; i++) {
+//        if ($rootScope.currentUser.roles[i].authority === ADMIN_ROLE_NAME)
+//          return true;
+//      }
+//    }
+//
+//    return false;
+//  };
+//
+//  /**
+//   * Handle login
+//   */
+//  self.handleLogin = function() {
+//    if (self.isLoggedIn()) {
+//      AuthenticationService.logout();
+//    } else {
+//      self.openAuthenticationDialog();
+//    }
+//  };
+//
+//  /**
+//   * Opens the authentication modal dialog
+//   */
+//  self.openAuthenticationDialog = function() {
+//    $uibModal.open({
+//      templateUrl: '/views/authentication/authenticationModal.html',
+//      controller: 'AuthenticationModalController',
+//      size: 'sm',
+//      backdrop: 'true'
+//    });
+//  };
+//
+//  /**
+//   * Opens the registration modal dialog
+//   */
+//  self.handleRegistration = function() {
+//    if (!self.isLoggedIn()) {
+//      $uibModal.open({
+//        templateUrl: '/views/authentication/registrationModal.html',
+//        controller: 'RegistrationModalController',
+//        size: 'md',
+//        backdrop: 'static'
+//      });
+//    }
+//  };
+//
+//  /**
+//   * Create a welcome message on login
+//   */
+//  $scope.$on('auth:login-success', function(event, data, status, headers, config) {
+//
+//    AuthenticationService.getCurrentUser().then(function(data) {
+//      console.log(JSON.stringify(data));
+//      self.welcomeMessage = 'Welcome, ' + data.firstName + '!';
+//    });
+//  });
+//
+//  /**
+//   * Remove the welcome message on logout
+//   */
+//  $scope.$on('auth:logout', function(event, data, status, headers, config) {
+//    self.welcomeMessage = 'Login/Register';
+//  });
+//
+//  /**
+//   * Listen for external calls to bring up the authentication modal
+//   */
+//  $scope.$on('auth:login', function(event) {
+//    if (!self.isLoggedIn()) {
+//      self.openAuthenticationDialog();
+//    }
+//  });
+//
+//  /**
+//   * Attempt to log in with authentication cookie stored in cookie
+//   */
+//  (function() {
+//    AuthenticationService.validate();
+//  })();
+}
 
 
-        /**
-         * Returns whether or not the user is logged in
-         * @returns {*}
-         */
-        self.isLoggedIn = function() {
-            return AuthenticationService.isLoggedIn();
-        };
-
-        self.isAdmin = function() {
-            if (AuthenticationService.isLoggedIn() && angular.isDefined($rootScope.currentUser.roles)) {
-                for (var i = 0; i < $rootScope.currentUser.roles.length; i++) {
-                    if ($rootScope.currentUser.roles[i].authority === ADMIN_ROLE_NAME)
-                        return true;
-                }
-            }
-
-            return false;
-        };
-
-        /**
-         * Handle login
-         */
-        self.handleLogin = function() {
-            if (self.isLoggedIn()) {
-                AuthenticationService.logout();
-            } else {
-                self.openAuthenticationDialog();
-            }
-        };
-
-        /**
-         * Opens the authentication modal dialog
-         */
-        self.openAuthenticationDialog = function() {
-            $uibModal.open({
-                templateUrl: '/views/authentication/authenticationModal.html',
-                controller: 'AuthenticationModalController',
-                size: 'sm',
-                backdrop: 'true'
-            });
-        };
-
-        /**
-         * Opens the registration modal dialog
-         */
-        self.handleRegistration = function() {
-            if (!self.isLoggedIn()) {
-                $uibModal.open({
-                    templateUrl: '/views/authentication/registrationModal.html',
-                    controller: 'RegistrationModalController',
-                    size: 'md',
-                    backdrop: 'static'
-                });
-            }
-        };
-
-        /**
-         * Create a welcome message on login
-         */
-        $scope.$on('auth:login-success', function(event, data, status, headers, config) {
-
-            AuthenticationService.getCurrentUser().then(function(data) {
-                console.log(JSON.stringify(data));
-                self.welcomeMessage = 'Welcome, ' + data.firstName + '!';
-            });
-        });
-
-        /**
-         * Remove the welcome message on logout
-         */
-        $scope.$on('auth:logout', function(event, data, status, headers, config) {
-            self.welcomeMessage = 'Login/Register';
-        });
-
-        /**
-         * Listen for external calls to bring up the authentication modal
-         */
-        $scope.$on('auth:login', function(event) {
-            if (!self.isLoggedIn()) {
-                self.openAuthenticationDialog();
-            }
-        });
-
-        /**
-         * Attempt to log in with authentication cookie stored in cookie
-         */
-        (function() {
-            AuthenticationService.validate();
-        })();
-    }
-})();
 
 
 
