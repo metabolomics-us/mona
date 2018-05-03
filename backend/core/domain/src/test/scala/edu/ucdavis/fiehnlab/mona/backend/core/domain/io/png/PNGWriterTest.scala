e9eb96f3fe731a1ff7c9073c98802c3d9dc27f02
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2018-05-03 14:56:26 -0700

    added support for png exports of spectral records
    Former-commit-id: 7668a290d9d2c7e14cfd1c71285f313368ceee6e

---
+++ backend/core/domain/src/test/scala/edu/ucdavis/fiehnlab/mona/backend/core/domain/io/png/PNGWriterTest.scala
@@ -0,0 +1,45 @@
+package edu.ucdavis.fiehnlab.mona.backend.core.domain.io.png
+
+import java.io.{InputStreamReader, StringWriter}
+
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.Spectrum
+import edu.ucdavis.fiehnlab.mona.backend.core.domain.io.json.JSONDomainReader
+import org.scalatest.WordSpec
+
+/**
+  * Created by sajjan on 4/30/2018.
+  */
+class PNGWriterTest extends WordSpec {
+
+  "we should be able to create an instance of the writer" when {
+    val writer: PNGWriter = new PNGWriter
+
+    "a writer" should {
+      "export monaRecord.json" must {
+        val reader: JSONDomainReader[Spectrum] = JSONDomainReader.create[Spectrum]
+        val input: InputStreamReader = new InputStreamReader(getClass.getResourceAsStream("/monaRecord.json"))
+        val spectrum: Spectrum = reader.read(input)
+
+        val out: StringWriter = new StringWriter()
+        writer.write(spectrum, out)
+
+        "contain the spectrum id" in {
+          assert(out.toString.startsWith("252"))
+        }
+      }
+
+      "export first curatedRecords.json" must {
+        val reader: JSONDomainReader[Array[Spectrum]] = JSONDomainReader.create[Array[Spectrum]]
+        val input: InputStreamReader = new InputStreamReader(getClass.getResourceAsStream("/curatedRecords.json"))
+        val spectrum: Spectrum = reader.read(input).head
+
+        val out: StringWriter = new StringWriter()
+        writer.write(spectrum, out)
+
+        "contain the spectrum id" in {
+          assert(out.toString.startsWith("AU100601"))
+        }
+      }
+    }
+  }
+}
