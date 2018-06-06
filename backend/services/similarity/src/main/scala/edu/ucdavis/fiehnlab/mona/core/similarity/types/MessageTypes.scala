fc5644fdd1f05f9c971b0b95b998fb1467ff4615
Matthew Mueller <matthewmueller@dhcp8-gc5.genomecenter.ucdavis.edu>
2018-06-06 13:53:48 -0700

    added 'tags' field to SimilaritySearchRequest and SimpleSpectrum
    Former-commit-id: 02b978b3fb5353b0859b1ecc80101f31a07be355

--- backend/services/similarity/src/main/scala/edu/ucdavis/fiehnlab/mona/core/similarity/types/MessageTypes.scala
+++ backend/services/similarity/src/main/scala/edu/ucdavis/fiehnlab/mona/core/similarity/types/MessageTypes.scala
@@ -14,11 +14,14 @@ case class SimilaritySearchRequest(
                                     minSimilarity: Double,
                                     precursorMZ: Double,
                                     precursorToleranceDa: Double,
-                                    precursorTolerancePPM: Double
+                                    precursorTolerancePPM: Double,
+                                    tags: Array[String]
                                   )
 
 object SimilaritySearchRequest {
-  def apply(spectrum: String, minSimilarity: Double): SimilaritySearchRequest = SimilaritySearchRequest(spectrum, minSimilarity, 0.0, 0.0, 0.0)
+  def apply(spectrum: String, minSimilarity: Double): SimilaritySearchRequest = SimilaritySearchRequest(spectrum, minSimilarity, 0.0, 0.0, 0.0, new Array[String] (0))
+
+  def apply(spectrum: String, minSimilarity: Double, precursorMZ: Double, precursorToleranceDa: Double, precursorTolerancePPM: Double): SimilaritySearchRequest = SimilaritySearchRequest(spectrum, minSimilarity, precursorMZ, precursorToleranceDa, precursorTolerancePPM, new Array[String] (0))
 }
 
 
