e21b900661889b74c4817c225220852a9f9aa8d4
NolanGuzman97 <nolanguzman97@gmail.com>
2022-02-09 13:55:28 -0800

    fixed spectra browser test, just need to finish adding the other tests, but will run now the simple one

--- backend/app/server/proxy/src/main/mona-web/src/app/components/browser/spectra-browser.component.spec.ts
+++ backend/app/server/proxy/src/main/mona-web/src/app/components/browser/spectra-browser.component.spec.ts
@@ -6,46 +6,50 @@ import {Location} from "@angular/common";
 import {SpectrumCacheService} from "../../services/cache/spectrum-cache.service";
 import {Metadata} from "../../services/persistence/metadata.resource";
 import {CookieMain} from "../../services/cookie/cookie-main.service";
-import {ToasterModule} from "angular2-toaster";
+import {ToasterModule, ToasterService} from "angular2-toaster";
 import {GoogleAnalyticsService} from "ngx-google-analytics";
 import {ActivatedRoute, Router} from "@angular/router";
 import {FeedbackCacheService} from "../../services/feedback/feedback-cache.service";
 import {AuthenticationService} from "../../services/authentication.service";
 import {HttpClientModule} from "@angular/common/http";
 import {LoggerTestingModule} from "ngx-logger/testing";
-import {BehaviorSubject, Observable} from "rxjs";
+import {BehaviorSubject, Observable, of} from "rxjs";
 import {RouterTestingModule} from "@angular/router/testing";
 import {User} from "../../mocks/user.model";
+import {Feedback} from "../../services/persistence/feedback.resource";
+import {NgbAccordionModule} from "@ng-bootstrap/ng-bootstrap";
 
 describe('Spectra Browser Component', () => {
   let component: SpectraBrowserComponent;
   let fixture: ComponentFixture<SpectraBrowserComponent>;
   let spectrumServiceStub: Partial<Spectrum>;
   let spectrum: Spectrum;
-  let spectraQueryBuilderStub: Partial<SpectraQueryBuilderService>;
-  let spectraQueryBuilder: SpectraQueryBuilderService;
-  let spectrumCacheStub: Partial<SpectrumCacheService>;
   let spectrumCache: SpectrumCacheService;
-  let metadataStub: Partial<Metadata>;
-  let metadata: Metadata;
-  let cookieStub: Partial<CookieMain>;
-  let cookie: CookieMain;
   let feedbackCacheStud: Partial<FeedbackCacheService>;
-  let feedback: FeedbackCacheService;
+  let feedbackCache: FeedbackCacheService;
+  let feedback: Feedback;
+  let feedbackServiceStub: Partial<Feedback>;
   let authenticationServiceStub: Partial<AuthenticationService>;
-  let authenticationService: FeedbackCacheService;
+  let authenticationService: AuthenticationService;
+  let router: Router;
+  let route: ActivatedRoute;
+  let spectraQueryBuilder: SpectraQueryBuilderService;
 
   let fakeIsAdmin$: BehaviorSubject<boolean>;
   let fakeIsLoggedIn$: BehaviorSubject<boolean>;
   let fakeIsAuthenticated$: BehaviorSubject<boolean>;
   let fakeCurrentUser$: BehaviorSubject<User>;
   let fakeModalRequest$: BehaviorSubject<boolean>;
-  let fakeLoginPaylod$: BehaviorSubject<any>;
+  let fakeLoginPayload$: BehaviorSubject<any>;
   let fakeSpectrumGetObservable$: BehaviorSubject<any>;
   let fakeSpectrumUpdateObservable$: BehaviorSubject<any>;
   let fakeSpectrumSimilarObservable$: BehaviorSubject<any>;
   let fakeSpectrumCountObservable$: BehaviorSubject<any>;
   let fakeSpectrumSearchObservable$: BehaviorSubject<any>;
+  let fakeFeedbackCacheObservable$: BehaviorSubject<any>;
+  let fakeFeedbackGetObservable$: BehaviorSubject<any>;
+  let fakeFeedbackSaveObservable$: BehaviorSubject<any>;
+  let queryParams: Observable<any>;
 
   beforeEach(async () => {
     fakeIsAdmin$ = new BehaviorSubject(false);
@@ -53,13 +57,30 @@ describe('Spectra Browser Component', () => {
     fakeIsAuthenticated$ = new BehaviorSubject(false);
     fakeCurrentUser$ = new BehaviorSubject<User>(null)
     fakeModalRequest$ = new BehaviorSubject<boolean>(false);
-    fakeLoginPaylod$ = new BehaviorSubject<any>(false);
+    fakeLoginPayload$ = new BehaviorSubject<any>(false);
 
     fakeSpectrumGetObservable$ = new BehaviorSubject<any>(false);
     fakeSpectrumUpdateObservable$ = new BehaviorSubject<any>(false);
     fakeSpectrumSimilarObservable$ = new BehaviorSubject<any>(false);
     fakeSpectrumCountObservable$ = new BehaviorSubject<any>(false);
+    fakeFeedbackCacheObservable$ = new BehaviorSubject<any>(false);
+    fakeFeedbackGetObservable$ = new BehaviorSubject<any>(false);
+    fakeFeedbackSaveObservable$ = new BehaviorSubject<any>(false);
+    fakeSpectrumSearchObservable$ = new BehaviorSubject<any>(false);
 
+    queryParams = new Observable<any>(observer => {
+      const urlParams = {
+        inchikey: undefined,
+        splash: undefined,
+        query: undefined,
+        text: undefined,
+        size: 10,
+        pageParam: undefined,
+        tableParam: undefined
+      }
+      observer.next(urlParams);
+      observer.complete();
+    });
     spectrumServiceStub = {
       get(): Observable<any> {
         return fakeSpectrumGetObservable$.asObservable();
@@ -75,6 +96,15 @@ describe('Spectra Browser Component', () => {
       },
       searchSpectra(): Observable<any> {
         return fakeSpectrumSearchObservable$.asObservable();
+      }
+    };
+
+    feedbackServiceStub = {
+      get(): Observable<any> {
+        return fakeFeedbackGetObservable$.asObservable();
+      },
+      save(): Observable<any> {
+        return fakeFeedbackSaveObservable$.asObservable();
       }
     };
 
@@ -95,12 +125,14 @@ describe('Spectra Browser Component', () => {
       currentUser: fakeCurrentUser$.asObservable(),
       modalRequest: fakeModalRequest$.asObservable(),
       login(): Observable<any> {
-        return fakeLoginPaylod$.asObservable();
+        return fakeLoginPayload$.asObservable();
       }
     };
 
-    spectraQueryBuilderStub = {
-
+    feedbackCacheStud = {
+      resolveFeedback(): Observable<any> {
+        return fakeFeedbackCacheObservable$.asObservable();
+      }
     }
 
     spyOn(authenticationServiceStub, 'isLoggedIn').and.callThrough();
@@ -118,19 +150,33 @@ describe('Spectra Browser Component', () => {
     await TestBed.configureTestingModule({
       declarations: [SpectraBrowserComponent],
       providers: [{provide: AuthenticationService, useValue: authenticationServiceStub}, Location, GoogleAnalyticsService,
-      Router, ActivatedRoute, {provide: Spectrum, useValue: spectrumServiceStub}],
+      {provide: Spectrum, useValue: spectrumServiceStub}, SpectraQueryBuilderService, SpectrumCacheService,
+      ToasterService, FeedbackCacheService, {provide: FeedbackCacheService, useValue: feedbackCacheStud}, Metadata, CookieMain,
+      Feedback, {provide: ActivatedRoute, useValue: {queryParams: queryParams}}],
       imports: [
-        HttpClientModule, LoggerTestingModule, RouterTestingModule, ToasterModule
+        HttpClientModule, LoggerTestingModule, RouterTestingModule.withRoutes([{
+          path:'spectra/browse', component: SpectraBrowserComponent
+        }]), ToasterModule,
+        NgbAccordionModule
       ]
     });
     fixture = TestBed.createComponent(SpectraBrowserComponent);
     component = fixture.componentInstance;
     spectrum = TestBed.inject(Spectrum);
+    spectrumCache = TestBed.inject(SpectrumCacheService);
+    authenticationService = TestBed.inject(AuthenticationService);
+    feedback = TestBed.inject(Feedback);
+    feedbackCache = TestBed.inject(FeedbackCacheService);
+    router = TestBed.inject(Router);
+    route = TestBed.inject(ActivatedRoute);
+    spectraQueryBuilder = TestBed.inject(SpectraQueryBuilderService)
 
     fixture.detectChanges();
   });
 
-  it('executes queries', () => {
-
+  it('reset queries', () => {
+    spyOn(spectraQueryBuilder, 'executeQuery').and.callThrough();
+    component.resetQuery();
+    expect(spectraQueryBuilder.executeQuery).toHaveBeenCalled();
   });
 });
