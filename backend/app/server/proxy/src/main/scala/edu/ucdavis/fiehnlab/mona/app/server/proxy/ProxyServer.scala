a707b62d43d235f9df3527c8061fe9d83a9a8de4
Nolan Guzman <nolanguzman97@gmail.com>
2023-02-13 13:31:36 -0800

    adjust cors issues and remove repository link in frontend (#76)

--- backend/app/server/proxy/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/proxy/ProxyServer.scala
+++ backend/app/server/proxy/src/main/scala/edu/ucdavis/fiehnlab/mona/app/server/proxy/ProxyServer.scala
@@ -1,6 +1,5 @@
 package edu.ucdavis.fiehnlab.mona.app.server.proxy
 
-import com.typesafe.scalalogging.LazyLogging
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.context.config.annotation.RefreshScope
@@ -9,14 +8,11 @@ import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurity
 import org.springframework.boot.actuate.autoconfigure.security.reactive.ReactiveManagementWebSecurityAutoConfiguration
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.core.annotation.Order
-import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.util.matcher.{NegatedServerWebExchangeMatcher, ServerWebExchangeMatchers}
 import org.springframework.stereotype.Controller
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.reactive.{CorsConfigurationSource, CorsWebFilter, UrlBasedCorsConfigurationSource}
 
 /**
   * Created by wohlgemuth on 3/28/16.
@@ -29,27 +25,6 @@ class ProxyServer
 
 object ProxyServer extends App {
   new SpringApplication(classOf[ProxyServer]).run()
-}
-
-@Configuration
-class PreFlightCorsConfiguration extends LazyLogging {
-  @Bean
-  def corsFilter: CorsWebFilter = {
-    new CorsWebFilter(corsConfigurationSource)
-  }
-
-  @Bean
-  def corsConfigurationSource: CorsConfigurationSource = {
-    val source: UrlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource()
-    val config: CorsConfiguration = new CorsConfiguration().applyPermitDefaultValues()
-    config.addAllowedMethod(HttpMethod.GET)
-    config.addAllowedMethod(HttpMethod.PUT)
-    config.addAllowedMethod(HttpMethod.POST)
-    config.addAllowedMethod(HttpMethod.OPTIONS)
-    config.addAllowedMethod(HttpMethod.DELETE)
-    source.registerCorsConfiguration("/**", config)
-    source
-  }
 }
 
 
