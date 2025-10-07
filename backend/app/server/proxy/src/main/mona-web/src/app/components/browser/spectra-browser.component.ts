4db47c0dea3cfaf52cc47936dad251e71490802a
jvogel <joevogel501@gmail.com>
2025-10-07 14:28:08 -0700

    Update 'Query Information' dropdown. Add copy to clipboard button for cURL command. Adjust tip for MSP format. Shorten warning label outline. Adjust formatting.

--- backend/app/server/proxy/src/main/mona-web/src/app/components/browser/spectra-browser.component.ts
+++ backend/app/server/proxy/src/main/mona-web/src/app/components/browser/spectra-browser.component.ts
@@ -19,7 +19,7 @@ import {MassDeletionService} from '../../services/persistence/mass-deletion.serv
 import {ActivatedRoute, Router} from '@angular/router';
 import {Component, OnInit, AfterViewInit} from '@angular/core';
 import {first} from 'rxjs/operators';
-import {faEdit, faTable, faList, faSearch, faSync, faServer, faSpinner, faTrash, faChartBar} from '@fortawesome/free-solid-svg-icons';
+import {faEdit, faTable, faList, faSearch, faSync, faServer, faSpinner, faTrash, faChartBar, faCopy} from '@fortawesome/free-solid-svg-icons';
 import {faStar, faStarHalfAlt} from '@fortawesome/free-solid-svg-icons';
 import {faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
 import {faBookmark} from '@fortawesome/free-regular-svg-icons';
@@ -64,6 +64,7 @@ export class SpectraBrowserComponent implements OnInit, AfterViewInit{
     faStar = faStar;
     faStarEmpty = faStarEmpty;
     faStarHalf = faStarHalfAlt;
+    faCopy = faCopy;
 
     constructor(public spectrum: Spectrum, public spectraQueryBuilderService: SpectraQueryBuilderService,  public location: Location,
                 public spectrumCache: SpectrumCacheService,  public metadata: Metadata,  public cookie: CookieMain,
@@ -544,5 +545,14 @@ export class SpectraBrowserComponent implements OnInit, AfterViewInit{
         rounded -= 1;
       }
       return result;
+    }
+
+    copyCurlCommand() {
+      const text = document.getElementById('curlCommand').innerText;
+      navigator.clipboard.writeText(text);
+
+      const msg = document.getElementById('copiedMsg');
+      msg.style.opacity = '1';
+      setTimeout(() => msg.style.opacity = '0', 2500);
     }
 }
