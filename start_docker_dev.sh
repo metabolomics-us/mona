e32d54fc02dac72327eea9170fc6d0500a3d5548
jvogel <joevogel501@gmail.com>
2026-06-23 10:33:09 -0700

    Add 'stop' arg to docker dev script

--- start_docker_dev.sh
+++ start_docker_dev.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+if [ "$1" = "stop" ]; then
+    echo "============================="
+    echo "  STOPPING DOCKER SERVICES   "
+    echo "============================="
+    # Stop full suite of microservices
+    docker-compose -f backend/docker-compose-dev.yml down
+    exit 0
+fi
+
 echo "============================="
 echo "  STARTING DOCKER SERVICES   "
 echo "============================="
-#Start full suite of microservices
+# Start full suite of microservices
 docker-compose -f backend/docker-compose-dev.yml up -d
