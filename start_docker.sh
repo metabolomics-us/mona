70fb490b19f1121a3471339f87163720069c51fb
jvogel <joevogel501@gmail.com>
2026-06-23 10:42:38 -0700

    Add environment arg parsing to docker start script

---
+++ start_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$1" == "stop" ]; then
+    echo "============================"
+    echo "  STOPPING DOCKER SERVICES   "
+    echo "============================"
+    if [ "$2" == "dev" ] || [ "$2" == "test" ] || [ "$2" == "local" ]; then
+        echo -e "Environment detected as $2\n"
+        docker-compose -f backend/docker-compose-"$2".yml down
+        exit 0
+    fi
+    # default to dev
+    docker-compose -f backend/docker-compose-dev.yml down
+    exit 0
+fi
+
+echo "============================"
+echo "  STARTING DOCKER SERVICES   "
+echo "============================"
+if [ "$1" == "dev" ] || [ "$1" == "test" ] || [ "$1" == "local" ]; then
+    echo -e "Environment detected as $1\n"
+    docker-compose -f backend/docker-compose-"$1".yml up -d
+    exit 0
+fi
+
+# default to dev
+docker-compose -f backend/docker-compose-dev.yml up -d
