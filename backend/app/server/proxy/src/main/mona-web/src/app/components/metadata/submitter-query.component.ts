7c29b52db979cf6e1442ee229c7191197d23aced
jvogel <joevogel501@gmail.com>
2026-07-09 09:31:33 -0700

    Admin button to delete all spectra by a user

--- backend/app/server/proxy/src/main/mona-web/src/app/components/metadata/submitter-query.component.ts
+++ backend/app/server/proxy/src/main/mona-web/src/app/components/metadata/submitter-query.component.ts
@@ -6,8 +6,10 @@
 import {SpectraQueryBuilderService} from '../../services/query/spectra-query-builder.service';
 import {AuthenticationService} from '../../services/authentication.service';
 import {AdminService} from '../../services/persistence/admin.resource';
+import {Spectrum} from '../../services/persistence/spectrum.resource';
 import {ToasterService} from 'angular2-toaster';
 import {Component, Input} from '@angular/core';
+import {first} from 'rxjs/operators';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -18,8 +20,14 @@ export class SubmitterQueryComponent {
 
     @Input() submitter;
     faSearch = faSearch;
+
+    // Confirmation state for deleting all of a submitter's spectra
+    showSpectraDeletionConfirmation = false;
+    pendingSpectraCount;
+
     constructor( public spectraQueryBuilderService: SpectraQueryBuilderService,
-                 public authenticationService: AuthenticationService, public adminService: AdminService, public toaster: ToasterService) {}
+                 public authenticationService: AuthenticationService, public adminService: AdminService,
+                 public spectrum: Spectrum, public toaster: ToasterService) {}
 
     /**
      * Create a new query based on the selected submitter
@@ -59,6 +67,65 @@ export class SubmitterQueryComponent {
             });
           });
       }
+    }
+
+    // Builds the RSQL query matching every spectrum submitted by this submitter
+    buildSubmitterQuery() {
+        this.spectraQueryBuilderService.prepareQuery();
+        this.spectraQueryBuilderService.addUserToQuery(this.submitter.emailAddress);
+        return this.spectraQueryBuilderService.getFilter();
+    }
+
+    // Prompts confirmation to delete all of the submitter's spectra, counting them first
+    askToDeleteSpectra() {
+        if (!this.isAdmin()) { return; }
+
+        this.pendingSpectraCount = null;
+        this.showSpectraDeletionConfirmation = true;
+
+        this.spectrum.searchSpectraCount({query: this.buildSubmitterQuery()}).pipe(first()).subscribe((res: any) => {
+            this.pendingSpectraCount = res.count;
+        }, (error) => {
+            this.toaster.pop({
+                type: 'error',
+                title: 'Could not count spectra',
+                body: 'Unable to determine how many spectra this user has submitted.'
+            });
+        });
+    }
+
+    // Cancels the pending spectra deletion confirmation
+    cancelDeleteSpectra() {
+        this.showSpectraDeletionConfirmation = false;
+        this.pendingSpectraCount = null;
+    }
+
+    /**
+     * Enqueues a background job deleting every spectrum submitted by this user. The backend rejects
+     * an empty query so this can never match everything
+     */
+    confirmDeleteSpectra() {
+        if (!this.isAdmin()) { return; }
+
+        const query = this.buildSubmitterQuery();
+        const submitterEmail = this.submitter.emailAddress;
+
+        this.spectrum.batchDelete({query}, this.authenticationService.getCurrentUser().accessToken)
+            .pipe(first()).subscribe(() => {
+                this.toaster.pop({
+                    type: 'success',
+                    title: 'Deletion Started',
+                    body: `Deleting all spectra submitted by ${submitterEmail}. This runs in the background and can take a while depending on how many spectra there are.`
+                });
+            }, (error) => {
+                this.toaster.pop({
+                    type: 'error',
+                    title: 'There was a problem deleting the user\'s spectra.',
+                    body: `${error.message}`
+                });
+            });
+
+        this.cancelDeleteSpectra();
     }
 
     isAdmin() {
